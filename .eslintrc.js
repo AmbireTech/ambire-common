6a616b76279c38655d1c3bce0f5d07f96e7542c8
superKalo <superkalo@devlabs.bg>
2022-05-19 12:59:35 +0300

    Fix: tsconfig ref

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'airbnb-typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './.tsconfig',
+    project: './tsconfig.json',
     ecmaFeatures: {
       jsx: true
     },
