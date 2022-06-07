f9f4a5bf2325d58b1c750a89894dccad60882d9c
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-06-07 15:35:15 +0300

    Moves types in a separate file

---
+++ src/hooks/accounts/types.ts
@@ -0,0 +1,42 @@
+import { UseToastsReturnType } from '../toasts'
+import { UseStorageProps, UseStorageReturnType } from '../useStorage'
+
+export type OnAddAccountOptions = {
+  shouldRedirect?: boolean
+  isNew?: boolean
+  select?: boolean
+}
+
+export type Account = {
+  baseIdentityAddr: string
+  bytecode: string
+  email: string
+  id: string
+  identityFactoryAddr: string
+  primaryKeyBackup: string
+  salt: string
+  signer: {
+    one: string
+    quickAccManager: string
+    timelock: number
+    two: string
+    // Sometimes passed as an extra prop
+    address?: string
+  }
+}
+
+export interface UseAccountsProps {
+  onAdd: (opts: OnAddAccountOptions) => void
+  onRemoveLastAccount: () => void
+  useStorage: (p: Omit<UseStorageProps, 'storage'>) => UseStorageReturnType
+  useToasts: () => UseToastsReturnType
+}
+
+export interface UseAccountsReturnType {
+  accounts: Account[]
+  account: Account | {}
+  selectedAcc: string
+  onSelectAcc: (accountId: Account['id']) => void
+  onAddAccount: (acc: Account, opts: OnAddAccountOptions) => void
+  onRemoveAccount: (accountId: Account['id']) => void
+}
