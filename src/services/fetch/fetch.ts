744ea09fa9a613892b81baf104b5cd0ff1b83bd8
superKalo <superkalo@devlabs.bg>
2022-06-08 17:47:24 +0300

    Add: fetch service, with option to pass custom fetch

---
+++ src/services/fetch/fetch.ts
@@ -0,0 +1,39 @@
+export async function fetchPost(_fetch: any, url: string, body: any) {
+  const r = await _fetch(url, {
+    headers: { 'content-type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify(body)
+  })
+  return r.json()
+}
+
+export async function fetchGet(_fetch: any, url: string) {
+  const response = await _fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if (response.status !== 200) throw new Error('Failed to fetch')
+  return response.json()
+}
+
+export async function fetchCaught(_fetch: any, url: any, params?: any) {
+  let resp
+  try {
+    resp = await fetch(url, params)
+  } catch (e: any) {
+    // TODO:
+    // console.error(e)
+    return { errMsg: `Unexpected error: ${e && e.message}` }
+  }
+  let body
+  try {
+    body = await resp.json()
+  } catch (e: any) {
+    // TODO:
+    // console.error(e)
+    return { errMsg: `Unexpected error: ${resp.status}, ${e && e.message}`, resp }
+  }
+  return { body, resp, errMsg: '' }
+}
