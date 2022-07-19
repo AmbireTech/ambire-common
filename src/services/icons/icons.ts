3918fa3f5cab401d2d3b6d8727bd6622375e3ca2
Kaloyan Kosev <superkalo@devlabs.bg>
2022-07-19 15:21:36 +0300

    Add: missing custom icons type

--- src/services/icons/icons.ts
+++ src/services/icons/icons.ts
@@ -1,4 +1,4 @@
-const customIcons = {
+const customIcons: { [key in string]: string } = {
   '0xb468a1e5596cfbcdf561f21a10490d99b4bb7b68':
     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Jeff_Sessions_with_Elmo_and_Rosita_%28cropped%29.jpg/220px-Jeff_Sessions_with_Elmo_and_Rosita_%28cropped%29.jpg', // TEST Polygon ELMO token,
   '0x88800092ff476844f74dc2fc427974bbee2794ae':
