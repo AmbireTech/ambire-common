0de3007d99194af623f04d7d89836507a4fa22df
superKalo <superkalo@devlabs.bg>
2022-08-08 17:33:11 +0300

    Change: move types to a separate file

--- src/hooks/useRewards/types.ts
+++ src/hooks/useRewards/types.ts
@@ -1,4 +1,5 @@
 import { UseAccountsReturnType } from '../useAccounts'
+import { Account } from '../useAccounts/types'
 import { UseRelayerDataReturnType } from '../useRelayerData'
 
 export enum RewardIds {
@@ -11,4 +12,31 @@ export type UseRewardsProps = {
   useAccounts: () => UseAccountsReturnType
   useRelayerData: (url: string | null) => UseRelayerDataReturnType
   useClaimableWalletToken: () => any // TODO
+}
+
+type Multiplier = {
+  mul: number
+  name: Text
+}
+
+type Reward = {
+  _id: string
+  rewards: {
+    [key in Account['id']]: number
+  }
+  updated: string // timestamp
+}
+
+export type RewardsData = {
+  data: {
+    adxTokenAPY: number
+    multipliers: Multiplier[]
+    rewards: Reward[]
+    success: boolean
+    usdPrice: number
+    walletTokenAPY: number
+    xWALLETAPY: number
+  }
+  errMsg: null
+  isLoading: boolean
 }
