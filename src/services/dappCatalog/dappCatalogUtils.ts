38b6555d4d33bb79cc82dbf9926bcd762fe401c0
IvoPaunov <ivo.paunov@gmail.com>
2022-08-11 15:57:24 +0300

    add more dapp catalog utils

--- src/services/dappCatalog/dappCatalogUtils.ts
+++ src/services/dappCatalog/dappCatalogUtils.ts
@@ -1,8 +1,9 @@
 import networks, { NetworkId } from '../../constants/networks'
+import { fetchCaught } from '../fetch'
 import walletGnosisDefaultCatalog from './ambire-wallet-gnosis-default.applist.json'
 import walletWalletconnectDefaultCatalog from './ambire-wallet-walletconnect-default.applist.json'
 import gnosisDefaultList from './gnosis-default.applist.json'
-import { AmbireDappManifest, WalletConnectionType } from './types'
+import { AmbireDappManifest, SupportedWeb3Connectivity, WalletConnectionType } from './types'
 
 export const chainIdToWalletNetworkId = (chainId: number): NetworkId | null => {
   return networks.find((n) => n.chainId === chainId)?.id || null
@@ -44,4 +45,49 @@ export function getWalletWalletconnectDefaultList(): AmbireDappManifest[] {
   )
 
   return walletGnosisDapps
+}
+
+export const getDappId = (name: string): string => {
+  return name.toLowerCase().replace(/s/g, '_') + '_' + Date.now()
+}
+
+export const getNormalizedUrl = (inputStr: string): string => {
+  const url = inputStr.toLowerCase().split(/[?#]/)[0].replace('/manifest.json', '')
+  return url
+}
+
+export const canOpenInIframe = async (fetch: any, url: string): Promise<boolean> => {
+  const res = await fetchCaught(url, { method: 'HEAD' })
+
+  // NOTE: looks like it enough to open it in iframe 
+  // It fails for cors and x-frame-options
+  const canBeLoaded = !!res?.resp?.ok
+
+  return canBeLoaded
+}
+
+export const getManifestFromDappUrl = async (fetch: any, dAppUrl: string): Promise<AmbireDappManifest | null> => {
+  const normalizedUrl = getNormalizedUrl(dAppUrl)
+  const url = normalizedUrl.replace(/\/$/, '')
+  const manifestUrl = url + '/manifest.json?' + Date.now()
+
+  const { body } = await fetchCaught(fetch, manifestUrl)
+
+  const hasManifest = !!body && body.name && (Array.isArray(body.icons) || body.iconPath)
+
+  const isGnosisManifest = hasManifest && body.description && body.iconPath
+  const isWalletPlugin = hasManifest && body.name && body.description && Array.isArray(body.Networks)
+    && (isGnosisManifest || (Array.isArray(body.web3Connectivity) && body.web3Connectivity.includes(SupportedWeb3Connectivity.gnosis)))
+
+  const manifest = hasManifest ? {
+    name: body.name,
+    description: body.description || body.name,
+    iconUrl: body.iconUrl || (url + '/' + body.iconPath?.replace(/^\//, '')),
+    connectionType: isGnosisManifest ? 'gnosis' : 'walletconnect',
+    networks: (body.networks || []).map(chainIdToWalletNetworkId),
+    isWalletPlugin,
+    web3Connectivity: body.web3Connectivity
+  } as AmbireDappManifest : null
+
+  return manifest
 }
