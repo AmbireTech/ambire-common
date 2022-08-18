7606ca64e7d03cc115b53d4cf40b2ca4beb5d6e0
Gergana Mihaylova <gergana@devlabs.bg>
2022-08-18 17:35:08 +0300

    Fix: refresh time

--- src/hooks/usePortfolio/usePortfolio.ts
+++ src/hooks/usePortfolio/usePortfolio.ts
@@ -197,7 +197,7 @@ export default function usePortfolio({
   useEffect(() => {
     const refreshIfHidden = () =>
       !isVisible && !areAllNetworksBalancesLoading() ? fetchTokens(account, currentNetwork) : null
-    const refreshInterval = setInterval(refreshIfHidden, 1500)
+    const refreshInterval = setInterval(refreshIfHidden, 150000)
     return () => clearInterval(refreshInterval)
   }, [account, currentNetwork, isVisible, fetchTokens])
 
