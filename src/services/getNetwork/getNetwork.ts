f05e6447a84562b7a122410e310f5e768f2ba9bb
Kaloyan Kosev <superkalo@devlabs.bg>
2022-09-02 17:53:24 +0300

    Fix: ts warn

--- src/services/getNetwork/getNetwork.ts
+++ src/services/getNetwork/getNetwork.ts
@@ -1,7 +1,7 @@
 import networks from '../../constants/networks'
 
 export const getNetworkByChainId = (chainId: string | number) => {
-  return networks.find((n) => n.chainId === parseInt(chainId))
+  return networks.find((n) => n.chainId === parseInt(chainId.toString()))
 }
 
 export const getNetworkById = (id: string | number) => {
