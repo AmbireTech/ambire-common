21649fabcc401156116b908febbc7859b552da2a
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-11-01 19:48:59 +0200

    fixed eslint

--- src/services/humanizers/Swappin.ts
+++ src/services/humanizers/Swappin.ts
@@ -1,34 +1,44 @@
 // TODO: add types
 // @ts-nocheck
 
-import { abis } from '../../constants/humanizerInfo.json'
 import { Interface } from 'ethers/lib/utils'
+import { HumanizerInfoType } from '../../hooks/useConstants'
 import { nativeToken, token } from '../humanReadableTransactions'
 
-const swappin = new Interface(abis.Swappin)
+const SwappinMapping = (humanizerInfo: HumanizerInfoType) => {
+  const swappin = new Interface(humanizerInfo.abis.Swappin)
 
-const SwappinMapping = {
-  [swappin.getSighash('swap')]: (txn, network, { extended = false }) => {
-    const { desc } = swappin.parseTransaction(txn).args
-    const paymentSrcToken = Number(desc.srcToken) === 0 ? nativeToken(network, desc.amount, true) : token(desc.srcToken, parseFloat(desc.amount), true)
-    const paymentToken = Number(desc.dstToken) === 0 ? nativeToken(network, desc.minReturnAmount, true) : token(desc.dstToken, parseFloat(desc.minReturnAmount), true)
+  return {
+    [swappin.getSighash('swap')]: (txn, network, { extended = false }) => {
+      const { desc } = swappin.parseTransaction(txn).args
+      const paymentSrcToken =
+        Number(desc.srcToken) === 0
+          ? nativeToken(network, desc.amount, true)
+          : token(desc.srcToken, parseFloat(desc.amount), true)
+      const paymentToken =
+        Number(desc.dstToken) === 0
+          ? nativeToken(network, desc.minReturnAmount, true)
+          : token(desc.dstToken, parseFloat(desc.minReturnAmount), true)
 
-    return [
-        [
-            'Swap',
-            {
+      return !extended
+        ? [`Swap ${paymentSrcToken} for at least ${paymentToken} on Swappin`]
+        : [
+            [
+              'Swap',
+              {
                 type: 'token',
                 ...paymentSrcToken
-            },
-            'for at least',
-            {
+              },
+              'for at least',
+              {
                 type: 'token',
                 ...paymentToken
-            },
-            'on Swappin'
-        ]
-    ]
-  },
+              },
+              'on Swappin'
+            ]
+          ]
+    }
+  }
 }
 
 export default SwappinMapping
