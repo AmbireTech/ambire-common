af37e3b5c944831652f4908b2255a7009ce6e263
superKalo <superkalo@devlabs.bg>
2022-11-02 11:05:36 +0200

    Change: enable TS, add missing types and sort imports

--- src/services/humanizers/Swappin.ts
+++ src/services/humanizers/Swappin.ts
@@ -1,7 +1,6 @@
-// TODO: add types
-// @ts-nocheck
-
+import { NetworkType } from 'constants/networks'
 import { Interface } from 'ethers/lib/utils'
+
 import { HumanizerInfoType } from '../../hooks/useConstants'
 import { nativeToken, token } from '../humanReadableTransactions'
 
@@ -9,7 +8,7 @@ const SwappinMapping = (humanizerInfo: HumanizerInfoType) => {
   const swappin = new Interface(humanizerInfo.abis.Swappin)
 
   return {
-    [swappin.getSighash('swap')]: (txn, network, { extended = false }) => {
+    [swappin.getSighash('swap')]: (txn: any, network: NetworkType, { extended = false }) => {
       const { desc } = swappin.parseTransaction(txn).args
       const paymentSrcToken =
         Number(desc.srcToken) === 0
