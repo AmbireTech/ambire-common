6cff42d455cac8f818331e10e779787e6203c590
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-11-02 19:19:13 +0200

    fixed supportedDApps urls, fixed warning

--- src/constants/supportedDApps.ts
+++ src/constants/supportedDApps.ts
@@ -2,9 +2,9 @@ const supportedDApps: string[] = [
   'https://snapshot.org',
   'https://guild.xyz',
   'https://sudoswap.xyz',
-  'https://evm-sigtools.ambire.com',
+  'https://sigtool.ambire.com/',
   'https://app.swappin.gifts',
-  'https://staging.swappin.gifts/ref/ambire'
+  'https://app.swappin.gifts/ref/ambire'
 ]
 
 export default supportedDApps
