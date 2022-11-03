4f4bfab6d92e05ede1255a222b5053283782373a
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-11-03 15:30:41 +0200

    Fixed swappin humanizer

--- src/services/humanizers/Swappin.ts
+++ src/services/humanizers/Swappin.ts
@@ -4,6 +4,12 @@ import { NetworkType } from '../../constants/networks'
 import { HumanizerInfoType } from '../../hooks/useConstants'
 import { nativeToken, token } from '../humanReadableTransactions'
 
+const parseZeroAddressIfNeeded = (address: string) => {
+  return address.toLowerCase() === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    ? '0x0000000000000000000000000000000000000000'
+    : address
+}
+
 const SwappinMapping = (humanizerInfo: HumanizerInfoType) => {
   // @ts-ignore: this type mismatch is a consistent issue with all
   // humanizers, not just this one. Temporary ignore it.
@@ -13,14 +19,16 @@ const SwappinMapping = (humanizerInfo: HumanizerInfoType) => {
   return {
     [swappin.getSighash('swap')]: (txn: any, network: NetworkType, { extended = false }) => {
       const { desc } = swappin.parseTransaction(txn).args
+      const srcToken = parseZeroAddressIfNeeded(desc.srcToken)
+      const dstToken = parseZeroAddressIfNeeded(desc.dstToken)
       const paymentSrcToken =
-        Number(desc.srcToken) === 0
+        Number(srcToken) === 0
           ? nativeToken(network, desc.amount, extended)
-          : token(humanizerInfo, desc.srcToken, parseFloat(desc.amount), extended)
+          : token(humanizerInfo, srcToken, desc.amount, extended)
       const paymentToken =
-        Number(desc.dstToken) === 0
+        Number(dstToken) === 0
           ? nativeToken(network, desc.minReturnAmount, extended)
-          : token(humanizerInfo, desc.dstToken, parseFloat(desc.minReturnAmount), extended)
+          : token(humanizerInfo, dstToken, desc.minReturnAmount, extended)
 
       return !extended
         ? [`Swap ${paymentSrcToken} for at least ${paymentToken} on Swappin`]
