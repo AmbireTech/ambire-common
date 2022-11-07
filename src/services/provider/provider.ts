911a92d75156ff55f15d7a91bbeb15665f988203
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-11-07 18:33:07 +0200

    improve readability

--- src/services/provider/provider.ts
+++ src/services/provider/provider.ts
@@ -2,17 +2,19 @@ import { NetworkId } from '../../constants/networks'
 
 // Provider instances by a network id
 // For instance: { 'ethereum': new providers.StaticJsonRpcProvider }
-let rpcProviders: { [key in NetworkId]: any }
+let rpcProviders: any = {}
 
 export const initRpcProviders = (providers: { [key in NetworkId]: any }) => {
   rpcProviders = providers
 }
 
 export function getProvider(networkId: NetworkId) {
-  const providersByNetwork = rpcProviders ? rpcProviders[networkId] : null
+  const providersByNetwork = rpcProviders[networkId]
 
   if (!providersByNetwork)
-    throw new Error(`getProvider called with non-existent provider for network: ${networkId}`)
+    throw new Error(
+      `getProvider called with non-existent provider for network: ${networkId}. Initialize providers before calling getProvider`
+    )
 
   return providersByNetwork
 }
