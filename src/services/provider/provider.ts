e5b6de5ef9131195b445c21d418b8d5d4e9451b0
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-11-07 11:34:43 +0200

    Remove rpc from networks. Add: init provider function

--- src/services/provider/provider.ts
+++ src/services/provider/provider.ts
@@ -1,33 +1,18 @@
-import { providers } from 'ethers'
+import { NetworkId } from '../../constants/networks'
 
-import networks, { NetworkId } from '../../constants/networks'
-
-// Cache provider instances by a network id
+// Provider instances by a network id
 // For instance: { 'ethereum': new providers.StaticJsonRpcProvider }
-const providersByNetwork: any = {}
+let rpcProviders: { [key in NetworkId]: any }
+
+export const initRpcProviders = (prov: { [key in NetworkId]: any }) => {
+  rpcProviders = prov
+}
 
 export function getProvider(networkId: NetworkId) {
-  const network = networks.find(({ id }) => id === networkId)
-  if (!network) throw new Error(`getProvider called with non-existent network: ${networkId}`)
+  const providersByNetwork = rpcProviders ? rpcProviders[networkId] : null
 
-  // If the provider instance is already created, just reuse the cached instance,
-  // instead of creating the same object again.
-  if (providersByNetwork[networkId]) return providersByNetwork[networkId]
+  if (!providersByNetwork)
+    throw new Error(`getProvider called with non-existent provider for network: ${networkId}`)
 
-  const { id: name, chainId, ensName } = network
-  const url = network.rpc
-
-  if (url.startsWith('wss:')) {
-    providersByNetwork[networkId] = new providers.WebSocketProvider(url, {
-      name: ensName || name,
-      chainId
-    })
-  } else {
-    providersByNetwork[networkId] = new providers.StaticJsonRpcProvider(url, {
-      name: ensName || name,
-      chainId
-    })
-  }
-
-  return providersByNetwork[networkId]
+  return providersByNetwork
 }
