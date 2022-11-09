b2857eeda5d197ee6bf15ec6499e5dc55aeacc75
superKalo <superkalo@devlabs.bg>
2022-11-09 11:56:54 +0200

    Change: instead of throwing, log an error, since it doesnt break the app

--- src/services/provider/provider.ts
+++ src/services/provider/provider.ts
@@ -11,10 +11,11 @@ export const initRpcProviders = (providers: { [key in NetworkId]: any }) => {
 export function getProvider(networkId: NetworkId) {
   const providersByNetwork = rpcProviders[networkId]
 
-  if (!providersByNetwork)
-    throw new Error(
+  if (!providersByNetwork) {
+    console.error(
       `getProvider called with non-existent provider for network: ${networkId}. Initialize providers before calling getProvider`
     )
+  }
 
   return providersByNetwork
 }
