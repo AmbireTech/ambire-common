bbfb882bec9060da79ab5669b39de10f2bf57982
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-11-28 16:50:03 +0200

    useGetMsgType fixes

--- src/hooks/useGetMsgType/useGetMsgType.ts
+++ src/hooks/useGetMsgType/useGetMsgType.ts
@@ -1,45 +1,62 @@
 import { _TypedDataEncoder } from 'ethers/lib/utils'
+import { useMemo } from 'react'
 
 import { UseGetMsgTypeProps, UseGetMsgTypeReturnType } from './types'
 
 const useGetMsgType = ({ msgToSign }: UseGetMsgTypeProps): UseGetMsgTypeReturnType => {
-  let typeDataErr
-  let dataV4: any
   let requestedChainId = msgToSign.chainId
-  const isTypedData = ['eth_signTypedData_v4', 'eth_signTypedData'].indexOf(msgToSign?.type) !== -1
-  if (isTypedData) {
-    dataV4 = msgToSign.txn
+  const isTypedData = useMemo(
+    () => ['eth_signTypedData_v4', 'eth_signTypedData'].indexOf(msgToSign?.type) !== -1,
+    [msgToSign?.type]
+  )
+
+  const typeData = useMemo(() => {
+    let dataV4
+    let typeDataErr
     try {
-      if (dataV4.startsWith('{')) {
-        dataV4 = JSON.parse(msgToSign.txn)
+      if (isTypedData) {
+        dataV4 = msgToSign.txn
+        try {
+          if (dataV4.startsWith('{')) {
+            dataV4 = JSON.parse(msgToSign.txn)
+          }
+        } catch (error) {
+          dataV4 = msgToSign.txn
+        }
+        if (typeof dataV4 === 'object' && dataV4 !== null) {
+          try {
+            if (dataV4?.types?.EIP712Domain) {
+              // Avoids failure in case some dapps explicitly add this (redundant) prop
+              delete dataV4?.types?.EIP712Domain
+            }
+            _TypedDataEncoder.hash(dataV4?.domain, dataV4.types, dataV4?.message)
+            // enforce chainId
+            if (dataV4.domain?.chainId) {
+              requestedChainId = dataV4.domain?.chainId
+            }
+          } catch {
+            typeDataErr = '.txn has Invalid TypedData object. Should be {domain, types, message}'
+          }
+        } else {
+          typeDataErr = '.txn should be a TypedData object'
+        }
       }
     } catch (error) {
-      dataV4 = msgToSign.txn
+      typeDataErr = error.message || error
     }
-    if (typeof dataV4 === 'object' && dataV4 !== null) {
-      try {
-        if (dataV4?.types?.EIP712Domain) {
-          // Avoids failure in case some dapps explicitly add this (redundant) prop
-          delete dataV4?.types?.EIP712Domain
-        }
-        _TypedDataEncoder.hash(dataV4?.domain, dataV4.types, dataV4?.message)
-        // enforce chainId
-        if (dataV4.domain?.chainId) {
-          requestedChainId = dataV4.domain?.chainId
-        }
-      } catch {
-        typeDataErr = '.txn has Invalid TypedData object. Should be {domain, types, message}'
-      }
-    } else {
-      typeDataErr = '.txn should be a TypedData object'
-    }
-  }
 
+    return {
+      dataV4,
+      typeDataErr
+    }
+  }, [isTypedData, msgToSign])
+
+  console.log(isTypedData, typeData.dataV4, typeData.typeDataErr, requestedChainId)
   return {
-    typeDataErr,
-    requestedChainId,
-    dataV4,
-    isTypedData
+    isTypedData,
+    dataV4: typeData.dataV4,
+    typeDataErr: typeData.typeDataErr,
+    requestedChainId
   }
 }
 
