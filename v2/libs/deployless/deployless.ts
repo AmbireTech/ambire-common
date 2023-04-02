c329ead4322d0188a68164621d8ff1ca650bd175
Ivo Georgiev <ivo@ambire.com>
2023-04-02 21:13:12 +0300

    deployless added

---
+++ v2/libs/deployless/deployless.ts
@@ -0,0 +1,5 @@
+// @TODO type for provider
+// @TODO proxy pattern and/or define all methods for an abi, return an object
+export async function call(provider: any, contract: { bin: string, abi: any }, method: string, args: any[]): Promise<any> {
+	
+}
