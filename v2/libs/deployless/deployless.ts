f28c97317fe34c86b2ff760a717d5243af73839e
Ivo Georgiev <ivo@ambire.com>
2023-04-03 17:13:19 +0300

    deployless: some work done but lets abandon the dynamically set methods pattern

--- v2/libs/deployless/deployless.ts
+++ v2/libs/deployless/deployless.ts
@@ -1,5 +1,22 @@
 // @TODO type for provider
 // @TODO proxy pattern and/or define all methods for an abi, return an object
-export async function call(provider: any, contract: { bin: string, abi: any }, method: string, args: any[]): Promise<any> {
+import { Interface } from 'ethers/lib/utils'
+
+export class Deployless {
+	isLimitedAt24kbData: boolean;
+	// to allow for the dynamically added properties
+	[key: string]: any;
+
+	constructor (abi: any, code: string) {
+		// true until we can test it
+		this.isLimitedAt24kbData = true
 	
+		const iface = new Interface(abi)
+		for (const item of abi) {
+			if (item.type !== 'function') continue
+			this[item.name] = (...args) => {
+				console.log(args)
+			}
+		}
+	}
 }
