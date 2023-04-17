30d65e1439b03ae5e2676a5e1bb2239a789dbcdc
Ivo Georgiev <ivo@ambire.com>
2023-04-17 19:41:56 +0300

    gasprice: define return data and return all necessary info

--- v2/libs/gasprice/gasprice.ts
+++ v2/libs/gasprice/gasprice.ts
@@ -6,18 +6,29 @@ const ELASTICITY_MULTIPLIER = 2n
 
 // multipliers from the old: https://github.com/AmbireTech/relayer/blob/wallet-v2/src/utils/gasOracle.js#L64-L76
 // 2x, 2x*0.4, 2x*0.2 - all of them divided by 8 so 0.25, 0.1, 0.05 - those seem usable; with a slight tweak for the ape
-// @TODO we may not use the gasPriceMultiplierBps in this form
 const speeds = [
-	{ name: 'slow', baseFeeAddBps: 0n, gasPriceMultiplierBps: 9000n },	
-	{ name: 'medium', baseFeeAddBps: 500n, gasPriceMultiplierBps: 10000n },
-	{ name: 'fast', baseFeeAddBps: 1000n, gasPriceMultiplierBps: 11000n },	
-	{ name: 'ape', baseFeeAddBps: 1500n, gasPriceMultiplierBps: 11500n },
+	{ name: 'slow', baseFeeAddBps: 0n },
+	{ name: 'medium', baseFeeAddBps: 500n },
+	{ name: 'fast', baseFeeAddBps: 1000n },
+	{ name: 'ape', baseFeeAddBps: 1500n },
 ]
-// @TODO return type
-export async function getGasPriceRecommendations (provider: Provider, blockTag: string | number = -1): Promise<any> {
+
+export interface GasPriceRecommendation {
+	name: string,
+	gasPrice: bigint
+}
+export interface Gas1559Recommendation {
+	name: string,
+	baseFeePerGas: bigint,
+	maxPriorityFeePerGas: bigint
+}
+export type GasRecommendation = GasPriceRecommendation | Gas1559Recommendation;
+
+export async function getGasPriceRecommendations (provider: Provider, blockTag: string | number = -1): Promise<GasRecommendation[]> {
 	const lastBlock = await provider.getBlock(blockTag, true)
-	if (lastBlock == null) return null
-	// console.log(lastBlock, lastBlock.prefetchedTransactions, isEIP1559)
+	if (lastBlock == null) throw new Error('unable to retrieve block')
+	// https://github.com/ethers-io/ethers.js/issues/3683#issuecomment-1436554995
+	const txns = lastBlock.prefetchedTransactions
 	if (lastBlock.baseFeePerGas != null) {
 		// https://eips.ethereum.org/EIPS/eip-1559
 		const gasTarget = lastBlock.gasLimit / ELASTICITY_MULTIPLIER
@@ -32,15 +43,44 @@ export async function getGasPriceRecommendations (provider: Provider, blockTag: 
 			expectedBaseFee -= baseFeeDelta
 		}
 
-		return speeds.map(({ name, baseFeeAddBps }) => ({
+		// calculate the tips
+		const tips = filterOutliers(txns.map(x => x.maxPriorityFeePerGas!).filter(x => x > 0))
+		return speeds.map(({ name, baseFeeAddBps }, i) => ({
 			name,
-			baseFeePerGas: expectedBaseFee + expectedBaseFee * baseFeeAddBps / 10000n
+			baseFeePerGas: expectedBaseFee + expectedBaseFee * baseFeeAddBps / 10000n,
+			maxPriorityFeePerGas: average(nthGroup(tips, i, speeds.length))
 		}))
 	} else {
-		const txns = lastBlock.prefetchedTransactions.filter(x => x.gasPrice > 0)
-		// console.log(txns.map(x => x.gasPrice))
-		// console.log(lastBlock)
+		const prices = filterOutliers(txns.map(x => x.gasPrice!).filter(x => x > 0))
+		return speeds.map(({ name }, i) => ({
+			name,
+			gasPrice: average(nthGroup(prices, i, speeds.length))
+		}))
 	}
+}
+
+// https://stackoverflow.com/questions/20811131/javascript-remove-outlier-from-an-array
+function filterOutliers (data: bigint[]): bigint[] {
+	// numeric sort, a - b doesn't work for bigint
+	data.sort((a, b) => a == b ? 0 : (a > b ? 1 : -1))
+	const q1 = data[Math.floor((data.length / 4))]
+	const endPosition = Math.ceil((data.length * (3 / 4)))
+	const q2 = data[endPosition < data.length ? endPosition : data.length - 1]
+	const iqr = q2 - q1
+	const maxValue = q2 + iqr * 15n / 10n
+	const minValue = q1 - iqr * 15n / 10n
+	const filteredValues = data.filter(x => x <= maxValue && x >= minValue)
+	return filteredValues
+}
+
+function nthGroup (data: bigint[], n: number, outOf: number): bigint[] {
+	const step = Math.floor(data.length / outOf)
+	const at = n * step
+	return data.slice(at, at + step)
+}
+
+function average (data: bigint[]): bigint {
+	return data.reduce((a, b) => a + b, 0n) / BigInt(data.length)
 }
 
 // temporary test
