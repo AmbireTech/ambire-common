a579cf2ffba1c3f4d9cc242d1447f51e0bbaea0b
Borislav Itskov <borislav@devlabs.bg>
2023-04-19 13:30:04 +0300

    remove hardhat and add waffle for testing

--- test/MultiSigTest.js
+++ test/MultiSigTest.js
@@ -1,13 +1,8 @@
-const { ethers } = require("hardhat")
 const { expect } = require("chai")
+const { ethers } = require("ethers")
 
 describe("Multi Sign Tests", function () {
-
-  function deployContract() {
-    return {}
-  }
-
   it("first test", async function () {
     expect(true).to.equal(true)
   })
-});
+})
