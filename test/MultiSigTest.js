72874936c900d7fb527023e0eb40d796bcf961f0
Borislav Itskov <borislav@devlabs.bg>
2023-04-19 08:30:27 +0300

    add: hardhat as a dev dependancy for testing and an optimizer to compile the contracts

---
+++ test/MultiSigTest.js
@@ -0,0 +1,13 @@
+const { ethers } = require("hardhat")
+const { expect } = require("chai")
+
+describe("Multi Sign Tests", function () {
+
+  function deployContract() {
+    return {}
+  }
+
+  it("first test", async function () {
+    expect(true).to.equal(true)
+  })
+});
