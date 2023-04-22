cb782e22ef36195032840d05ad61324d2b7a2eac
Ivo Georgiev <ivo@ambire.com>
2023-04-22 20:53:46 +0300

    batcher: allow coingecko style responses, fix a concurrency bug

--- v2/libs/portfolio/batcher.ts
+++ v2/libs/portfolio/batcher.ts
@@ -10,15 +10,21 @@ export default function batcher (fetch: Function, urlGenerator: (queue: any[]) =
 	async function resolveQueue() {
 		// Note: intentionally just using the first values in the queue
 		if (queue.length === 0) return
+		const queueCopy = queue
+		queue = []
 		try {
-			const url = urlGenerator(queue.map(x => x.data))
+			const url = urlGenerator(queueCopy.map(x => x.data))
 			const resp = await fetch(url)
 			const body = await resp.json()
-			if (!Array.isArray(body) || body.hasOwnProperty('message')) throw body
-			if (body.length !== queue.length) throw new Error('internal error: queue length and response length mismatch')
-			queue.forEach(({ resolve }, i) => resolve(body[i]))
-		} catch (e) { queue.forEach(({ reject }) => reject(e)) }
-		queue = []
+			if (body.hasOwnProperty('message')) throw body
+			if (body.hasOwnProperty('error')) throw body
+			if (Array.isArray(body)) {
+				if (body.length !== queueCopy.length) throw new Error('internal error: queue length and response length mismatch')
+				queueCopy.forEach(({ resolve }, i) => resolve(body[i]))
+			} else if (queueCopy.every(x => typeof x.data['responseIdentitier'] === 'string')) {
+				queueCopy.forEach(({ resolve, data }) => resolve(body[data.responseIdentifier as string]))
+			} else throw body
+		} catch (e) { queueCopy.forEach(({ reject }) => reject(e)) }
 	}
 	return async (data: any): Promise<any> => {
 		// always do the setTimeout - if it's a second or third batchedCall within a tick, all setTimeouts will fire but only the first will perform work
