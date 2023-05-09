8cf5fafa55af1c844b76acc3b9bd34a76f80d6d1
Ivo Georgiev <ivo@ambire.com>
2023-05-09 10:34:09 +0300

    gecko: add token override for stkAAVE

--- v2/libs/portfolio/gecko.ts
+++ v2/libs/portfolio/gecko.ts
@@ -24,6 +24,7 @@ export function geckoIdMapper (address: string, networkId: string): string | nul
 		optimism: 'ethereum',
 		// kucoin, gnosis, kc not added
 	})[networkId] || networkId
+	if (address === '0x4da27a545c0c5B758a6BA100e3a049001de870f5') return 'aave'
 	return null
 }
 
