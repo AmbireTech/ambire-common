654e0c174621327a28b15a0f591e437293acae2d
Ivo Georgiev <ivo@ambire.com>
2023-05-14 19:37:47 +0300

    portfolio: explain recommended behavior

--- v2/controllers/portfolio.ts
+++ v2/controllers/portfolio.ts
@@ -15,9 +15,14 @@ class PortfolioController {
 		this.pending = new Map()
 		// NOTE: we always pass in all `accounts` and `networks` to ensure that the user of this
 		// controller doesn't have to update this controller every time that those are updated
+
+		// The recommended behavior of the application that this API encourages is:
+		// 1) when the user selects an account, update it's portfolio on all networks: updateAccountOnAllNetworks
+		// 2) every time the user has a change in their pending (to be signed or to be mined) bundle(s) on a
+		// certain network, update this network only: updateAccountOnOneNetwork
 		/*
 		updateAccountOnAllNetworks(accounts, networks, accountId)
-		updateAccountOnOneNetworkWithSimulation(accounts, networks, accountId, networkId, simulationBundles)
+		updateAccountOnOneNetwork(accounts, networks, accountId, networkId, simulationBundles)
 		
 		*/
 	}
