66b5c3a20ada4af6d868359e5eb8177a74e2d404
Ivo Georgiev <ivo@ambire.com>
2023-05-14 19:00:39 +0300

    portfolio controller: add the state types

--- v2/controllers/portfolio.ts
+++ v2/controllers/portfolio.ts
@@ -1,8 +1,18 @@
 import { Portfolio } from '../libs/portfolio'
 import { Storage } from '../interfaces/storage'
 
+type networkId = string
+type accountId = string
+// @TODO fix the any
+type PortfolioState = Map<accountId, Map<networkId, any>>
+
 class PortfolioController {
+	latest: PortfolioState
+	pending: PortfolioState
+
 	constructor(storage: Storage) {
+		this.latest = new Map()
+		this.pending = new Map()
 		// NOTE: we always pass in all `accounts` and `networks` to ensure that the user of this
 		// controller doesn't have to update this controller every time that those are updated
 		/*
