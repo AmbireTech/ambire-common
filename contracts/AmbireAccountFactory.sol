618dca8afba551b414606bc6d2dd81577e908597
Ivo Georgiev <ivo@ambire.com>
2023-05-17 16:02:29 +0300

    contracts: AmbireAccountFactory: get rid of deployAndCall, it is obsolete

--- contracts/AmbireAccountFactory.sol
+++ contracts/AmbireAccountFactory.sol
@@ -12,11 +12,13 @@ contract AmbireAccountFactory {
 		allowedToDrain = allowed;
 	}
 
+	// @notice allows anyone to deploy any contracft with a specific code/salt
+	// this is safe because it's CREATE2 deployment
 	function deploy(bytes calldata code, uint256 salt) external {
 		deploySafe(code, salt);
 	}
 
-	// When the relayer needs to act upon an /identity/:addr/submit call, it'll either call execute on the AmbireAccount directly
+	// @notice when the relayer needs to act upon an /identity/:addr/submit call, it'll either call execute on the AmbireAccount directly
 	// if it's already deployed, or call `deployAndExecute` if the account is still counterfactual
 	// we can't have deployAndExecuteBySender, because the sender will be the factory
 	function deployAndExecute(
@@ -27,38 +29,6 @@ contract AmbireAccountFactory {
 	) external {
 		address payable addr = payable(deploySafe(code, salt));
 		AmbireAccount(addr).execute(txns, signature);
-	}
-
-	// but for the quick accounts we need this
-	function deployAndCall(bytes calldata code, uint256 salt, address callee, bytes calldata data) external {
-		deploySafe(code, salt);
-		require(data.length > 4, 'DATA_LEN');
-		bytes4 method;
-		// solium-disable-next-line security/no-inline-assembly
-		assembly {
-			// can also do shl(224, shr(224, calldataload(0)))
-			method := and(calldataload(data.offset), 0xffffffff00000000000000000000000000000000000000000000000000000000)
-		}
-		require(
-			method == 0x6171d1c9 || // execute((address,uint256,bytes)[],bytes)
-				method == 0x534255ff || // send(address,(uint256,address,address),(bool,bytes,bytes),(address,uint256,bytes)[])
-				method == 0x4b776c6d || // sendTransfer(address,(uint256,address,address),(bytes,bytes),(address,address,uint256,uint256))
-				method == 0x63486689, // sendTxns(address,(uint256,address,address),(bytes,bytes),(string,address,uint256,bytes)[])
-			'INVALID_METHOD'
-		);
-
-		assembly {
-			let dataPtr := mload(0x40)
-			calldatacopy(dataPtr, data.offset, data.length)
-			let result := call(gas(), callee, 0, dataPtr, data.length, 0, 0)
-
-			if eq(result, 0) {
-				let size := returndatasize()
-				let ptr := mload(0x40)
-				returndatacopy(ptr, 0, size)
-				revert(ptr, size)
-			}
-		}
 	}
 
 	// This method can be used to withdraw stuck tokens or airdrops
