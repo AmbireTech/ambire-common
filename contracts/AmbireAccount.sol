deaf7fcbf79bfb5ea99f9ba3e5c72f8f409edfb1
Ivo Georgiev <ivo@ambire.com>
2023-05-27 05:47:38 +0300

    AmbireAccount: add ERC721TokenReceiver to supportsInterface

--- contracts/AmbireAccount.sol
+++ contracts/AmbireAccount.sol
@@ -253,6 +253,7 @@ contract AmbireAccount {
 	function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
 		return
 			interfaceID == 0x01ffc9a7 || // ERC-165 support (i.e. `bytes4(keccak256('supportsInterface(bytes4)'))`).
+			interfaceID == 0x150b7a02 || // ERC721TokenReceiver
 			interfaceID == 0x4e2312e0; // ERC-1155 `ERC1155TokenReceiver` support (i.e. `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")) ^ bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`).
 	}
 }
