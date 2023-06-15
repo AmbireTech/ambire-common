fb9e3358bb94b77e6e8d761ecd7b2a7091a857f4
JIOjosBG <jjjooosssiiifff@gmail.com>
2023-06-15 16:22:16 +0300

    Formating and test structure

--- src/libs/emailVault/emailVault.test.ts
+++ src/libs/emailVault/emailVault.test.ts
@@ -2,14 +2,14 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable @typescript-eslint/no-useless-constructor */
 /* eslint-disable max-classes-per-file */
-import { describe, expect, test } from '@jest/globals'
+import { beforeAll, describe, expect, test } from '@jest/globals'
 import fetch from 'node-fetch'
 import { Wallet } from 'ethers'
 
 import { EmailVault } from './emailVault'
 import { requestMagicLink } from '../magicLink/magicLink'
 
-const email = `yosif+${Wallet.createRandom().address.slice(12, 20)}@ambire.com`
+let email: String
 
 // Relayer have to be start with NODE_ENV === 'testing' to can retrive the secret
 const relayerUrl = 'http://localhost:1934'
@@ -18,55 +18,53 @@ const emailVault = new EmailVault(fetch, relayerUrl)
 let authKey: String
 let authSecret: String // this will not be return in prod mode
 let recoveryKey: String
-let keyBackup:String  = JSON.stringify( {a:1} )
-const keyStoreSecret = "keyStoreSecretHere"
-describe('MagicLink', () => {
-  test('should return a key for session and relayer should be run in test mode', async () => {
-    const result = await requestMagicLink(email, relayerUrl, fetch)
-    authKey = result.key
-    authSecret = result.secret
-    expect(authKey).not.toBe('')
-    expect(authSecret).not.toBe('')
-  })
+const keyBackup: String = JSON.stringify({ a: 1 })
+const keyStoreSecret = 'keyStoreSecretHere'
+describe('Email vault', () => {
+  describe('positive tests', () => {
+    beforeAll(async () => {
+      email = `yosif+${Wallet.createRandom().address.slice(12, 20)}@ambire.com`
+      const result = await requestMagicLink(email, relayerUrl, fetch)
+      authKey = result.key
+      authSecret = result.secret
+      await fetch(`${relayerUrl}/email-vault/confirmationKey/${email}/${authKey}/${authSecret}`)
+    })
 
-  test('confirm magic link', async () => {
-    const resp = await fetch(
-      `${relayerUrl}/email-vault/confirmationKey/${email}/${authKey}/${authSecret}`
-    )
-    const { success, message } = await resp.json()
-    expect(success).toBeTruthy()
-    expect(message).toBe('email is confirmed')
-  })
+    test('create an email vault', async () => {
+      const { key, type, value } = await emailVault.create(email, authKey)
+      expect(key).not.toBe('')
+      expect(type).toBe('recoveryKey')
+      expect(value).toBeFalsy()
+    })
 
-  test('create an email vault', async () => {
-    const { key, type, value } = await emailVault.create(email, authKey)
-    expect(key).not.toBe('')
-    expect(type).toBe('recoveryKey')
-    expect(value).toBeFalsy()
-  })
+    test('get recoveryKey address', async () => {
+      const { key, type, value } = await emailVault.getRecoveryKeyAddress(email, authKey)
+      expect(key).not.toBe('')
+      expect(type).toBe('recoveryKey')
+      expect(value).toBeFalsy()
+      recoveryKey = key
+    })
 
-  test('get recoveryKey address', async () => {
-    const { key, type, value } = await emailVault.getRecoveryKeyAddress(email, authKey)
-    expect(key).not.toBe('')
-    expect(type).toBe('recoveryKey')
-    expect(value).toBeFalsy()
-    recoveryKey = key
-  })
+    test('add keyStoreSecret', async () => {
+      const success = await emailVault.addKeyStoreSecret(
+        email,
+        authKey,
+        recoveryKey,
+        keyStoreSecret
+      )
+      expect(success).toBeTruthy()
+    })
 
-  test('add keyStoreSecret', async () => {
-    const success = await emailVault.addKeyStoreSecret(email, authKey, recoveryKey, keyStoreSecret)
-    expect(success).toBeTruthy()
-  })
+    test('retrieve keyStoreSecret', async () => {
+      const res = await emailVault.retrieveKeyStoreSecret(email, authKey, recoveryKey)
+      expect(res.key).toBe(recoveryKey)
+      expect(res.value).toBe(keyStoreSecret)
+      expect(res.type).toBe('keyStore')
+    })
 
-  test('retrieve keyStoreSecret', async () => {
-    const res = await emailVault.retrieveKeyStoreSecret(email, authKey, recoveryKey)
-    expect(res.key).toBe(recoveryKey)
-    expect(res.value).toBe(keyStoreSecret)
-    expect(res.type).toBe('keyStore')
-  })
-
-  test('add keyBackup', async () => {
-    const success = await emailVault.addKeyBackup(email, authKey, recoveryKey, keyBackup)
-    expect(success).toBeTruthy()
+    test('add keyBackup', async () => {
+      const success = await emailVault.addKeyBackup(email, authKey, recoveryKey, keyBackup)
+      expect(success).toBeTruthy()
+    })
   })
 })
