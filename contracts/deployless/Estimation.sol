51c9d86e797ad90e84f24eb299200393ee0ba022
Ivo Georgiev <ivo@ambire.com>
2023-06-16 18:50:37 +0300

    deployless contracts: Estimation: implement simulateSigned

--- contracts/deployless/Estimation.sol
+++ contracts/deployless/Estimation.sol
@@ -4,14 +4,25 @@ pragma solidity 0.8.19;
 import "./IAmbireAccount.sol";
 
 contract Estimation {
-  struct DeploymentOutcome {
+  struct AccountOp {
+    IAmbireAccount account;
+    uint nonce;
+    IAmbireAccount.Transaction[] calls;
+    bytes signature;
+  }
+
+  // We do not care about nonces here, unlike portfolio simulations
+  // In portfolio simulations we may want to simulate multiple AccountOps and see what nonce we started with, to know where the executing node is
+  // Here, we only care about one particular AccountOp (and potentially accountOpToExecuteBefore for recovery finalization)
+  struct SimulationOutcome {
     uint gasUsed;
     bool success;
     bytes err;
   }
 
   struct EstimationOutcome {
-    DeploymentOutcome deployment;
+    SimulationOutcome deployment;
+    SimulationOutcome accountOpToExecuteBefore;
   }
 
   function makeSpoofSignature(address account) internal pure returns (bytes memory spoofSig) {
@@ -21,7 +32,7 @@ contract Estimation {
   function simulateDeployment(
     IAmbireAccount account,
     address factory, bytes memory factoryCalldata
-  ) public returns (DeploymentOutcome memory outcome) {
+  ) public returns (SimulationOutcome memory outcome) {
     uint gasInitial = gasleft();
     if (address(account).code.length == 0) {
       (outcome.success, outcome.err) = factory.call(factoryCalldata);
@@ -29,10 +40,17 @@ contract Estimation {
     outcome.gasUsed = gasInitial - gasleft();
   }
 
-  function simulateSigned() public {
-  }
-
-  function simulateNonSigned() public {
+  function simulateSigned(AccountOp memory op) public returns (SimulationOutcome memory outcome) {
+    if (op.nonce != op.account.nonce()) {
+      outcome.err = bytes("NONCE_ERROR");
+    }
+    uint gasInitial = gasleft();
+    try op.account.execute(op.calls, op.signature) {
+      outcome.success = true;
+    } catch (bytes memory err) {
+      outcome.err = err;
+    }
+    outcome.gasUsed = gasInitial - gasleft();
   }
 
   // @TODO simulateFeePayments
