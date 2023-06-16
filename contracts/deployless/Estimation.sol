6f877a652b7ff4191eeb9602db4e2e2dac8b89c3
Ivo Georgiev <ivo@ambire.com>
2023-06-16 18:34:54 +0300

    deployless contracts: Estimation: implement simulateDeployment

--- contracts/deployless/Estimation.sol
+++ contracts/deployless/Estimation.sol
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.19;
 
+import "./IAmbireAccount.sol";
+
 contract Estimation {
   function makeSpoofSignature(address account) internal pure returns (bytes memory spoofSig) {
     spoofSig = abi.encodePacked(uint256(uint160(account)), uint8(0x03));
   }
 
-  function simulateDeployment() {
+  function simulateDeployment(
+    IAmbireAccount account,
+    address factory, bytes memory factoryCalldata
+  ) public returns (uint gasUsed, bool success, bytes memory error) {
+    uint gasInitial = gasleft();
+    if (address(account).code.length == 0) {
+      (success, error) = factory.call(factoryCalldata);
+    }
+    gasUsed = gasInitial - gasleft();
   }
 
-  function simulateSigned() {
+  function simulateSigned() public {
   }
 
-  function simulateNonSigned() {
+  function simulateNonSigned() public {
   }
 
   // @TODO simulateFeePayments
@@ -20,4 +30,5 @@ contract Estimation {
   // @TODO simulateComplete that also returns gasPrice, nativeBalance
   // @TODO `estimate` takes the `accountOpToExecuteBefore` parameters separately because it's simulated via `simulateSigned`
   // vs the regular accountOp for which we use siimulateNonSigned
+
 }
