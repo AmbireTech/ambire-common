6caa819c4d32804765092f56247e00e055879f09
Ivo Georgiev <ivo@ambire.com>
2023-06-17 17:27:53 +0300

    lib: estimate: start working on it

---
+++ src/libs/estimate/estimate.ts
@@ -0,0 +1,17 @@
+import { Deployless, parseErr } from '../deployless/deployless'
+import { getAccountDeployParams } from '../account/account'
+import { NetworkDescriptor } from '../../interfaces/networkDescriptor'
+import { AccountOp, callToTuple } from '../accountOp/accountOp'
+import { Account } from '../../interfaces/account'
+import { Provider } from 'ethers'
+import estimator from './estimator.json'
+
+// @TODO return type
+export async function estimate(provider: Provider, network: NetworkDescriptor, op: AccountOp): Promise<any> {
+  // @TODO construrct Deployless instance
+  const deploylessEstimator = new Deployless(provider, estimator.abi, estimator.bin, network.rpcNoStateOverride ? undefined : estimator.binRuntime)
+}
+
+// @TODO test
+// transfer some random token from 0xa07D75aacEFd11b425AF7181958F0F85c312f143
+// USDT, USDC
