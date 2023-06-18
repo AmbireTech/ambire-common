ff0c2f59fb572e97470e91984356c11145883ec9
Ivo Georgiev <ivo@ambire.com>
2023-06-18 10:40:55 +0300

    Estimator: get rid of scheduledRecoveries, this is the job of a different contract

--- contracts/deployless/Estimation.sol
+++ contracts/deployless/Estimation.sol
@@ -37,7 +37,6 @@ contract Estimation {
     uint nonce;
     FeeTokenOutcome[] feeTokenOutcomes;
     bytes32[] associatedKeyPrivileges;
-    uint[] scheduledRecoveries;
     uint[] nativeAssetBalances;
   }
 
@@ -84,8 +83,6 @@ contract Estimation {
     if (feeTokens.length != 0) outcome.feeTokenOutcomes = simulateFeePayments(account, feeTokens, spoofSig, relayer);
 
     if (associatedKeys.length != 0) {
-      outcome.scheduledRecoveries = getScheduledRecoveries(op.account, associatedKeys);
-
       // Safety check: anti-bricking
       bool isOk;
       for (uint i=0; i!=associatedKeys.length; i++) {
@@ -140,32 +137,6 @@ contract Estimation {
       outcome.err = err;
     }
     outcome.gasUsed = gasInitial - gasleft();
-  }
-
-  function getScheduledRecoveries(IAmbireAccount account, address[] memory associatedKeys)
-    public
-    returns (uint[] memory scheduledRecoveries)
-  {
-    // Don't do this if we're not ambire v2
-    try this.ambireV2Check(account) {}
-    catch { return scheduledRecoveries; }
-
-    // Check if there's a pending recovery that sets any of the associatedKeys
-    scheduledRecoveries = new uint[](associatedKeys.length);
-    uint currentNonce = account.nonce();
-    for (uint i=0; i!=associatedKeys.length; i++) {
-      address key = associatedKeys[i];
-      IAmbireAccount.Transaction[] memory calls = new IAmbireAccount.Transaction[](1);
-      calls[0].to = address(account);
-      // @TODO the value of setAddrPrivilege is not necessarily 1 cause of the recovery
-      calls[0].data = abi.encodeWithSelector(IAmbireAccount.setAddrPrivilege.selector, key, bytes32(uint256(1)));
-      bytes32 hash = keccak256(abi.encode(address(account), block.chainid, currentNonce, calls));
-      scheduledRecoveries[i] = account.scheduledRecoveries(hash);
-    }
-  }
-
-  function ambireV2Check(IAmbireAccount account) external returns (uint) {
-    return account.scheduledRecoveries(bytes32(0));
   }
 
   function simulateFeePayments(IAmbireAccount account, address[] memory feeTokens, bytes memory spoofSig, address relayer)
