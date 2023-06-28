f68a60ed578ad443a368b3802407a569562674b0
JIOjosBG <jjjooosssiiifff@gmail.com>
2023-06-28 14:05:50 +0300

    Fix initial check

--- src/libs/relayerCall/relayerCall.ts
+++ src/libs/relayerCall/relayerCall.ts
@@ -6,7 +6,7 @@ export async function relayerCall(
   body: any = null,
   headers: any = null
 ): Promise<any> {
-  if (['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'].includes(method))
+  if (!['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'].includes(method))
     return { success: false, message: 'bad method' }
   if (!url) return { success: false, message: 'no url' }
   if (body && ['GET', 'DELETE', 'HEAD'].includes(method))
