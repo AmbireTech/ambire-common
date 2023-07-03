1db310078d0069bf8f6aae784af8473c85f3f3c3
Jordan Enev <jordantoen@gmail.com>
2023-07-03 20:04:09 +0300

    Add: Estimate lib -> calculates gasUsage, refunds, feeTokenOutcome, nativeAssetBalances. Add: Typescript support. Add: Tests.

---
+++ src/libs/estimate/estimate.test.ts
@@ -0,0 +1,149 @@
+import { JsonRpcProvider, AbiCoder } from 'ethers'
+import { describe, expect } from '@jest/globals'
+import fetch from 'node-fetch'
+import { estimate, EstimateResult } from './estimate'
+
+import { networks } from '../../consts/networks'
+import { Portfolio } from '../portfolio/portfolio'
+
+const ethereum = networks.find((x) => x.id === 'ethereum')
+if (!ethereum) throw new Error('no eth')
+const provider = new JsonRpcProvider(ethereum.rpcUrl)
+
+const account = {
+  addr: '0xa07D75aacEFd11b425AF7181958F0F85c312f143',
+  label: '',
+  pfp: '',
+  associatedKeys: ['0xd6e371526cdaeE04cd8AF225D42e37Bc14688D9E'],
+  creation: {
+    factoryAddr: '0xBf07a0Df119Ca234634588fbDb5625594E2a5BCA',
+    bytecode:
+      '0x7f28d4ea8f825adb036e9b306b2269570e63d2aa5bd10751437d98ed83551ba1cd7fa57498058891e98f45f8abb85dafbcd30f3d8b3ab586dfae2e0228bbb1de7018553d602d80604d3d3981f3363d3d373d3d3d363d732a2b85eb1054d6f0c6c2e37da05ed3e5fea684ef5af43d82803e903d91602b57fd5bf3',
+    salt: '0x0000000000000000000000000000000000000000000000000000000000000001'
+  }
+}
+const to = '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'
+
+const tomorrowHex = Math.floor((Date.now() + 86400000) / 1000).toString(16)
+// 64 chars expire hex
+// we set swap deadline always for tomorrow, in order to prevent the test failure with 'TRANSACTION TOO OLD'
+const expire = '0'.repeat(64 - tomorrowHex.length) + tomorrowHex
+
+// USDT -> USDC swap
+// Fee tokens: USDT, USDC
+const data = `0x5ae401dc${expire}00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e404e45aaf000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb4800000000000000000000000000000000000000000000000000000000000001f4000000000000000000000000a07d75aacefd11b425af7181958f0f85c312f14300000000000000000000000000000000000000000000000000000000000f424000000000000000000000000000000000000000000000000000000000000c33d9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`
+
+const SPOOF_SIGTYPE = '03'
+const spoofSig =
+  new AbiCoder().encode(['address'], ['0xd6e371526cdaeE04cd8AF225D42e37Bc14688D9E']) + SPOOF_SIGTYPE
+
+const nativeToCheck = [
+  '0x0000000000000000000000000000000000000001',
+  '0x942f9CE5D9a33a82F88D233AEb3292E680230348'
+]
+const feeTokens = [
+  '0x0000000000000000000000000000000000000000',
+  '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+  '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+]
+
+const portfolio = new Portfolio(fetch, provider, ethereum)
+
+describe('estimate', () => {
+  const checkBalance = (
+    responseTokens: EstimateResult['nativeAssetBalances'],
+    tokenAddresses: string[]
+  ) => {
+    tokenAddresses.forEach((tokenAddress) => {
+      expect(responseTokens.find((t) => t.address === tokenAddress)!.balance).toBeGreaterThan(0n)
+    })
+  }
+
+  it('estimates gasUsage, fee and native tokens outcome', async () => {
+    const op = {
+      accountAddr: account.addr,
+      signingKeyAddr: null,
+      gasLimit: null,
+      gasFeePayment: null,
+      networkId: 'ethereum',
+      nonce: null, // does not matter when estimating
+      signature: spoofSig,
+      calls: [{ to, value: BigInt(0), data }],
+      accountOpToExecuteBefore: null
+    }
+
+    const portfolioResponse = await portfolio.get('0xa07D75aacEFd11b425AF7181958F0F85c312f143')
+    const usdt = portfolioResponse.tokens.find(
+      (token) => token.address === '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    )
+    const usdc = portfolioResponse.tokens.find(
+      (token) => token.address === '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+    )
+
+    const response = await estimate(provider, ethereum, account, op, nativeToCheck, feeTokens)
+    const usdtOutcome = response.feeTokenOutcome.find(
+      (token) => token.address === '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    )
+    const usdcOutcome = response.feeTokenOutcome.find(
+      (token) => token.address === '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+    )
+
+    // This is the min gas unit we can spend, but we expect more than that having in mind that multiple computations happens in the Contract
+    expect(response.gasUsed).toBeGreaterThan(21000n)
+    // As we swap 1 USDT for 1 USDC, we expect the estimate (outcome) balance of USDC to be greater than before the estimate (portfolio value)
+    expect(usdcOutcome!.balance).toBeGreaterThan(usdc!.amount)
+    expect(usdtOutcome!.balance).toBeLessThan(usdt!.amount)
+    checkBalance(response.nativeAssetBalances, nativeToCheck)
+  })
+
+  it('estimates with `accountOpToExecuteBefore`', async () => {
+    const op = {
+      accountAddr: account.addr,
+      signingKeyAddr: null,
+      gasLimit: null,
+      gasFeePayment: null,
+      networkId: 'ethereum',
+      nonce: null, // does not matter when estimating
+      signature: spoofSig,
+      calls: [{ to, value: BigInt(0), data }],
+      accountOpToExecuteBefore: null
+    }
+
+    const opWithExecuteBefore = {
+      accountAddr: account.addr,
+      signingKeyAddr: null,
+      gasLimit: null,
+      gasFeePayment: null,
+      networkId: 'ethereum',
+      nonce: null, // does not matter when estimating
+      signature: spoofSig,
+      calls: [{ to, value: BigInt(0), data }],
+      accountOpToExecuteBefore: {
+        accountAddr: account.addr,
+        signingKeyAddr: null,
+        gasLimit: null,
+        gasFeePayment: null,
+        networkId: 'ethereum',
+        // @TODO - read it from Contract
+        nonce: 357,
+        signature: spoofSig,
+        calls: [{ to, value: BigInt(0), data }],
+        accountOpToExecuteBefore: null
+      }
+    }
+
+    const response = await estimate(provider, ethereum, account, op, nativeToCheck, feeTokens)
+    const responseWithExecuteBefore = await estimate(
+      provider,
+      ethereum,
+      account,
+      opWithExecuteBefore,
+      nativeToCheck,
+      feeTokens,
+      { calculateRefund: true }
+    )
+
+    // Gas used in case of `accountOpToExecuteBefore` should be greater, because more AccountOps are simulated
+    expect(responseWithExecuteBefore.gasUsed).toBeGreaterThan(response.gasUsed)
+  })
+})
--- src/libs/estimate/estimate.ts
+++ src/libs/estimate/estimate.ts
@@ -1,83 +1,119 @@
-import { fromDescriptor, parseErr } from '../deployless/deployless'
+import { Provider, JsonRpcProvider, Interface } from 'ethers'
+import { fromDescriptor } from '../deployless/deployless'
 import { getAccountDeployParams } from '../account/account'
 import { NetworkDescriptor } from '../../interfaces/networkDescriptor'
-import { AccountOp, callToTuple } from '../accountOp/accountOp'
+import { AccountOp } from '../accountOp/accountOp'
 import { Account } from '../../interfaces/account'
-import { Provider } from 'ethers'
 import estimator from './estimator.json'
+import { AmbireAccount, AmbireAccountFactory } from '../../../test/config'
 
-// @TODO return type
+export interface EstimateResult {
+  gasUsed: bigint
+  feeTokenOutcome: {
+    address: string
+    gasUsed: bigint
+    balance: bigint
+  }[]
+  nativeAssetBalances: {
+    address: string
+    balance: bigint
+  }[]
+}
+
 export async function estimate(
-  provider: Provider,
+  provider: Provider | JsonRpcProvider,
   network: NetworkDescriptor,
-  acc: Account,
+  account: Account,
   op: AccountOp,
+  nativeToCheck: string[],
+  feeTokens: string[],
+  opts?: {
+    calculateRefund?: boolean
+  },
+  blockFrom: string = '0x0000000000000000000000000000000000000001',
   blockTag: string | number = 'latest'
-): Promise<any> {
-  //@ TODO implement EOAs
-  if (!acc.creation) throw new Error('EOA not supported yet')
+): Promise<EstimateResult> {
+  // @ TODO implement EOAs
+  if (!account.creation) throw new Error('EOA not supported yet')
   const deploylessEstimator = fromDescriptor(provider, estimator, !network.rpcNoStateOverride)
 
-  // @TODO this is temp
-  const nativeToCheck = ['0x0000000000000000000000000000000000000001', '0x942f9CE5D9a33a82F88D233AEb3292E680230348']
-  const feeTokens: string[]= [
-    '0x0000000000000000000000000000000000000000',
-    '0xdAC17F958D2ee523a2206206994597C13D831ec7', '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-  ]
+  // @TODO - .env or passed as parameter?
+  const relayerAddress = '0x942f9CE5D9a33a82F88D233AEb3292E680230348'
+
   const args = [
     account.addr,
     ...getAccountDeployParams(account),
     // @TODO can pass 0 here for the addr
-    // @TODO op.accountOpToExecuteBefore
-    [account.addr, 0n, [], '0x'],
-    [account.addr, op.nonce || 0, op.calls, '0x'],
+    [
+      account.addr,
+      op.accountOpToExecuteBefore?.nonce || 0,
+      op.accountOpToExecuteBefore?.calls || [],
+      op.accountOpToExecuteBefore?.signature || '0x'
+    ],
+    [account.addr, op.nonce || 1, op.calls, '0x'],
     account.associatedKeys,
-
     feeTokens,
-    // @TODO
-    '0x942f9CE5D9a33a82F88D233AEb3292E680230348',
+    relayerAddress,
     nativeToCheck
   ]
-  const [ estimationResult ] = await deploylessEstimator.call('estimate', args, { from: '0x0000000000000000000000000000000000000001', blockTag })
-  return estimationResult
-}
 
-// @TODO test
-// USDT -> USDC swap
-// Fee tokens: USDT, USDC
-import { networks } from '../../consts/networks'
-import { JsonRpcProvider } from 'ethers'
-const ethereum = networks.find(x => x.id === 'ethereum')
-if (!ethereum) throw new Error('no eth')
-const provider = new JsonRpcProvider(ethereum.rpcUrl)
+  // eslint-disable-next-line prefer-const
+  let [[, , , , feeTokenOutcomes, , nativeAssetBalances, gasUsed]] = await deploylessEstimator.call(
+    'estimate',
+    args,
+    {
+      from: blockFrom,
+      blockTag
+    }
+  )
 
-const account = {
-  addr: '0xa07D75aacEFd11b425AF7181958F0F85c312f143',
-  label: '',
-  pfp: '',
-  associatedKeys: ['0xd6e371526cdaeE04cd8AF225D42e37Bc14688D9E'],
-  creation: {
-    factoryAddr: '0xBf07a0Df119Ca234634588fbDb5625594E2a5BCA',
-    bytecode: '0x7f28d4ea8f825adb036e9b306b2269570e63d2aa5bd10751437d98ed83551ba1cd7fa57498058891e98f45f8abb85dafbcd30f3d8b3ab586dfae2e0228bbb1de7018553d602d80604d3d3981f3363d3d373d3d3d363d732a2b85eb1054d6f0c6c2e37da05ed3e5fea684ef5af43d82803e903d91602b57fd5bf3',
-    salt: '0x0000000000000000000000000000000000000000000000000000000000000001'
+  if (opts?.calculateRefund) {
+    const IAmbireAccount = new Interface(AmbireAccount.abi)
+    const IAmbireAccountFactory = new Interface(AmbireAccountFactory.abi)
+
+    const accountCalldata = op.accountOpToExecuteBefore
+      ? IAmbireAccount.encodeFunctionData('executeMultiple', [
+          [
+            [op.accountOpToExecuteBefore.calls, op.accountOpToExecuteBefore.signature],
+            [op.calls, op.signature]
+          ]
+        ])
+      : IAmbireAccount.encodeFunctionData('execute', [op.calls, op.signature])
+
+    const factoryCalldata = IAmbireAccountFactory.encodeFunctionData('deployAndExecute', [
+      account.creation.bytecode,
+      account.creation.salt,
+      [[account.addr, 0, accountCalldata]],
+      op.signature
+    ])
+
+    const estimatedGas = await provider.estimateGas({
+      from: '0x0000000000000000000000000000000000000001',
+      to: account.creation.factoryAddr,
+      data: factoryCalldata
+    })
+
+    const estimatedRefund = gasUsed - estimatedGas
+
+    // As of EIP-3529, the max refund is 1/5th of the entire cost
+    // @TODO - in case of accountOpToExecuteBefore, estimatedRefund is a negative number.
+    // 1. Check data we pass for accountOpToExecuteBefore (maybe it's wrong)
+    // 2. Is there any possibility for such negative case? How do will handle it?
+    if (estimatedRefund <= gasUsed / 5n && estimatedRefund > 0n) gasUsed = estimatedGas
+  }
+
+  // @TODO - addedNative
+
+  return {
+    gasUsed,
+    feeTokenOutcome: feeTokenOutcomes.map((token: any, key: number) => ({
+      address: feeTokens[key],
+      gasUsed: token.gasUsed,
+      balance: token.amount
+    })),
+    nativeAssetBalances: nativeAssetBalances.map((balance: bigint, key: number) => ({
+      address: nativeToCheck[key],
+      balance
+    }))
   }
 }
-const to = '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'
-// Expired data
-// const data = '0x5ae401dc00000000000000000000000000000000000000000000000000000000648eaffb00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e404e45aaf000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000000000000000000000000000000000000000000064000000000000000000000000a07d75aacefd11b425af7181958f0f85c312f1430000000000000000000000000000000000000000000000000000000005f5e1000000000000000000000000000000000000000000000000000000000005e587c3000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-const data = '0x5ae401dc00000000000000000000000000000000000000000000000000000000648f9f0b00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e404e45aaf000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000000000000000000000000000000000000000000064000000000000000000000000a07d75aacefd11b425af7181958f0f85c312f1430000000000000000000000000000000000000000000000000000000005f5e1000000000000000000000000000000000000000000000000000000000005e5d981000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-const op = {
-  accountAddr: account.addr,
-  signingKeyAddr: null,
-  gasLimit: null,
-  gasFeePayment: null,
-  networkId: 'ethereum',
-  nonce: null, // does not matter when estimating
-  signature: null,
-  calls: [{ to, value: BigInt(0), data }],
-  accountOpToExecuteBefore: null
-}
-
-estimate(provider, ethereum, account, op)
-  .then(console.log)
-  .catch(e => console.error('caught', e))
