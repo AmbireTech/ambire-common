285dc28f4222080ffa3d7284330fe36ac85826c0
Jordan Enev <jordantoen@gmail.com>
2023-07-31 18:31:40 +0300

    Add: Pagination and filtration for AccountsOps and SignedMessages in ActivityController.

--- src/controllers/activity/activity.test.ts
+++ src/controllers/activity/activity.test.ts
@@ -1,44 +1,16 @@
 import { describe, expect } from '@jest/globals'
-import { Storage } from '../../interfaces/storage'
 import { AccountOp } from '../../libs/accountOp/accountOp'
 import { ActivityController } from './activity'
-import {SignedMessage} from "../../interfaces/userRequest";
-
-// @TODO: Reuse BigInt JSON lib, once merged: https://github.com/AmbireTech/ambire-common/pull/286
-export function stringify(obj: any): string {
-  return JSON.stringify(obj, (key, value) => {
-    return typeof value === 'bigint' ? { $bigint: value.toString() } : value
-  })
-}
-export function parse(json: string) {
-  return JSON.parse(json, (key, value) => {
-    if (value?.$bigint) {
-      return BigInt(value.$bigint)
-    }
-
-    return value
-  })
-}
-
-// @TODO: maybe this should be shared with the rest of the tests?
-export function produceMemoryStore(): Storage {
-  const storage = new Map()
-  return {
-    get: (key, defaultValue): any => {
-      const serialized = storage.get(key)
-      return Promise.resolve(serialized ? parse(serialized) : defaultValue)
-    },
-    set: (key, value) => {
-      storage.set(key, stringify(value))
-      return Promise.resolve(null)
-    }
-  }
-}
+import { SignedMessage } from '../../interfaces/userRequest'
+import { produceMemoryStore } from '../../../test/helpers'
 
 describe('Activity Controller ', () => {
-  test('AccountsOps are persisted in the storage', async () => {
+  test('AccountsOps - retrieved from Controller and persisted in Storage', async () => {
     const storage = produceMemoryStore()
-    const controller = new ActivityController(storage)
+    const controller = new ActivityController(storage, {
+      account: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      network: 'ethereum'
+    })
 
     const accountOp = {
       accountAddr: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
@@ -58,16 +30,140 @@ describe('Activity Controller ', () => {
     } as AccountOp
 
     await controller.addAccountOp(accountOp)
-    const storageActivity = await controller.getAccountsOps()
+    const controllerAccountsOps = controller.accountsOps
+    const storageAccountsOps = await storage.get('accountsOps', {})
 
-    expect(storageActivity['0xB674F3fd5F43464dB0448a57529eAF37F04cceA5:ethereum']).toEqual([
+    expect(controllerAccountsOps).toEqual({
+      items: [accountOp],
+      itemsTotal: 1,
+      currentPage: 0,
+      maxPages: 1
+    })
+    expect(storageAccountsOps['0xB674F3fd5F43464dB0448a57529eAF37F04cceA5'].ethereum).toEqual([
       accountOp
     ])
   })
 
-  test('SignedMessages are persisted in the storage', async () => {
+  test('AccountsOps - pagination and filtration handled correctly', async () => {
     const storage = produceMemoryStore()
-    const controller = new ActivityController(storage)
+    const controller = new ActivityController(storage, {
+      account: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      network: 'ethereum'
+    })
+
+    const accountsOps = [
+      {
+        accountAddr: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+        signingKeyAddr: '0x5Be214147EA1AE3653f289E17fE7Dc17A73AD175',
+        gasLimit: null,
+        gasFeePayment: null,
+        networkId: 'ethereum',
+        nonce: 225,
+        signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503',
+        calls: [
+          {
+            to: '0x18Ce9CF7156584CDffad05003410C3633EFD1ad0',
+            value: BigInt(0),
+            data: '0x23b872dd000000000000000000000000b674f3fd5f43464db0448a57529eaf37f04ccea500000000000000000000000077777777789a8bbee6c64381e5e89e501fb0e4c80000000000000000000000000000000000000000000000000000000000000089'
+          }
+        ]
+      },
+      {
+        accountAddr: '0x40b38765696e3d5d8d9d834d8aad4bb6e418e489',
+        signingKeyAddr: '0x5Be214147EA1AE3653f289E17fE7Dc17A73AD175',
+        gasLimit: null,
+        gasFeePayment: null,
+        networkId: 'ethereum',
+        nonce: 225,
+        signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503',
+        calls: [
+          {
+            to: '0x18Ce9CF7156584CDffad05003410C3633EFD1ad0',
+            value: BigInt(0),
+            data: '0x23b872dd000000000000000000000000b674f3fd5f43464db0448a57529eaf37f04ccea500000000000000000000000077777777789a8bbee6c64381e5e89e501fb0e4c80000000000000000000000000000000000000000000000000000000000000089'
+          }
+        ]
+      },
+      {
+        accountAddr: '0x40b38765696e3d5d8d9d834d8aad4bb6e418e489',
+        signingKeyAddr: '0x5Be214147EA1AE3653f289E17fE7Dc17A73AD175',
+        gasLimit: null,
+        gasFeePayment: null,
+        networkId: 'optimism',
+        nonce: 225,
+        signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503',
+        calls: [
+          {
+            to: '0x18Ce9CF7156584CDffad05003410C3633EFD1ad0',
+            value: BigInt(0),
+            data: '0x23b872dd000000000000000000000000b674f3fd5f43464db0448a57529eaf37f04ccea500000000000000000000000077777777789a8bbee6c64381e5e89e501fb0e4c80000000000000000000000000000000000000000000000000000000000000089'
+          }
+        ]
+      },
+      {
+        accountAddr: '0x40b38765696e3d5d8d9d834d8aad4bb6e418e489',
+        signingKeyAddr: '0x5Be214147EA1AE3653f289E17fE7Dc17A73AD175',
+        gasLimit: null,
+        gasFeePayment: null,
+        networkId: 'optimism',
+        nonce: 225,
+        signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503',
+        calls: [
+          {
+            to: '0x18Ce9CF7156584CDffad05003410C3633EFD1ad0',
+            value: BigInt(0),
+            data: '0x23b872dd000000000000000000000000b674f3fd5f43464db0448a57529eaf37f04ccea500000000000000000000000077777777789a8bbee6c64381e5e89e501fb0e4c80000000000000000000000000000000000000000000000000000000000000089'
+          }
+        ]
+      }
+    ] as AccountOp[]
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const accountOp of accountsOps) {
+      // eslint-disable-next-line no-await-in-loop
+      await controller.addAccountOp(accountOp)
+    }
+
+    // For the following criteria, we have 2 matching AccountsOps, these will be paginated on 2 pages (1 AccountOp per page)
+    await controller.setAccountsOpsPagination({ fromPage: 1, itemsPerPage: 1 })
+    await controller.setFilters({
+      account: '0x40b38765696e3d5d8d9d834d8aad4bb6e418e489',
+      network: 'optimism'
+    })
+
+    const controllerAccountsOps = controller.accountsOps
+
+    expect(controllerAccountsOps).toEqual({
+      items: [
+        {
+          accountAddr: '0x40b38765696e3d5d8d9d834d8aad4bb6e418e489',
+          signingKeyAddr: '0x5Be214147EA1AE3653f289E17fE7Dc17A73AD175',
+          gasLimit: null,
+          gasFeePayment: null,
+          networkId: 'optimism',
+          nonce: 225,
+          signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503',
+          calls: [
+            {
+              to: '0x18Ce9CF7156584CDffad05003410C3633EFD1ad0',
+              value: BigInt(0),
+              data: '0x23b872dd000000000000000000000000b674f3fd5f43464db0448a57529eaf37f04ccea500000000000000000000000077777777789a8bbee6c64381e5e89e501fb0e4c80000000000000000000000000000000000000000000000000000000000000089'
+            }
+          ]
+        }
+      ],
+      itemsTotal: 2,
+      currentPage: 1, // index based
+      maxPages: 2
+    })
+  })
+
+  test('SignedMessages - retrieved from Controller and persisted in Storage', async () => {
+    const storage = produceMemoryStore()
+    const controller = new ActivityController(storage, {
+      account: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      network: 'ethereum'
+    })
 
     const signedMessage: SignedMessage = {
       content: {
@@ -78,11 +174,85 @@ describe('Activity Controller ', () => {
       signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503'
     }
 
-    await controller.addSignedMessage(signedMessage, '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5')
-    const storageActivity = await controller.getSignedMessages()
+    await controller.addSignedMessage(
+      signedMessage,
+      '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      'ethereum'
+    )
+    const controllerSignedMessages = controller.signedMessages
+    const storageSignedMessages = await storage.get('signedMessages', {})
 
-    expect(storageActivity['0xB674F3fd5F43464dB0448a57529eAF37F04cceA5']).toEqual([
+    expect(controllerSignedMessages).toEqual({
+      items: [signedMessage],
+      itemsTotal: 1,
+      currentPage: 0,
+      maxPages: 1
+    })
+    expect(storageSignedMessages['0xB674F3fd5F43464dB0448a57529eAF37F04cceA5'].ethereum).toEqual([
       signedMessage
     ])
+  })
+
+  test('SignedMessages - pagination and filtration handled correctly', async () => {
+    const storage = produceMemoryStore()
+    const controller = new ActivityController(storage, {
+      account: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      network: 'ethereum'
+    })
+
+    const signedMessage: SignedMessage = {
+      content: {
+        kind: 'message',
+        message: '0x74657374'
+      },
+      fromUserRequestId: 1n,
+      signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503'
+    }
+
+    const expectedSignedMessage: SignedMessage = {
+      content: {
+        kind: 'message',
+        message: '0x123456'
+      },
+      fromUserRequestId: 1n,
+      signature: '0x0000000000000000000000005be214147ea1ae3653f289e17fe7dc17a73ad17503'
+    }
+
+    await controller.addSignedMessage(
+      signedMessage,
+      '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      'ethereum'
+    )
+    await controller.addSignedMessage(
+      signedMessage,
+      '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      'ethereum'
+    )
+    await controller.addSignedMessage(
+      signedMessage,
+      '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      'optimism'
+    )
+    await controller.addSignedMessage(
+      expectedSignedMessage,
+      '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      'optimism'
+    )
+
+    // For the following criteria, we have 2 matching SignedMessages, these will be paginated on 2 pages (1 SignedMessage per page)
+    await controller.setSignedMessagesPagination({ fromPage: 1, itemsPerPage: 1 })
+    await controller.setFilters({
+      account: '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5',
+      network: 'optimism'
+    })
+
+    const controllerSignedMessages = controller.signedMessages
+
+    expect(controllerSignedMessages).toEqual({
+      items: [expectedSignedMessage],
+      itemsTotal: 2,
+      currentPage: 1, // index based
+      maxPages: 2
+    })
   })
 })
--- src/controllers/activity/activity.ts
+++ src/controllers/activity/activity.ts
@@ -3,50 +3,163 @@ import { Storage } from '../../interfaces/storage'
 import { AccountOp } from '../../libs/accountOp/accountOp'
 import { SignedMessage } from '../../interfaces/userRequest'
 
+interface Pagination {
+  fromPage: number
+  itemsPerPage: number
+}
+
+interface PaginationResult {
+  items: any[]
+  itemsTotal: number
+  currentPage: number
+  maxPages: number
+}
+
+interface AccountsOps extends PaginationResult {
+  items: AccountOp[]
+}
+interface SignedMessages extends PaginationResult {
+  items: SignedMessage[]
+}
+
+interface Filters {
+  account: string
+  network: string
+}
+
+interface InternalAccountsOps {
+  // account => network => AccountOp[]
+  [key: string]: { [key: string]: AccountOp[] }
+}
+
+interface InternalSignedMessages {
+  // account => network => SignedMessage[]
+  [key: string]: { [key: string]: SignedMessage[] }
+}
+
 export class ActivityController extends EventEmitter {
   private storage: Storage
 
-  constructor(storage: Storage) {
+  private initialLoadPromise: Promise<void>
+
+  #accountsOps: InternalAccountsOps = {}
+
+  accountsOps: AccountsOps | undefined
+
+  #signedMessages: InternalSignedMessages = {}
+
+  signedMessages: SignedMessages | undefined
+
+  accountsOpsPagination: Pagination = {
+    fromPage: 0,
+    itemsPerPage: 10
+  }
+
+  signedMessagesPagination: Pagination = {
+    fromPage: 0,
+    itemsPerPage: 10
+  }
+
+  filters: Filters
+
+  constructor(storage: Storage, filters: Filters) {
     super()
     this.storage = storage
+    this.filters = filters
+    this.initialLoadPromise = this.load()
+  }
+
+  private async load(): Promise<void> {
+    const [accountsOps, signedMessages] = await Promise.all([
+      this.storage.get('accountsOps', {}),
+      this.storage.get('signedMessages', {})
+    ])
+
+    this.#accountsOps = accountsOps
+    this.#signedMessages = signedMessages
+
+    this.accountsOps = this.filterAndPaginate(this.#accountsOps, this.accountsOpsPagination)
+    this.signedMessages = this.filterAndPaginate(
+      this.#signedMessages,
+      this.signedMessagesPagination
+    )
+
+    this.emitUpdate()
+  }
+
+  private filterAndPaginate(
+    items: InternalAccountsOps | InternalSignedMessages,
+    pagination: Pagination
+  ): PaginationResult {
+    const filteredItems = items?.[this.filters.account]?.[this.filters.network] || []
+    const { fromPage, itemsPerPage } = pagination
+
+    return {
+      items: filteredItems.slice(fromPage * itemsPerPage, fromPage * itemsPerPage + itemsPerPage),
+      itemsTotal: filteredItems.length,
+      currentPage: fromPage, // zero/index based
+      maxPages: Math.ceil(filteredItems.length / itemsPerPage)
+    }
   }
 
   // @TODO: Implement AccountOp status - pending | confirmed | failed
   async addAccountOp(accountOp: AccountOp) {
-    const accountsOps = await this.storage.get('accountsOps', {})
+    await this.initialLoadPromise
 
     const account = accountOp.accountAddr
     const network = accountOp.networkId
-    const key = `${account}:${network}`
 
-    if (!accountsOps[key]) {
-      accountsOps[key] = []
-    }
+    if (!this.#accountsOps[account]) this.#accountsOps[account] = {}
+    if (!this.#accountsOps[account][network]) this.#accountsOps[account][network] = []
 
-    accountsOps[key].push(accountOp)
+    this.#accountsOps[account][network].push(accountOp)
+    this.accountsOps = this.filterAndPaginate(this.#accountsOps, this.accountsOpsPagination)
 
-    await this.storage.set('accountsOps', accountsOps)
+    await this.storage.set('accountsOps', this.#accountsOps)
     this.emitUpdate()
   }
 
-  async getAccountsOps() {
-    return this.storage.get('accountsOps', {})
-  }
+  async addSignedMessage(signedMessage: SignedMessage, account: string, network: string) {
+    await this.initialLoadPromise
 
-  async addSignedMessage(signedMessage: SignedMessage, accountAddr: string) {
-    const signedMessages = await this.storage.get('signedMessages', {})
+    if (!this.#signedMessages[account]) this.#signedMessages[account] = {}
+    if (!this.#signedMessages[account][network]) this.#signedMessages[account][network] = []
 
-    if (!signedMessages[accountAddr]) {
-      signedMessages[accountAddr] = []
-    }
+    this.#signedMessages[account][network].push(signedMessage)
+    this.signedMessages = this.filterAndPaginate(
+      this.#signedMessages,
+      this.signedMessagesPagination
+    )
 
-    signedMessages[accountAddr].push(signedMessage)
-
-    await this.storage.set('signedMessages', signedMessages)
+    await this.storage.set('signedMessages', this.#signedMessages)
     this.emitUpdate()
   }
 
-  async getSignedMessages() {
-    return this.storage.get('signedMessages', {})
+  setFilters(filters: Filters): void {
+    this.filters = filters
+    this.accountsOps = this.filterAndPaginate(this.#accountsOps, this.accountsOpsPagination)
+    this.signedMessages = this.filterAndPaginate(
+      this.#signedMessages,
+      this.signedMessagesPagination
+    )
+
+    this.emitUpdate()
+  }
+
+  setAccountsOpsPagination(pagination: Pagination): void {
+    this.accountsOpsPagination = pagination
+
+    this.accountsOps = this.filterAndPaginate(this.#accountsOps, this.accountsOpsPagination)
+    this.emitUpdate()
+  }
+
+  setSignedMessagesPagination(pagination: Pagination): void {
+    this.signedMessagesPagination = pagination
+
+    this.signedMessages = this.filterAndPaginate(
+      this.#signedMessages,
+      this.signedMessagesPagination
+    )
+    this.emitUpdate()
   }
 }
