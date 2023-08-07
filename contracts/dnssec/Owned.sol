d74a58108377c75a3c9210f69ffcd9fc1d495948
Borislav Itskov <borislav@devlabs.bg>
2023-08-07 11:37:00 +0300

    add: dkim and dnssec contract

---
+++ contracts/dnssec/Owned.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/**
+ * @dev Contract mixin for 'owned' contracts.
+ */
+contract Owned {
+    address public owner;
+
+    modifier owner_only() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setOwner(address newOwner) public owner_only {
+        owner = newOwner;
+    }
+}
