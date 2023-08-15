482189b3ba010e6adacfb80ca74ab7eec165f151
Yosif Hamed <josif.hameed@gmail.com>
2023-08-15 15:28:20 +0300

    Fix eslint err

--- src/libs/humanizer/humanizerUniswap.test.ts
+++ src/libs/humanizer/humanizerUniswap.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals'
+import { describe, test } from '@jest/globals'
 
 import { AccountOp } from '../accountOp/accountOp'
 import { callsToIr, initHumanizerMeta } from './humanizer'
