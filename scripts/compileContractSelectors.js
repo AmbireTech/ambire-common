68f34d5fcca7937d7b9d2dab0a970c600e83bf22
Yosif Hamed <josif.hameed@gmail.com>
2023-08-15 13:28:54 +0300

    Add func selectors from abis

--- scripts/compileContractSelectors.js
+++ scripts/compileContractSelectors.js
@@ -1,5 +1,8 @@
 /* eslint-disable prettier/prettier */
+const ethers = require('ethers')
 
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+// const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
 const fsPromises = require('fs').promises
 
 const fs = require('fs')
@@ -8,7 +11,9 @@ const path = require('path')
 
 const directoryPath = path.join(__dirname, '..', 'contracts', 'dappAddressList')
 let allAddresses = {}
-const getAllData = async (dirPath) => {
+
+
+const getAllDataFromFolder = async (dirPath) => {
   const networkFolders = fs.readdirSync(dirPath)
 	await Promise.all(networkFolders.map(async (folder)=>{
 		const files = fs.readdirSync(path.join(dirPath, folder))
@@ -20,11 +25,38 @@ const getAllData = async (dirPath) => {
   return allAddresses
 }
 
-
+const getFnName = (f)=>{
+	const args = f.inputs.map(i=>i.type).join(',')
+	return `${f.name}(${args})`
+}
 const main  = async () => {
-	const data = await getAllData(directoryPath)
-	const writeTo = path.join(__dirname, '..', 'contracts', 'dappAddressList.json')
-	await fsPromises.writeFile(writeTo, JSON.stringify(data, null, 4), 'utf8')
+	const abiPath = path.join(__dirname, '..', 'contracts', 'dappAbiList.json')
+	const selectorsPath = path.join(__dirname, '..', 'src', 'consts', 'dappSelectors.json')
+	const abis = JSON.parse(await fsPromises.readFile(abiPath, 'utf-8'))
+	const interfaces = Object.keys(abis).map((address) => {
+		try {
+
+			const iface = new ethers.Interface(abis[address])
+			// console.log(iface)
+			return iface
+		} catch (e){
+			return null
+		}
+		// return abis[address].filter(i=>i.type === 'function')
+	}).filter((i)=>i)
+	const entries = []
+	interfaces.forEach((i)=>{
+		i.fragments.forEach((f)=>{
+			if (f.type === 'function') entries.push([`funcSelectors:${f.selector}`, getFnName(f)])
+		})
+	})
+	const res = Object.fromEntries(entries)
+	await fsPromises.writeFile(selectorsPath, JSON.stringify(res, null, 4), 'utf8')
+	// console.log(Object.keys(abis).length)
+	// console.log(interfaces[0].fragments[0].selector)
+	// console.log(interfaces[0].fragments[0])
+	// console.log(interfaces[0].fragments[interfaces[0].fragments.length - 1])
+	// @TODO convert abi's function to selectors
 }
 
 main()
