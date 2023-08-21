ebb26e30464e091419eeb183528a3c139c82209d
Borislav Itskov <borislav@devlabs.bg>
2023-08-21 09:39:19 +0300

    change: AmbireAccountState checkes for 0x0a417632 to recognized whether the account is v2 or not

--- src/consts/deploy.ts
+++ src/consts/deploy.ts
@@ -1,10 +1,3 @@
-// NOTE<Bobby>: This is an address I deployed with my PK and nonce.
-// Currently, it's live only on Polygon.
-// The address needs to be the same on each chain and deployed with nonce 0.
-// Once that is done, please change the PROXY_AMBIRE_ACCOUNT to the correct
-// one and delete this NOTE.
-// Currently, we use this proxy for tests.
+// TO DO: redeploy with the supportsInterface 0x0a417632 identifier
 export const PROXY_AMBIRE_ACCOUNT = '0xfF69afDE895B381eE71e17C60350aE4c70b16a92'
-
-// These's a decent chance the factory address is the permanent one.
 export const AMBIRE_ACCOUNT_FACTORY = '0xA3A22Bf212C03ce55eE7C3845D4c177a6fEC418B'
