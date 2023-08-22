49835a66a774625bd2598be007d420b861983e85
Kaloyan Kosev <superkalo@devlabs.bg>
2023-08-22 12:17:21 +0300

    Fix: init the account adder and use the new ambire factory

--- src/controllers/main/main.test.ts
+++ src/controllers/main/main.test.ts
@@ -1,11 +1,19 @@
+import { ethers } from 'ethers'
 import fetch from 'node-fetch'
 
 import { describe, expect, test } from '@jest/globals'
 
 import { produceMemoryStore } from '../../../test/helpers'
+import { AMBIRE_ACCOUNT_FACTORY } from '../../consts/deploy'
+import { networks } from '../../consts/networks'
 import { UserRequest } from '../../interfaces/userRequest'
 import { KeyIterator } from '../../libs/keyIterator/keyIterator'
+import { getBytecode } from '../../libs/proxyDeploy/bytecode'
+import { getAmbireAccountAddress } from '../../libs/proxyDeploy/getAmbireAddressTwo'
 import { MainController } from './main'
+
+const polygon = networks.find((x) => x.id === 'polygon')
+if (!polygon) throw new Error('unable to find polygon network in consts')
 
 describe('Main Controller ', () => {
   const accounts = [
@@ -109,38 +117,44 @@ describe('Main Controller ', () => {
     // console.log('isUnlock ==>', controller.isUnlock())
   })
 
-  test('should add smart accounts', (done) => {
+  test('should add smart accounts', async () => {
     controller = new MainController(storage, fetch, relayerUrl)
 
+    const signerAddr = '0xB674F3fd5F43464dB0448a57529eAF37F04cceA5'
+    const priv = { addr: signerAddr, hash: true }
+
+    // Same as `accountNotDeployed` in accountState.test.ts
+    const bytecode = await getBytecode(polygon, [priv])
     const accountPendingCreation = {
-      addr: '0x043d40D94c42Ba245d71C4ee68083e1a65c04777',
-      label: '',
-      pfp: '',
-      associatedKeys: ['0xa07D75aacEFd11b425AF7181958F0F85c312f143'],
+      addr: getAmbireAccountAddress(AMBIRE_ACCOUNT_FACTORY, bytecode),
+      label: 'test account',
+      pfp: 'pfp',
+      associatedKeys: [signerAddr],
       creation: {
-        factoryAddr: '0xF9c2504741f0116f7aff6015b6E210058A8Ac1e4',
-        bytecode:
-          '0x7f28d4ea8f825adb036e9b306b2269570e63d2aa5bd10751437d98ed83551ba1cd7fa57498058891e98f45f8abb85dafbcd30f3d8b3ab586dfae2e0228bbb1de7018553d602d80604d3d3981f3363d3d373d3d3d363d732a2b85eb1054d6f0c6c2e37da05ed3e5fea684ef5af43d82803e903d91602b57fd5bf3',
-        salt: '0x0000000000000000000000000000000000000000000000000000000000000001'
+        factoryAddr: AMBIRE_ACCOUNT_FACTORY,
+        bytecode,
+        salt: ethers.toBeHex(0, 32)
       }
     }
 
     let emitCounter = 0
-    controller.onUpdate(() => {
-      emitCounter++
+    await new Promise((resolve) => {
+      controller.onUpdate(() => {
+        emitCounter++
 
-      if (emitCounter === 1 && controller.isReady) {
-        const keyIterator = new KeyIterator(
-          '0x574f261b776b26b1ad75a991173d0e8ca2ca1d481bd7822b2b58b2ef8a969f12'
-        )
-        controller.accountAdder.init({ keyIterator, preselectedAccounts: [] })
-        controller.accountAdder.addAccounts([accountPendingCreation]).catch(console.error)
-      }
+        if (emitCounter === 1 && controller.isReady) {
+          const keyIterator = new KeyIterator(
+            '0x574f261b776b26b1ad75a991173d0e8ca2ca1d481bd7822b2b58b2ef8a969f12'
+          )
+          controller.accountAdder.init({ keyIterator, preselectedAccounts: [] })
+          controller.accountAdder.addAccounts([accountPendingCreation]).catch(console.error)
+        }
 
-      if (emitCounter === 2) {
-        expect(controller.accounts).toContainEqual(accountPendingCreation)
-        done()
-      }
+        if (emitCounter === 2) {
+          expect(controller.accounts).toContainEqual(accountPendingCreation)
+          resolve(null)
+        }
+      })
     })
   })
 })
