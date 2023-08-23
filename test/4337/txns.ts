71f8f6e539c5115c0e9572b69d382135a2ada0cc
Emil <stojnovsky@gmail.com>
2023-08-23 21:56:59 +0300

    for pimlico api change to using fetch

--- test/4337/txns.ts
+++ test/4337/txns.ts
@@ -1,4 +1,4 @@
-import { Wallet, Contract, Interface, keccak256, AbiCoder, getBytes, JsonRpcProvider } from 'ethers'
+import { Wallet, Contract, keccak256, AbiCoder, getBytes, JsonRpcProvider } from 'ethers'
 import fetch from 'node-fetch'
 import { wrapEthSign } from '../ambireSign'
 import AMBIRE_ACCOUNT from '../../contracts/compiled/AmbireAccount.json'
@@ -15,13 +15,11 @@ const provider = new JsonRpcProvider(polygonUrl)
 
 const ENTRY_POINT_ADDR = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'
 
-const txn = ['0xC2E6dFcc2C6722866aD65F211D5757e1D2879337', 100000000000000000n, '0x']
+const txn = ['0xC2E6dFcc2C6722866aD65F211D5757e1D2879337', 10000000000000000n, '0x']
 
 const apiKey = 'c6eabeca-dd7c-49b5-afa6-50ff36cfc5be'
 const pimlicoEndpoint = `https://api.pimlico.io/v1/polygon/rpc?apikey=${apiKey}`
 const pimlicoProvider = new JsonRpcProvider(pimlicoEndpoint)
-
-console.log(pimlicoEndpoint)
 
 async function test() {
   const signer = new Wallet(SIGNER_PRIV_KEY)
@@ -39,7 +37,7 @@ async function test() {
   const s = wrapEthSign(await signer.signMessage(getBytes(hash)))
   const callData = ambireAccount.interface.encodeFunctionData('execute', [[txn], s])
 
-  const newNonce = await entryPoint.getNonce(AMBIRE_ACCOUNT_ADDR, 0)
+  const newNonce = await entryPoint.getNonce(...[AMBIRE_ACCOUNT_ADDR, 0])
   const gasPrice = 200000000000
 
   function hexlify(x: number) {
@@ -56,8 +54,8 @@ async function test() {
     preVerificationGas: hexlify(50000), // hardcode it for now at a high value
     maxFeePerGas: hexlify(gasPrice),
     maxPriorityFeePerGas: hexlify(gasPrice),
-    paymasterAndData: '0x',
-    signature: s
+    paymasterAndData: '0x0000000000000000000000000000000000000000',
+    signature: '0x'
   }
 
   const args = [userOperation, ENTRY_POINT_ADDR]
@@ -69,12 +67,41 @@ async function test() {
     body: JSON.stringify({ id: 1, jsonrpc: '2.0', method: 'pm_sponsorUserOperation', params: args })
   }
 
-  await fetch(pimlicoEndpoint, options)
+  const paymasterAndData = await fetch(pimlicoEndpoint, options)
     .then((response) => response.json())
     .then((response) => {
-      console.log(response)
+      return response.result.paymasterAndData
     })
     .catch((err) => console.error(err))
+
+  userOperation.paymasterAndData = paymasterAndData
+
+  const signature = await signer.signMessage(
+    getBytes(await entryPoint.getUserOpHash(userOperation))
+  )
+
+  userOperation.signature = signature
+
+  options.body = JSON.stringify({
+    id: 1,
+    jsonrpc: '2.0',
+    method: 'eth_sendUserOperation',
+    params: [userOperation, ENTRY_POINT_ADDR]
+  })
+
+  console.log({ options })
+
+  const userOperationHash = await fetch(pimlicoEndpoint, options)
+    .then((response) => {
+      console.log(response)
+      return response.json()
+    })
+    .then((response) => {
+      return response
+    })
+    .catch((err) => console.error(err))
+
+  console.log({ userOperationHash })
 
   // const sponsorUserOperationResult = await pimlicoProvider
   //   .send('pm_sponsorUserOperation', args)
