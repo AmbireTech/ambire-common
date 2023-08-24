13b0b2c7c5c6daa08999360932270c824d5fb679
Borislav Itskov <borislav@devlabs.bg>
2023-08-24 12:06:41 +0300

    add: a fallback working example

--- test/4337/txns.ts
+++ test/4337/txns.ts
@@ -5,13 +5,10 @@ import AMBIRE_ACCOUNT from '../../contracts/compiled/AmbireAccount.json'
 import ENTRY_POINT_ABI from './ENTRY_POINT.json'
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
 
-const abiCoder = new AbiCoder()
-
 const AMBIRE_ACCOUNT_ADDR = '0xD1cE5E6AE56693D2D3D52b2EBDf969C1D7901971'
 const SIGNER_PRIV_KEY = '0x574f261b776b26b1ad75a991173d0e8ca2ca1d481bd7822b2b58b2ef8a969f12'
 
 const polygonUrl = 'https://rpc.ankr.com/polygon'
-const polygonChainId = 137
 const provider = new JsonRpcProvider(polygonUrl)
 
 const ENTRY_POINT_ADDR = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'
@@ -19,25 +16,16 @@ const ENTRY_POINT_ADDR = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'
 // const txn = ['0xC2E6dFcc2C6722866aD65F211D5757e1D2879337', 10000000000000000n, '0x']
 const txn = ['0xC2E6dFcc2C6722866aD65F211D5757e1D2879337', 0, '0x68656c6c6f']
 
-const apiKey = 'c6eabeca-dd7c-49b5-afa6-50ff36cfc5be'
+// const apiKey = 'c6eabeca-dd7c-49b5-afa6-50ff36cfc5be'
+const apiKey = '2b56fcf6-7796-4a89-90ac-f80d5dcf6192'
 const pimlicoEndpoint = `https://api.pimlico.io/v1/polygon/rpc?apikey=${apiKey}`
 const pimlicoProvider = new StaticJsonRpcProvider(pimlicoEndpoint)
 
 async function test() {
   const signer = new Wallet(SIGNER_PRIV_KEY)
   const ambireAccount = new Contract(AMBIRE_ACCOUNT_ADDR, AMBIRE_ACCOUNT.abi, provider)
-  const nonce = await ambireAccount.nonce()
   const entryPoint = new Contract(ENTRY_POINT_ADDR, ENTRY_POINT_ABI, provider)
-
-  const hash = keccak256(
-    abiCoder.encode(
-      ['address', 'uint', 'uint', 'tuple(address, uint, bytes)[]'],
-      [AMBIRE_ACCOUNT_ADDR, polygonChainId, nonce, [txn]]
-    )
-  )
-
-  const s = wrapEthSign(await signer.signMessage(getBytes(hash)))
-  const callData = ambireAccount.interface.encodeFunctionData('execute', [[txn], s])
+  const callData = ambireAccount.interface.encodeFunctionData('executeBySender', [[txn]])
 
   const newNonce = await entryPoint.getNonce(...[AMBIRE_ACCOUNT_ADDR, 0])
   const gasPrice = await provider.send('eth_gasPrice', [])
