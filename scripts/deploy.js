8a5624f9a85e2729e8a6c525778a357ae04748f9
Borislav Itskov <borislav@devlabs.bg>
2023-08-24 11:48:23 +0300

    add: setting the fallback handler txn

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const { ethers, JsonRpcProvider } = require("ethers")
 const AmbireAccount = require("../contracts/compiled/AmbireAccount.json");
 const AmbireAccountFactory = require("../artifacts/contracts/AmbireAccountFactory.sol/AmbireAccountFactory.json");
+const erc4337Manager = require("../artifacts/contracts/AmbireERC4337Manager.sol/AmbireERC4337Manager.json");
 require('dotenv').config();
 
 const polygonUrl = 'https://rpc.ankr.com/polygon'
@@ -46,10 +47,53 @@ async function generateFactory (gasPrice) {
 	return await fundWallet.signTransaction(txn)
 }
 
+async function generateManager (gasPrice) {
+	const txn = {}
+	const pk = process.env.DEPLOY_PRIVATE_KEY
+	const fundWallet = new ethers.Wallet(pk, provider)
+	const factory = new ethers.ContractFactory(erc4337Manager.abi, erc4337Manager.bytecode, fundWallet)
+
+	txn.data = await factory.getDeployTransaction()
+	txn.from = fundWallet.address
+	txn.value = '0x00'
+	txn.type = null
+	txn.gasLimit = 10000000n
+	txn.data = txn.data.data
+	txn.gasPrice = gasPrice
+	txn.nonce = await provider.getTransactionCount(fundWallet.address)
+	txn.chainId = polygonChainId
+	return await fundWallet.signTransaction(txn)
+}
+
+async function setFallbackHandler(gasPrice) {
+	const pk = process.env.SIGNER_PRIV_KEY
+	const fundWallet = new ethers.Wallet(pk, provider)
+	const AMBIRE_ACCOUNT_ADDR = '0xD1cE5E6AE56693D2D3D52b2EBDf969C1D7901971'
+	const ERC_4337_MANAGER = '0xba9b9B22aBf1b088c22967f01947236d723432c9'
+	const ambireAccount = new ethers.Contract(AMBIRE_ACCOUNT_ADDR, AmbireAccount.abi, fundWallet)
+
+	const setAddrPrivilegeABI = ['function setAddrPrivilege(address addr, bytes32 priv)']
+	const iface = new ethers.Interface(setAddrPrivilegeABI)
+	const calldata = iface.encodeFunctionData('setAddrPrivilege', [
+		ethers.toBeHex(0x6969, 20),
+		ethers.toBeHex(ERC_4337_MANAGER, 32)
+	])
+	const setPrivTxn = [AMBIRE_ACCOUNT_ADDR, 0, calldata]
+	const data = await ambireAccount.interface.encodeFunctionData('executeBySender', [[setPrivTxn]])
+	const txn = await fundWallet.sendTransaction({
+		to: AMBIRE_ACCOUNT_ADDR,
+		value: 0,
+		data: data,
+		gasPrice: gasPrice,
+		gasLimit: 100000n
+	})
+	return txn
+}
+
 async function deploy() {
 
   const feeData = await provider.getFeeData()
-  const sig = await generateFactory(feeData.gasPrice)
+//   const sig = await setFallbackHandler(feeData.gasPrice)
   console.log(sig)
 }
 
--- src/consts/deploy.ts
+++ src/consts/deploy.ts
@@ -9,6 +9,9 @@ export const PROXY_AMBIRE_ACCOUNT = '0xfF69afDE895B381eE71e17C60350aE4c70b16a92'
 // These's a decent chance the factory address is the permanent one.
 export const AMBIRE_ACCOUNT_FACTORY = '0xA3A22Bf212C03ce55eE7C3845D4c177a6fEC418B'
 
+// NOTE<Bobby>: deployed on polygon with my account
+export const ERC_4337_MANAGER = '0xba9b9B22aBf1b088c22967f01947236d723432c9'
+
 // this is a proxy ambire account with a validateUserOp method.
 // use it for testing purposes only
 export const PROXY_VALIDATE_OP = '0x499A2f72393958cc4b78A7EAfA7DA1B954bbbAFF'
