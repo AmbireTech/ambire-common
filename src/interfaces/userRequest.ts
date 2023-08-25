002663fa4e421d20472137d7c972c344b8c9bcbe
sonytooo <simeonpetkov11999977@gmail.com>
2023-08-25 14:04:03 +0300

    add: additional props to DappNotificationRequest

--- src/interfaces/userRequest.ts
+++ src/interfaces/userRequest.ts
@@ -46,4 +46,6 @@ export interface UserRequest {
 export interface DappNotificationRequest {
   id: bigint
   ident: number
+  screen: string
+  session?: { origin: string; name: string; icon: string }
 }
