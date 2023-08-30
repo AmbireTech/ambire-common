a996ef183047441daa876f70e361c9a11d7caecc
Emil <stojnovsky@gmail.com>
2023-08-30 21:04:15 +0300

    changed consts names related to 4337

--- src/libs/accountState/accountState.ts
+++ src/libs/accountState/accountState.ts
@@ -57,12 +57,14 @@ export async function getAccountState(
       isV2: accResult.isV2,
       balance: accResult.balance,
       isEOA: accResult.isEOA,
-      isErc4337Enabled: !!associatedKeys.find(
-        (associatedKey: string[]) =>
-          associatedKey[0] === network?.erc4337?.entryPointAddr &&
-          (associatedKey[1] === network.erc4337?.entryPointMarker ||
-            associatedKey[1] === `0x${'0'.repeat(63)}1`)
-      ),
+      isErc4337Enabled:
+        network?.erc4337?.enabled &&
+        associatedKeys.find(
+          (associatedKey: string[]) =>
+            associatedKey[0] === network?.erc4337?.entryPointAddr &&
+            (associatedKey[1] === network.erc4337?.entryPointMarker ||
+              associatedKey[1] === `0x${'0'.repeat(63)}1`)
+        ),
       deployError:
         accounts[index].associatedKeys.length > 0 && accResult.associatedKeyPriviliges.length === 0
     }
