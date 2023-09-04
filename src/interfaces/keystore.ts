6caf5c98a334cc6283d0703e2032bc1a6ccba7c2
Yosif Hamed <josif.hameed@gmail.com>
2023-09-04 15:12:30 +0300

    Remove comment

--- src/interfaces/keystore.ts
+++ src/interfaces/keystore.ts
@@ -2,7 +2,6 @@ import { TypedMessage } from './userRequest'
 
 export interface KeystoreSigner {
   signRawTransaction: (params: any) => Promise<string>
-  // @TODO update this
   signTypedData: (typedMessage: TypedMessage) => Promise<string>
   signMessage: (hash: string | Uint8Array) => Promise<string>
 }
