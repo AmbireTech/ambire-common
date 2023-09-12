fb84f1aa22a2ae790ec314edbb7e450d783e3092
Yosif Hamed <josif.hameed@gmail.com>
2023-09-12 15:31:02 +0300

    Updates to the email-vault tests

--- src/libs/magicLink/magicLink.ts
+++ src/libs/magicLink/magicLink.ts
@@ -17,10 +17,10 @@ export async function requestMagicLink(
   const resp = await fetch(`${relayerUrl}/email-vault/requestKey/${email}`)
   const result: RequestMagicLinkResult = await resp.json()
 
-  if (result?.data?.secret)
-    await fetch(
-      `${relayerUrl}/email-vault/confirmationKey/${email}/${result.data.key}/${result.data.secret}`
-    )
+  // if (result?.data?.secret)
+  //   await fetch(
+  //     `${relayerUrl}/email-vault/confirmationKey/${email}/${result.data.key}/${result.data.secret}`
+  //   )
 
   if (!result.success) throw new Error(`magicLink: error getting magic link: ${result.message}`)
   return result.data
