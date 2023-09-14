35d719bc1b5494c5989505741fb57b95e96b9500
sonytooo <simeonpetkov11999977@gmail.com>
2023-09-14 14:58:30 +0300

    udate type in main.js

--- src/controllers/main/main.ts
+++ src/controllers/main/main.ts
@@ -101,8 +101,8 @@ export class MainController extends EventEmitter {
     fetch: Function
     relayerUrl: string
     keystoreSigners: { [key: string]: KeystoreSignerType }
-    onResolveDappRequest: (data: any, id?: bigint) => void
-    onRejectDappRequest: (err: any, id?: bigint) => void
+    onResolveDappRequest: (data: any, id?: number) => void
+    onRejectDappRequest: (err: any, id?: number) => void
     onUpdateDappSelectedAccount: (accountAddr: string) => void
   }) {
     super()
