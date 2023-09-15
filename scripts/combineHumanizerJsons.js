bd85a27624d2369868cddcef75f3b8422027cce5
Yosif Hamed <josif.hameed@gmail.com>
2023-09-15 12:25:50 +0300

    Typo and indents

--- scripts/combineHumanizerJsons.js
+++ scripts/combineHumanizerJsons.js
@@ -27,7 +27,7 @@ function initHumanizerMeta(humanizerMeta) {
   }
 const dappSelectorsPath = path.join(__dirname, '..', 'src', 'consts', 'dappSelectors.json')
 const dappNamesPath = path.join(__dirname, '..', 'src', 'consts', 'dappNames.json')
-const amireConstants = path.join(__dirname, '..', 'src', 'consts', 'ambireConstantsHumanizerInfo.json')
+const ambireConstants = path.join(__dirname, '..', 'src', 'consts', 'ambireConstantsHumanizerInfo.json')
 const resultPath = path.join(__dirname, '..', 'src', 'consts', 'humanizerInfo.json')
 
 
@@ -35,7 +35,7 @@ const main = async () => {
 	const prepedJsons = await Promise.all([
 		fsPromises.readFile(dappSelectorsPath, 'utf8').then(JSON.parse),
 		fsPromises.readFile(dappNamesPath, 'utf8').then(JSON.parse),
-		fsPromises.readFile(amireConstants, 'utf8').then(JSON.parse).then(initHumanizerMeta)
+		fsPromises.readFile(ambireConstants, 'utf8').then(JSON.parse).then(initHumanizerMeta)
 	])
 	const res = prepedJsons.reduce((a, b)=>({ ...a, ...b }), {})
 	await fsPromises.writeFile(resultPath, JSON.stringify(res, null, 4), 'utf8')
