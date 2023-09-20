3fac493d5f7dc876adb46eb813e6802bc9f5dd40
Kaloyan Kosev <superkalo@devlabs.bg>
2023-09-20 12:36:44 +0300

    Change: no need to specify (I am not sure if this is right)

--- src/consts/derivation.ts
+++ src/consts/derivation.ts
@@ -2,8 +2,8 @@
 export const BIP44_HD_PATH = "m/44'/60'/0'/0"
 
 export const LEDGER_BIP44_HD_PATH = BIP44_HD_PATH
-export const LEDGER_LEGACY_HD_PATH = "m/44'/60'/0'" // BIP32
-export const LEDGER_LIVE_HD_PATH = "m/44'/60'/0'/0/0" // BIP39
+export const LEDGER_LEGACY_HD_PATH = "m/44'/60'/0'"
+export const LEDGER_LIVE_HD_PATH = "m/44'/60'/0'/0/0"
 export const TREZOR_HD_PATH = BIP44_HD_PATH
 export const TREZOR_PATH_BASE = 'm'
 export const LATTICE_STANDARD_HD_PATH = "m/44'/60'/0'/0/x"
