160c8d5d9549f5e3cc8fd581eb52641bf4faeb61
Gergana Mihaylova <gergana@devlabs.bg>
2023-09-26 17:36:27 +0300

    Add: derivation path labels

--- src/consts/derivation.ts
+++ src/consts/derivation.ts
@@ -7,3 +7,11 @@ export const LEDGER_LIVE_HD_PATH = "m/44'/60'/0'/0/0"
 export const TREZOR_HD_PATH = BIP44_HD_PATH
 export const TREZOR_PATH_BASE = 'm'
 export const LATTICE_STANDARD_HD_PATH = "m/44'/60'/0'/0/x"
+
+export const HD_PATHS: { [key: string]: string } = {
+  BIP44: "m/44'/60'/0'/0",
+  'Ledger Legacy': "m/44'/60'/0'",
+  'Ledger Live': "m/44'/60'/0'/0/0",
+  'Trezor Base': 'm',
+  'Lattice Standard': "m/44'/60'/0'/0/x"
+}
