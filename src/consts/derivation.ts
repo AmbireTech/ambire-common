5e2adbc62ef4d3184748a6ab4f967b7835511848
Kaloyan Kosev <superkalo@devlabs.bg>
2023-09-29 11:23:35 +0300

    Change: HD_PATHS array with label and path

--- src/consts/derivation.ts
+++ src/consts/derivation.ts
@@ -8,10 +8,18 @@ export const TREZOR_HD_PATH = BIP44_HD_PATH
 export const TREZOR_PATH_BASE = 'm'
 export const LATTICE_STANDARD_HD_PATH = "m/44'/60'/0'/0/x"
 
-export const HD_PATHS: { [key: string]: string } = {
-  BIP44: "m/44'/60'/0'/0",
-  'Ledger Legacy': "m/44'/60'/0'",
-  'Ledger Live': "m/44'/60'/0'/0/0",
-  'Trezor Base': 'm',
-  'Lattice Standard': "m/44'/60'/0'/0/x"
+interface HDPath {
+  label: string
+  path:
+    | typeof BIP44_HD_PATH
+    | typeof LEDGER_LEGACY_HD_PATH
+    | typeof LEDGER_LIVE_HD_PATH
+    | typeof LATTICE_STANDARD_HD_PATH
 }
+
+export const HD_PATHS: HDPath[] = [
+  { label: 'BIP44', path: BIP44_HD_PATH },
+  { label: 'Ledger Legacy', path: LEDGER_LEGACY_HD_PATH },
+  { label: 'Ledger Live', path: LEDGER_LIVE_HD_PATH },
+  { label: 'Lattice Standard', path: LATTICE_STANDARD_HD_PATH }
+]
