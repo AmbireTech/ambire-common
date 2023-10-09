76bb056ed4a2f1c0bcf2e88420b78aab5d83fde7
Borislav Itskov <borislav@devlabs.bg>
2023-10-09 19:08:22 +0300

    add: start a sign() method in the main controller

--- src/controllers/main/main.ts
+++ src/controllers/main/main.ts
@@ -414,6 +414,63 @@ export class MainController extends EventEmitter {
     console.log(estimation)
   }
 
+  async sign(accountOp: AccountOp) {
+    const signer = this.#validateSign(accountOp)
+
+    if (accountOp.gasFeePayment?.isERC4337) {
+      // TODO:
+      // add fee call
+      // transform the accountOp to userOp
+      // sign it
+    }
+  }
+
+  #validateSign(accountOp: AccountOp) {
+    const account = this.accounts.find((x) => x.addr === accountOp.accountAddr)
+    if (!account) {
+      return this.emitError({
+        level: 'major',
+        message: 'Please select a signing key and try again.',
+        error: new Error(`sign: called for non-existant acc ${accountOp.accountAddr}`)
+      })
+    }
+
+    if (!accountOp.signingKeyAddr) {
+      return this.emitError({
+        level: 'major',
+        message: 'Please select a signing key and try again.',
+        error: new Error('No signer selected.')
+      })
+    }
+    const keys = this.keystore.keys.filter(key => key.addr == accountOp.signingKeyAddr)
+    if (!keys.length) {
+      return this.emitError({
+        level: 'major',
+        message: 'Please select a signing key and try again.',
+        error: new Error('Signing key not found.')
+      })
+    }
+    const signingKey = keys[0]
+    const signer = this.keystore.getSigner(signingKey.addr, signingKey.type)
+    if (!signer) {
+      return this.emitError({
+        level: 'major',
+        message: 'Please select a signing key and try again.',
+        error: new Error('Signing key not found.')
+      })
+    }
+
+    if (!accountOp.gasFeePayment) {
+      return this.emitError({
+        level: 'major',
+        message: 'Please select a gas fee payment and try again.',
+        error: new Error('No gas fee selected.')
+      })
+    }
+
+    return signer
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, class-methods-use-this
   broadcastSignedAccountOp(accountOp: AccountOp) {}
 
--- src/libs/broadcast/broadcast.ts
+++
@@ -1,8 +0,0 @@
-import { AccountOp } from "libs/accountOp/accountOp";
-import { broadcast as broadcastOnPimlico } from "services/pimlico";
-
-export async function broadcast4337(accountOp: AccountOp) {
-  // TODO:
-  // 1. transform to userOperation
-  // 2. broadcast to pimlico
-}
