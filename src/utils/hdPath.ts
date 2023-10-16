e8ba28fe544de4dafad29d65da1173cc7afd2558
Kaloyan Kosev <superkalo@devlabs.bg>
2023-10-16 12:44:20 +0300

    Add: index (because it starts from 0)

--- src/utils/hdPath.ts
+++ src/utils/hdPath.ts
@@ -1,10 +1,10 @@
 import { LEDGER_LIVE_HD_PATH } from '../consts/derivation'
 
-export const getHdPath = (derivationPath: string, slot: number) => {
+export const getHdPath = (derivationPath: string, index: number) => {
   if (derivationPath === LEDGER_LIVE_HD_PATH) {
-    return `m/44'/60'/${slot}'/0/0`
+    return `m/44'/60'/${index}'/0/0`
   }
 
   // TODO: add support for other derivation paths
-  return `${derivationPath}/${slot}`
+  return `${derivationPath}/${index}`
 }
