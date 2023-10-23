3f64d0e8c88f7fc58f93e7ed5c667d70134e6d2d
Kaloyan Kosev <superkalo@devlabs.bg>
2023-10-23 22:49:23 +0300

    Add: getHdPathFromTemplate method

--- src/utils/hdPath.ts
+++ src/utils/hdPath.ts
@@ -1,4 +1,5 @@
 import { LEDGER_LIVE_HD_PATH } from '../consts/derivation'
+import { ExternalKey } from '../interfaces/keystore'
 
 export const getHdPath = (derivationPath: string, index: number) => {
   if (derivationPath === LEDGER_LIVE_HD_PATH) {
@@ -6,4 +7,20 @@ export const getHdPath = (derivationPath: string, index: number) => {
   }
 
   return `${derivationPath}/${index}`
+}
+
+export const getHdPathFromTemplate = (
+  derivationPathTemplate: ExternalKey['meta']['hdPathTemplate'],
+  index: number
+) => {
+  return derivationPathTemplate.replace('<account>', index.toString())
+}
+
+// TODO: Remove this
+export const getHdPathFormula = (derivationPath: string) => {
+  if (derivationPath === LEDGER_LIVE_HD_PATH) {
+    return "m/44'/60'/x/0/0"
+  }
+
+  return `${derivationPath}/x`
 }
