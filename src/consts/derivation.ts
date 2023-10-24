eb85dacded894178750c94e2f389e220891012f2
Kaloyan Kosev <superkalo@devlabs.bg>
2023-10-24 18:32:02 +0300

    Change: remove ledger derivation and introduce hd ledger template

--- src/consts/derivation.ts
+++ src/consts/derivation.ts
@@ -1,36 +1,32 @@
 // The "standard" default one for many, including MetaMask
 export const BIP44_HD_PATH = "m/44'/60'/0'/0"
 
-export const LEDGER_BIP44_HD_PATH = BIP44_HD_PATH
-export const LEDGER_LEGACY_HD_PATH = "m/44'/60'/0'"
-export const LEDGER_LIVE_HD_PATH = "m/44'/60'/0'/0/0"
 export const LATTICE_STANDARD_HD_PATH = "m/44'/60'/0'/0/x"
-
-export enum DERIVATION {
-  BIP44 = 'BIP44',
-  BIP39 = 'BIP39'
-}
 
 export const BIP44_LEDGER_LIVE_TEMPLATE = "m/44'/60'/<account>'/0/0"
 export const BIP44_TREZOR_TEMPLATE = "m/44'/60'/0'/0/<account>"
 export const BIP44_LATTICE_TEMPLATE = "m/44'/60'/0'/0/<account>"
+// Closely related to the BIP44 standard, but it does not include the last "/0"
+// which is typically used to distinguish between external and internal
+// addresses (change addresses). There isn't a specific, universally recognized
+// name for this path beyond its association with Ethereum and the
+// Ethereum wallets like MyEtherWallet (MEW) and MyCrypto.
+export const MEW_LEGACY_TEMPLATE = "m/44'/60'/0'/<account>"
 
 export interface HDPath {
   label: string
   path:
     | typeof BIP44_HD_PATH
-    | typeof LEDGER_LEGACY_HD_PATH
-    | typeof LEDGER_LIVE_HD_PATH
     | typeof LATTICE_STANDARD_HD_PATH
+    | typeof MEW_LEGACY_TEMPLATE
     | typeof BIP44_TREZOR_TEMPLATE
     | typeof BIP44_LEDGER_LIVE_TEMPLATE
 }
 
 export const HD_PATHS: HDPath[] = [
   { label: 'BIP44', path: BIP44_HD_PATH },
-  { label: 'Ledger Legacy', path: LEDGER_LEGACY_HD_PATH },
-  { label: 'Ledger Live', path: LEDGER_LIVE_HD_PATH },
   { label: 'Lattice Standard', path: LATTICE_STANDARD_HD_PATH },
+  { label: 'Legacy (MyEtherWallet, MyCrypto)', path: MEW_LEGACY_TEMPLATE },
   { label: 'BIP44 (MetaMask, Trezor, GridPlus)', path: BIP44_TREZOR_TEMPLATE },
   { label: 'BIP44 (Ledger Live)', path: BIP44_LEDGER_LIVE_TEMPLATE }
 ]
--- src/utils/hdPath.ts
+++ src/utils/hdPath.ts
@@ -1,13 +1,4 @@
-import { LEDGER_LIVE_HD_PATH } from '../consts/derivation'
 import { ExternalKey } from '../interfaces/keystore'
-
-export const getHdPath = (derivationPath: string, index: number) => {
-  if (derivationPath === LEDGER_LIVE_HD_PATH) {
-    return `m/44'/60'/${index}'/0/0`
-  }
-
-  return `${derivationPath}/${index}`
-}
 
 export const getHdPathFromTemplate = (
   derivationPathTemplate: ExternalKey['meta']['hdPathTemplate'],
@@ -18,9 +9,5 @@ export const getHdPathFromTemplate = (
 
 // TODO: Remove this
 export const getHdPathFormula = (derivationPath: string) => {
-  if (derivationPath === LEDGER_LIVE_HD_PATH) {
-    return "m/44'/60'/x/0/0"
-  }
-
   return `${derivationPath}/x`
 }
