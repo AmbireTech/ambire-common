8ba8271144002d3875f103398bd21befb778a578
Kaloyan Kosev <superkalo@devlabs.bg>
2023-10-24 17:44:12 +0300

    Add: derivation templates in hd path labels

--- src/consts/derivation.ts
+++ src/consts/derivation.ts
@@ -4,8 +4,6 @@ export const BIP44_HD_PATH = "m/44'/60'/0'/0"
 export const LEDGER_BIP44_HD_PATH = BIP44_HD_PATH
 export const LEDGER_LEGACY_HD_PATH = "m/44'/60'/0'"
 export const LEDGER_LIVE_HD_PATH = "m/44'/60'/0'/0/0"
-export const TREZOR_HD_PATH = BIP44_HD_PATH
-export const TREZOR_PATH_BASE = 'm'
 export const LATTICE_STANDARD_HD_PATH = "m/44'/60'/0'/0/x"
 
 export enum DERIVATION {
@@ -24,11 +22,15 @@ export interface HDPath {
     | typeof LEDGER_LEGACY_HD_PATH
     | typeof LEDGER_LIVE_HD_PATH
     | typeof LATTICE_STANDARD_HD_PATH
+    | typeof BIP44_TREZOR_TEMPLATE
+    | typeof BIP44_LEDGER_LIVE_TEMPLATE
 }
 
 export const HD_PATHS: HDPath[] = [
   { label: 'BIP44', path: BIP44_HD_PATH },
   { label: 'Ledger Legacy', path: LEDGER_LEGACY_HD_PATH },
   { label: 'Ledger Live', path: LEDGER_LIVE_HD_PATH },
-  { label: 'Lattice Standard', path: LATTICE_STANDARD_HD_PATH }
+  { label: 'Lattice Standard', path: LATTICE_STANDARD_HD_PATH },
+  { label: 'BIP44 (MetaMask, Trezor, GridPlus)', path: BIP44_TREZOR_TEMPLATE },
+  { label: 'BIP44 (Ledger Live)', path: BIP44_LEDGER_LIVE_TEMPLATE }
 ]
