a07e5ea8312f3b6a8a3b8f113761dcdecfa5305d
Kaloyan Kosev <superkalo@devlabs.bg>
2023-10-25 12:17:32 +0300

    Change: remove lattice derivation

--- src/consts/derivation.ts
+++ src/consts/derivation.ts
@@ -1,8 +1,6 @@
 // The "standard" default one for many, including MetaMask
 export const BIP44_HD_PATH = "m/44'/60'/0'/0"
 
-export const LATTICE_STANDARD_HD_PATH = "m/44'/60'/0'/0/x"
-
 export const BIP44_LEDGER_LIVE_TEMPLATE = "m/44'/60'/<account>'/0/0"
 export const BIP44_TREZOR_TEMPLATE = "m/44'/60'/0'/0/<account>"
 export const BIP44_LATTICE_TEMPLATE = "m/44'/60'/0'/0/<account>"
@@ -17,7 +15,6 @@ export interface HDPath {
   label: string
   path:
     | typeof BIP44_HD_PATH
-    | typeof LATTICE_STANDARD_HD_PATH
     | typeof MEW_LEGACY_TEMPLATE
     | typeof BIP44_TREZOR_TEMPLATE
     | typeof BIP44_LEDGER_LIVE_TEMPLATE
@@ -25,7 +22,6 @@ export interface HDPath {
 
 export const HD_PATHS: HDPath[] = [
   { label: 'BIP44', path: BIP44_HD_PATH },
-  { label: 'Lattice Standard', path: LATTICE_STANDARD_HD_PATH },
   { label: 'Legacy (MyEtherWallet, MyCrypto)', path: MEW_LEGACY_TEMPLATE },
   { label: 'BIP44 (MetaMask, Trezor, GridPlus)', path: BIP44_TREZOR_TEMPLATE },
   { label: 'BIP44 (Ledger Live)', path: BIP44_LEDGER_LIVE_TEMPLATE }
