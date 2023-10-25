65aaace9c293b3b9fbe02fe253d0fb3f26f44b98
Borislav Itskov <borislav@devlabs.bg>
2023-10-25 10:27:48 +0300

    add wrapHash in the missing spots for the erc-4337 tests to work

--- test/helpers.ts
+++ test/helpers.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat'
 
 import { Storage } from '../src/interfaces/storage'
 import { parse, stringify } from '../src/libs/bigintJson/bigintJson'
-import { wrapEthSign } from './ambireSign'
+import { wrapEthSign, wrapHash } from './ambireSign'
 import { abiCoder, addressOne, addressTwo, AmbireAccount } from './config'
 
 async function sendFunds(to: string, ether: number) {
@@ -172,7 +172,7 @@ async function buildUserOp(paymaster: BaseContract, entryPointAddr: string, opti
       ]
     )
   )
-  const signature = wrapEthSign(await relayer.signMessage(ethers.getBytes(hash)))
+  const signature = wrapEthSign(await relayer.signMessage(wrapHash(ethers.getBytes(hash))))
 
   // abi.decode(userOp.paymasterAndData[20:], (uint48, uint48, bytes))
   const paymasterData = abiCoder.encode(
