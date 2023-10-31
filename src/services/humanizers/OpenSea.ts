fa556708f6bd201f0c97230c90250050b1050612
Gergana Mihaylova <gergana@devlabs.bg>
2023-10-31 17:47:09 +0200

    Add: opensea humanizer

--- src/services/humanizers/OpenSea.ts
+++ src/services/humanizers/OpenSea.ts
@@ -6,8 +6,24 @@ import { Interface } from 'ethers/lib/utils'
 import { HumanizerInfoType } from '../../hooks/useConstants'
 import { nativeToken, token } from '../humanReadableTransactions'
 
+const partialOpenSeaAbi = [
+  {
+    inputs: [
+      { internalType: 'address', name: 'nftContract', type: 'address' },
+      { internalType: 'address', name: 'feeRecipient', type: 'address' },
+      { internalType: 'address', name: 'minterIfNotPayer', type: 'address' },
+      { internalType: 'uint256', name: 'quantity', type: 'uint256' }
+    ],
+    name: 'mintPublic',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function'
+  }
+]
+
 const OpenSeaMapping = (humanizerInfo: HumanizerInfoType) => {
   const WyvernExchange = new Interface(humanizerInfo.abis.WyvernExchange)
+  const OpenSeaInterface = new Interface(partialOpenSeaAbi)
 
   return {
     [WyvernExchange.getSighash('atomicMatch_')]: (txn, network, { extended = false }) => {
@@ -65,6 +81,14 @@ const OpenSeaMapping = (humanizerInfo: HumanizerInfoType) => {
               'on OpenSea'
             ]
           ]
+    },
+    [OpenSeaInterface.getSighash('mintPublic')]: (txn, network, { extended = false }) => {
+      const { nftContract, feeRecipient, minterIfNotPayer, quantity } =
+        OpenSeaInterface.parseTransaction(txn).args
+
+      return !extended
+        ? [`Mint ${quantity} NFT${quantity > 1 ? 's' : ''} on ${network}`]
+        : [['Mint', `${quantity} NFT${quantity > 1 ? 's' : ''}`, 'on OpenSea']]
     }
   }
 }
