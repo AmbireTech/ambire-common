67a95429aa4e3a0411e2ac0137d877a2ebbcfa48
Kaloyan Kosev <superkalo@devlabs.bg>
2023-11-03 10:48:14 +0200

    Add: getPrivateKeyFromSeed helper

--- src/libs/keyIterator/keyIterator.ts
+++ src/libs/keyIterator/keyIterator.ts
@@ -24,6 +24,23 @@ export function isValidPrivateKey(value: string): boolean {
   }
 }
 
+export const getPrivateKeyFromSeed = (
+  seed: string,
+  keyIndex: number,
+  hdPathTemplate: HD_PATH_TEMPLATE_TYPE
+) => {
+  const mnemonic = Mnemonic.fromPhrase(seed)
+  const wallet = HDNodeWallet.fromMnemonic(
+    mnemonic,
+    getHdPathFromTemplate(hdPathTemplate, keyIndex)
+  )
+  if (wallet) {
+    return wallet.privateKey
+  }
+
+  throw new Error('Getting the private key from the seed phrase failed.')
+}
+
 /**
  * Derives a (second) private key based on a derivation algorithm that uses
  * the combo of (the first) private key as an entropy and a salt (constant)
