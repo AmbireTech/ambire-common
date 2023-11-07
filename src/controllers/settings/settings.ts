719407819d87addf835fc9c9ce45b6c23fd788cf
Kaloyan Kosev <superkalo@devlabs.bg>
2023-11-07 18:51:37 +0200

    Add: check if addresses incoming are valid; Change: selectively update account prefs

--- src/controllers/settings/settings.ts
+++ src/controllers/settings/settings.ts
@@ -1,3 +1,5 @@
+import { isValidAddress } from 'services/address'
+
 import { AccountPreferences, Settings } from '../../interfaces/settings'
 import { Storage } from '../../interfaces/storage'
 import EventEmitter from '../eventEmitter'
@@ -50,12 +52,21 @@ export class SettingsController extends EventEmitter {
   async addAccountPreferences(newAccountPreferences: AccountPreferences = {}) {
     if (!Object.keys(newAccountPreferences).length) return
 
-    // TODO: Check if this addresses exist?
-
-    this.currentSettings.accountPreferences = {
-      ...this.currentSettings.accountPreferences,
-      ...newAccountPreferences
+    if (!Object.keys(newAccountPreferences).some((key) => !isValidAddress(key))) {
+      return this.#throwInvalidAddress(Object.keys(newAccountPreferences))
     }
+
+    // TODO: Check if this addresses exist in the imported addressed?
+    // Update the account preferences with the new values incoming
+    Object.keys(newAccountPreferences).forEach((key) => {
+      // @ts-ignore even if the accountPreferences is empty object, that won't
+      // cause an issue in the logic
+      this.currentSettings.accountPreferences[key] = {
+        // @ts-ignore same as above
+        ...this.currentSettings.accountPreferences[key],
+        ...newAccountPreferences[key]
+      }
+    })
 
     await this.#storeCurrentSettings()
 
@@ -69,6 +80,10 @@ export class SettingsController extends EventEmitter {
     // There's nothing to delete
     if (!Object.keys(this.currentSettings.accountPreferences).length) return
 
+    if (!accountPreferenceKeys.some((key) => !isValidAddress(key))) {
+      return this.#throwInvalidAddress(accountPreferenceKeys)
+    }
+
     accountPreferenceKeys.forEach((key) => {
       // Cast to AccountPreferences, since above the case when the
       // accountPreferences is empty (and there is nothing to delete) is handled
@@ -79,5 +94,18 @@ export class SettingsController extends EventEmitter {
 
     // Emit an update event
     this.emitUpdate()
+  }
+
+  #throwInvalidAddress(addresses: string[]) {
+    return this.emitError({
+      message:
+        'Invalid account address incoming in the account preferences. Please try again or contact support if the problem persists.',
+      level: 'major',
+      error: new Error(
+        `settings: invalid address in the account preferences keys incoming: ${addresses.join(
+          ', '
+        )}`
+      )
+    })
   }
 }
