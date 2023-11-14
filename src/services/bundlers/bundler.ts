e01b308574e96f79ce7cad542f9c0b08691e4ed8
Borislav Itskov <borislav@devlabs.bg>
2023-11-14 12:44:49 +0200

    add: return the correct txn hash on dApp level for erc-4337 requests

--- src/services/bundlers/bundler.ts
+++ src/services/bundlers/bundler.ts
@@ -7,6 +7,13 @@ require('dotenv').config();
 
 export class Bundler {
   /**
+   * The default pollWaitTime. This is used to determine
+   * how many milliseconds to wait until before another request to the
+   * bundler for the receipt is sent
+   */
+  public pollWaitTime = 1500
+
+  /**
    * Get the transaction receipt from the userOperationHash if ready
    *
    * @param userOperationHash
@@ -16,6 +23,23 @@ export class Bundler {
     const url = `https://api.pimlico.io/v1/${network.id}/rpc?apikey=${process.env.REACT_APP_PIMLICO_API_KEY}`
     const provider = new StaticJsonRpcProvider(url)
     return provider.send("eth_getUserOperationReceipt", [userOperationHash])
+  }
+
+  /**
+   * Call getReceipt until a result is returned
+   *
+   * @param userOperationHash
+   * @param network
+   * @returns https://docs.alchemy.com/reference/eth-getuseroperationreceipt
+   */
+  async poll(userOperationHash: string, network: NetworkDescriptor): Promise<any> {
+    const receipt = await this.getReceipt(userOperationHash, network)
+    if (!receipt) {
+      const delayPromise = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+      await delayPromise(this.pollWaitTime)
+      return this.poll(userOperationHash, network)
+    }
+    return receipt
   }
 
   /**
