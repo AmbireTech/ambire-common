7d4ef7b38186a63762992bb504d2b6fd0f32fad1
Borislav Itskov <borislav@devlabs.bg>
2023-11-17 15:09:44 +0200

    remove not needed code block

--- src/libs/signMessage/signMessage.ts
+++ src/libs/signMessage/signMessage.ts
@@ -17,9 +17,6 @@ import hexStringToUint8Array from '../../utils/hexStringToUint8Array'
 /**
  * For EIP712 signatures, we need to append 00 at the end
  * for ambire to recognize it
- *
- * @param signature 
- * @returns string
  */
 export const wrapEIP712 = (signature: string) => {
   return signature + '00'
@@ -28,9 +25,6 @@ export const wrapEIP712 = (signature: string) => {
 /**
  * For EthSign signatures, we need to append 01 at the end
  * for ambire to recognize it
- *
- * @param signature 
- * @returns string
  */
 export const wrapEthSign = (signature: string) => {
   return signature + '01'
