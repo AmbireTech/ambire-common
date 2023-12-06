7947aacbfa784cc475cae26d6ef2d8f00c944555
Petromir <petromir@devlabs.bg>
2023-12-06 14:51:36 +0200

    delete: proxy account provider.getCode in getBytecode

--- scripts/4337/firstTxn.ts
+++ scripts/4337/firstTxn.ts
@@ -1,12 +1,14 @@
-import { Wallet, Contract, getBytes, JsonRpcProvider, ethers } from 'ethers'
+import { Contract, ethers, getBytes, JsonRpcProvider, Wallet } from 'ethers'
 import fetch from 'node-fetch'
-import AMBIRE_ACCOUNT from '../../contracts/compiled/AmbireAccount.json'
-import ENTRY_POINT_ABI from './ENTRY_POINT.json'
+
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
+
+import AMBIRE_ACCOUNT from '../../contracts/compiled/AmbireAccount.json'
 import { AMBIRE_ACCOUNT_FACTORY } from '../../src/consts/deploy'
-import { getBytecode } from '../../src/libs/proxyDeploy/bytecode'
 import { networks } from '../../src/consts/networks'
+import { getBytecode } from '../../src/libs/proxyDeploy/bytecode'
 import { wrapEthSign } from '../../test/ambireSign'
+import ENTRY_POINT_ABI from './ENTRY_POINT.json'
 
 // const AMBIRE_ACCOUNT_ADDR = '0xD1cE5E6AE56693D2D3D52b2EBDf969C1D7901971'
 const SIGNER_PRIV_KEY = '0x574f261b776b26b1ad75a991173d0e8ca2ca1d481bd7822b2b58b2ef8a969f12'
@@ -25,27 +27,25 @@ const abiCoder = new ethers.AbiCoder()
 const salt = '0x0'
 
 function getAmbireAccountAddress(factoryAddress: string, bytecode: string) {
-  return ethers.getCreate2Address(factoryAddress, ethers.toBeHex(salt, 32), ethers.keccak256(bytecode))
+  return ethers.getCreate2Address(
+    factoryAddress,
+    ethers.toBeHex(salt, 32),
+    ethers.keccak256(bytecode)
+  )
 }
 
 function getDeployCalldata(bytecodeWithArgs: string, txns: any, sig: string) {
-  const abi = ['function deployAndExecute(bytes calldata code, uint256 salt, tuple(address, uint256, bytes)[] calldata txns, bytes calldata signature) external returns (address)']
+  const abi = [
+    'function deployAndExecute(bytes calldata code, uint256 salt, tuple(address, uint256, bytes)[] calldata txns, bytes calldata signature) external returns (address)'
+  ]
   const iface = new ethers.Interface(abi)
-  return iface.encodeFunctionData('deployAndExecute', [
-    bytecodeWithArgs,
-    salt,
-    txns,
-    sig
-  ])
+  return iface.encodeFunctionData('deployAndExecute', [bytecodeWithArgs, salt, txns, sig])
 }
 
 function getPriviledgeTxn(ambireAccountAddr: string, privAddress: string, privHash: string) {
   const setAddrPrivilegeABI = ['function setAddrPrivilege(address addr, bytes32 priv)']
   const iface = new ethers.Interface(setAddrPrivilegeABI)
-  const calldata = iface.encodeFunctionData('setAddrPrivilege', [
-    privAddress,
-    privHash
-  ])
+  const calldata = iface.encodeFunctionData('setAddrPrivilege', [privAddress, privHash])
   return [ambireAccountAddr, 0, calldata]
 }
 
@@ -60,14 +60,18 @@ async function test() {
   ]
   console.log(signer.address)
   console.log(secondKeyAddr)
-  const bytecodeWithArgs = await getBytecode(polygon, privs)
+  const bytecodeWithArgs = await getBytecode(privs)
   const senderAddress = getAmbireAccountAddress(AMBIRE_ACCOUNT_FACTORY, bytecodeWithArgs)
 
   const ambireAccount = new Contract(senderAddress, AMBIRE_ACCOUNT.abi, provider)
   const entryPoint = new Contract(ENTRY_POINT_ADDR, ENTRY_POINT_ABI, provider)
   const anotherTxn = [senderAddress, 0, '0x68656c6c6f']
   const callData = ambireAccount.interface.encodeFunctionData('executeBySender', [[anotherTxn]])
-  const txn = getPriviledgeTxn(senderAddress, ENTRY_POINT_ADDR, '0x42144640c7cb5ff8aa9595ae175ffcb6dd152db6e737c13cc2d5d07576967020')
+  const txn = getPriviledgeTxn(
+    senderAddress,
+    ENTRY_POINT_ADDR,
+    '0x42144640c7cb5ff8aa9595ae175ffcb6dd152db6e737c13cc2d5d07576967020'
+  )
   const gasPrice = await provider.send('eth_gasPrice', [])
   const msg = ethers.getBytes(
     ethers.keccak256(
@@ -78,10 +82,9 @@ async function test() {
     )
   )
   const s = wrapEthSign(await signer.signMessage(msg))
-  const initCode = ethers.hexlify(ethers.concat([
-    AMBIRE_ACCOUNT_FACTORY,
-    getDeployCalldata(bytecodeWithArgs, [txn], s)
-  ]))
+  const initCode = ethers.hexlify(
+    ethers.concat([AMBIRE_ACCOUNT_FACTORY, getDeployCalldata(bytecodeWithArgs, [txn], s)])
+  )
 
   const userOperation = {
     sender: senderAddress,
@@ -114,26 +117,29 @@ async function test() {
 
   userOperation.paymasterAndData = paymasterAndData
 
-  const signature = wrapEthSign(await signer.signMessage(
-    getBytes(await entryPoint.getUserOpHash(userOperation))
-  ))
+  const signature = wrapEthSign(
+    await signer.signMessage(getBytes(await entryPoint.getUserOpHash(userOperation)))
+  )
   userOperation.signature = signature
 
-  const userOperationHash = await pimlicoProvider.send("eth_sendUserOperation", [userOperation, ENTRY_POINT_ADDR])
-  console.log("UserOperation hash:", userOperationHash)
+  const userOperationHash = await pimlicoProvider.send('eth_sendUserOperation', [
+    userOperation,
+    ENTRY_POINT_ADDR
+  ])
+  console.log('UserOperation hash:', userOperationHash)
 
   // let's also wait for the userOperation to be included, by continually querying for the receipts
-  console.log("Querying for receipts...")
+  console.log('Querying for receipts...')
   let receipt = null
   let counter = 0
   while (receipt === null) {
     try {
-      await new Promise((r) => setTimeout(r, 1000)) //sleep
+      await new Promise((r) => setTimeout(r, 1000)) // sleep
       counter++
-      receipt = await pimlicoProvider.send("eth_getUserOperationReceipt", [userOperationHash])
+      receipt = await pimlicoProvider.send('eth_getUserOperationReceipt', [userOperationHash])
       console.log(receipt)
     } catch (e) {
-      console.log('error throwed, retry counter ' + counter)
+      console.log(`error throwed, retry counter ${counter}`)
     }
   }
 }
