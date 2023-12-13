d36ec1cccdec0eb58521098db625a97e1f7a6cf8
Yosif Hamed <josif.hameed@gmail.com>
2023-12-13 14:59:31 +0200

    Add Dapps to supported dapps

--- src/constants/supportedDApps.ts
+++ src/constants/supportedDApps.ts
@@ -6,7 +6,12 @@ const supportedDApps: string[] = [
   'https://app.swappin.gifts',
   'https://app.swappin.gifts/ref/ambire',
   'https://app.uniswap.org',
-  'https://opensea.io'
+  'https://opensea.io',
+  'https://app.gmx.io',
+  'https://mean.finance',
+  'https://swap.cow.fi',
+  'https://www.bungee.exchange',
+  'https://layer3.xyz'
 ]
 
 export default supportedDApps
