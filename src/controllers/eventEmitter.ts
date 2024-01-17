5bb75fa9002723cb47cb79348804e80ce63f8e69
sonytooo <simeonpetkov11999977@gmail.com>
2024-01-17 15:01:45 +0200

    state public/private props fixes

--- src/controllers/eventEmitter.ts
+++ src/controllers/eventEmitter.ts
@@ -10,53 +10,61 @@ export type ErrorRef = {
 }
 
 export default class EventEmitter {
-  callbacks: {
+  #callbacks: {
     id: string | null
     cb: () => void
   }[] = []
 
-  errorCallbacks: {
+  #errorCallbacks: {
     id: string | null
     cb: (error: ErrorRef) => void
   }[] = []
 
   #errors: ErrorRef[] = []
 
+  get onUpdateIds() {
+    return this.#callbacks.map((item) => item.id)
+  }
+
+  get onErrorIds() {
+    return this.#errorCallbacks.map((item) => item.id)
+  }
+
   protected emitUpdate() {
     // eslint-disable-next-line no-restricted-syntax
-    for (const i of this.callbacks) i.cb()
+    for (const i of this.#callbacks) i.cb()
   }
 
   protected emitError(error: ErrorRef) {
     this.#errors.push(error)
-    this.trimErrorsIfNeeded()
+    this.#trimErrorsIfNeeded()
 
     // eslint-disable-next-line no-restricted-syntax
-    for (const i of this.errorCallbacks) i.cb(error)
+    for (const i of this.#errorCallbacks) i.cb(error)
   }
 
   // Prevents memory leaks and storing huge amount of errors
-  private trimErrorsIfNeeded() {
+  #trimErrorsIfNeeded() {
     if (this.#errors.length > LIMIT_ON_THE_NUMBER_OF_ERRORS) {
       const excessErrors = this.#errors.length - LIMIT_ON_THE_NUMBER_OF_ERRORS
       this.#errors = this.#errors.slice(excessErrors)
     }
   }
 
-  public getErrors() {
+  getErrors() {
     return this.#errors
   }
 
   // returns an unsub function
   onUpdate(cb: () => void, id?: string): () => void {
     const callbackId = id || new Date().getTime().toString()
-    this.callbacks.push({
+    this.#callbacks.push({
       id: callbackId,
       cb
     })
 
     return () => {
-      this.callbacks = this.callbacks.filter((callbackItem) => callbackItem.id !== callbackId)
+      this.#callbacks = this.#callbacks.filter((callbackItem) => callbackItem.id !== callbackId)
     }
   }
 
@@ -64,13 +72,13 @@ export default class EventEmitter {
   onError(cb: (error: ErrorRef) => void, id?: string): () => void {
     const callbackId = id || new Date().getTime().toString()
 
-    this.errorCallbacks.push({
+    this.#errorCallbacks.push({
       id: callbackId,
       cb
     })
 
     return () => {
-      this.errorCallbacks = this.errorCallbacks.filter(
+      this.#errorCallbacks = this.#errorCallbacks.filter(
         (callbackItem) => callbackItem.id !== callbackId
       )
     }
