8eed29d76682d50496dd6a02967b7c794a0585f2
Yosif Hamed <josif.hameed@gmail.com>
2024-02-05 14:59:07 +0200

    Update and run humanizerInfo script

--- scripts/combineHumanizerJsons.js
+++ scripts/combineHumanizerJsons.js
@@ -8,8 +8,8 @@ require('dotenv').config()
 const AMBIRE_CONSTANTS_URL = process.env.AMBIRE_CONSTANTS_URL || 'http://localhost:5000'
 // const AMBIRE_CONSTANTS_URL = 'http://localhost:5000'
 
-const resultPath = path.join(__dirname, '..', 'src', 'consts', 'humanizerInfo.json')
-const storedAmbireConstantsPath = path.join(
+const humanizerV2ResultPath = path.join(__dirname, '..', 'src', 'consts', 'humanizerInfo.json')
+const humanizerLagacyResultPath = path.join(
   __dirname,
   '..',
   'src',
@@ -18,11 +18,25 @@ const storedAmbireConstantsPath = path.join(
 )
 
 // @TODO: rename dappSelectors.json file name
-const sigHashesPath = path.join(__dirname, '..', 'src', 'consts', 'dappSelectors.json')
-const dappNamesPath = path.join(__dirname, '..', 'src', 'consts', 'dappAddressList.json')
+const sigHashesSourcePath = path.join(
+  __dirname,
+  '..',
+  'src',
+  'consts',
+  'humanizer',
+  'dappSelectors.json'
+)
+const dappNamesSourcePath = path.join(
+  __dirname,
+  '..',
+  'src',
+  'consts',
+  'humanizer',
+  'dappAddressList.json'
+)
 
-function initHumanizerMeta(humanizerMeta) {
-  const humanizerInfo = humanizerMeta.humanizerInfo
+function parseToV2HumanizerMeta(humanizerMeta) {
+  const { humanizerInfo } = humanizerMeta
   const newHumanizerMeta = {}
   Object.keys(humanizerInfo?.tokens).forEach((k2) => {
     newHumanizerMeta[`tokens:${ethers.getAddress(k2)}`] = humanizerInfo.tokens?.[k2]
@@ -42,9 +56,11 @@ function initHumanizerMeta(humanizerMeta) {
   }
 }
 
-const addExtraStoredData = async () => {
-  const funcAndErrSigHashes = await fsPromises.readFile(sigHashesPath, 'utf-8').then(JSON.parse)
-  const dappNames = await fsPromises.readFile(dappNamesPath, 'utf-8').then(JSON.parse)
+const readExtraStoredData = async () => {
+  const funcAndErrSigHashes = await fsPromises
+    .readFile(sigHashesSourcePath, 'utf-8')
+    .then(JSON.parse)
+  const dappNames = await fsPromises.readFile(dappNamesSourcePath, 'utf-8').then(JSON.parse)
 
   const res = { ...funcAndErrSigHashes }
 
@@ -54,37 +70,42 @@ const addExtraStoredData = async () => {
   return res
 }
 
-const getAmbireConstants = async () => {
+const fetchAmbireConstants = async () => {
   const fethcedAmbireConstants = await fetch(`${AMBIRE_CONSTANTS_URL}/result.json`)
     .then((res) => res.json())
     .catch(console.log)
   const storedAmbireConstants = await fsPromises
-    .readFile(storedAmbireConstantsPath, 'utf-8')
+    .readFile(humanizerLagacyResultPath, 'utf-8')
     .then(JSON.parse)
     .catch(console.log)
   if (fethcedAmbireConstants) {
     await fsPromises
-      .writeFile(storedAmbireConstantsPath, JSON.stringify(fethcedAmbireConstants), 'utf8')
+      .writeFile(humanizerLagacyResultPath, JSON.stringify(fethcedAmbireConstants), 'utf8')
       .then(() => console.log('stored fetched ambire-constants'))
       .catch((e) => console.log(`failed to store fetched ambire-constants, ${e}`))
   }
   return fethcedAmbireConstants || storedAmbireConstants
 }
 const main = async () => {
-  const oldHumanizerMeta = await fsPromises.readFile(resultPath, 'utf-8').then(JSON.parse)
+  const initialV2HumanizerMeta = await fsPromises
+    .readFile(humanizerV2ResultPath, 'utf-8')
+    .then(JSON.parse)
 
-  const ambireConstants = await getAmbireConstants()
-  const parsedAmbireConstants = initHumanizerMeta(ambireConstants)
-  const extraStoredData = await addExtraStoredData()
-  const finalJson = { ...extraStoredData, ...parsedAmbireConstants }
+  const fetchedConstants = await fetchAmbireConstants()
+
+  const parsedAmbireConstants = parseToV2HumanizerMeta(fetchedConstants)
+
+  const extraStoredData = await readExtraStoredData()
+
+  const finalV2HumanizerMeta = { ...extraStoredData, ...parsedAmbireConstants }
   // await fsPromises.writeFile(resultPath, JSON.stringify(finalJson, null, 4), 'utf8')
-  await fsPromises.writeFile(resultPath, JSON.stringify(finalJson), 'utf8')
+  await fsPromises.writeFile(humanizerV2ResultPath, JSON.stringify(finalV2HumanizerMeta), 'utf8')
 
   // console.log(JSON.stringify(finalJson, null, 4))
   console.log(
-    `Old file had ${Object.keys(oldHumanizerMeta).length} keys, the new object has ${
-      Object.keys(finalJson).length
-    } keys. Res written to ${resultPath}`
+    `Old file had ${Object.keys(initialV2HumanizerMeta).length} keys, the new object has ${
+      Object.keys(finalV2HumanizerMeta).length
+    } keys. Res written to ${humanizerV2ResultPath}`
   )
 }
 
