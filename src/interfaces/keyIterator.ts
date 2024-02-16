782c4edbe99c8f695008b6fda820d7a2dedccfce
Kaloyan Kosev <superkalo@devlabs.bg>
2024-02-16 16:26:31 +0200

    Add: todo

--- src/interfaces/keyIterator.ts
+++ src/interfaces/keyIterator.ts
@@ -1,6 +1,7 @@
 import { HD_PATH_TEMPLATE_TYPE } from '../consts/derivation'
 
 export interface KeyIterator {
+  // TODO: maybe add type in here
   /** The wallet native SDK instance, if any exists */
   walletSDK?: any
   retrieve: (
