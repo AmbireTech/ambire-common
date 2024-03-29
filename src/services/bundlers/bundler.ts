515f473a9bef978c43f123017d1c620deb60d38c
Borislav Itskov <borislav@devlabs.bg>
2024-03-29 09:21:59 +0200

    add: binRuntime state override to be sure payment is calculated

--- src/services/bundlers/bundler.ts
+++ src/services/bundlers/bundler.ts
@@ -155,6 +155,7 @@ export class Bundler {
           }
         : {
             [userOperation.sender]: {
+              code: AmbireAccountNoReverts.binRuntime,
               stateDiff
             }
           }
