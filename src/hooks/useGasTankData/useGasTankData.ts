4648e96e6bf1ac0584211a391d8da5a1ecff0d08
Gergana Mihaylova <gergana@devlabs.bg>
2024-04-04 15:35:32 +0300

    Fix: feeToken check if object

--- src/hooks/useGasTankData/useGasTankData.ts
+++ src/hooks/useGasTankData/useGasTankData.ts
@@ -100,7 +100,7 @@ export default function useGasTankData({
       gasTankTxns.length &&
       gasTankTxns.map((item: any) => {
         const txnFilled = gasTankFilledTxns && gasTankFilledTxns.length && gasTankFilledTxns.find((i: any) => i._id === item._id)
-        const feeToken = item.feeToken || txnFilled && feeAssetsRes.find((i: any) => i.network === network?.id && i.address.toLowerCase() === txnFilled.address.toLowerCase())?.symbol 
+        const feeToken = (item.feeToken && item.feeToken.symbol && item.feeToken.symbol.toLowerCase()) || item.feeToken || txnFilled && feeAssetsRes.find((i: any) => i.network === network?.id && i.address.toLowerCase() === txnFilled.address.toLowerCase())?.symbol 
         
         const feeTokenDetails = feeAssetsRes
           ? feeAssetsRes.find((i: any) => i.symbol === feeToken)
