2c474b75d9b7d6dff3c56f7622eef5d37c67fc4d
Borislav Itskov <borislav@devlabs.bg>
2024-04-08 16:15:30 +0300

    fix: estimateBunlder tests

--- src/libs/estimate/estimateBundler.test.ts
+++ src/libs/estimate/estimateBundler.test.ts
@@ -38,19 +38,22 @@ describe('Bundler estimation tests', () => {
           address: '0x0000000000000000000000000000000000000000',
           isGasTank: false,
           amount: 1n,
-          symbol: 'ETH'
+          symbol: 'ETH',
+          networkId: 'optimism'
         },
         {
           address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
           isGasTank: false,
           amount: 1n,
-          symbol: 'USDT'
+          symbol: 'USDT',
+          networkId: 'optimism'
         },
         {
           address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
           isGasTank: false,
           amount: 1n,
-          symbol: 'USDC'
+          symbol: 'USDC',
+          networkId: 'optimism'
         }
       ]
       const result = await bundlerEstimate(
