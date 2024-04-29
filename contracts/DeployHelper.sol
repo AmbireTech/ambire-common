c0e7d5f60e875b6d877e7af35fd9ca4792301611
Borislav Itskov <borislav@devlabs.bg>
2024-04-29 13:41:20 +0300

    remove DeployHelperStaging and add the live DeployHelper

--- contracts/DeployHelper.sol
+++ contracts/DeployHelper.sol
@@ -9,7 +9,7 @@ contract DeployHelper {
   constructor() {
     new AmbireAccount();
     new AmbireAccountFactory(0x942f9CE5D9a33a82F88D233AEb3292E680230348);
-    new AmbirePaymaster(0x942f9CE5D9a33a82F88D233AEb3292E680230348);
+    new AmbirePaymaster(0x39f1B4be409F2DFC8FCB84C1196537728df727f3);
     // no need to selfdestruct because it doesn't give a refund anymore
   }
 }
