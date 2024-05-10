74ce3683f414324846e1c27ff0f97ddf1163b3db
Yosif Hamed <josif.hameed@gmail.com>
2024-05-10 13:52:42 +0300

    Update compiled deployless jsonrpc fix

--- dist/src/libs/deployless/deployless.js
+++ dist/src/libs/deployless/deployless.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseErr = exports.fromDescriptor = exports.Deployless = exports.DeploylessMode = void 0;
-const ethers_1 = require("ethers");
 const assert_1 = __importDefault(require("assert"));
+const ethers_1 = require("ethers");
 const Deployless_json_1 = __importDefault(require("../../../contracts/compiled/Deployless.json"));
 // this is a magic contract that is constructed like `constructor(bytes memory contractBytecode, bytes memory data)` and returns the result from the call
 // compiled from relayer:a7ea373559d8c419577ac05527bd37fbee8856ae/src/velcro-v3/contracts/Deployless.sol with solc 0.8.17
@@ -19,8 +19,14 @@ const codeOfContractAbi = ['function codeOf(bytes deployCode) external view'];
 const deployErrorSig = '0xb4f54111';
 // Signature of Error(string)
 const errorSig = '0x08c379a0';
+// LBRouter__InvalidTokenPath
+const invalidPath = '0x4feac00c';
 // Signature of Panic(uint256)
 const panicSig = '0x4e487b71';
+// uniswap swap expired
+const expiredSwap = '0x5bf6f916';
+// uniswap signature expired
+const expiredSig = '0xcd21db4f';
 // any made up addr would work
 const arbitraryAddr = '0x0000000000000000000000000000000000696969';
 const abiCoder = new ethers_1.AbiCoder();
@@ -33,7 +39,9 @@ var DeploylessMode;
 const defaultOptions = {
     mode: DeploylessMode.Detect,
     blockTag: 'latest',
-    from: undefined
+    from: undefined,
+    to: arbitraryAddr,
+    stateToOverride: null
 };
 class Deployless {
     get isLimitedAt24kbData() {
@@ -53,7 +61,11 @@ class Deployless {
     }
     // this will detect whether the provider supports state override and also retrieve the actual code of the contract we are using
     async detectStateOverride() {
-        if (!(this.provider instanceof ethers_1.JsonRpcProvider)) {
+        const isJsonRpcProvider = this.provider &&
+            typeof this.provider.send === 'function' &&
+            // eslint-disable-next-line no-underscore-dangle
+            typeof this.provider._send === 'function';
+        if (!isJsonRpcProvider) {
             throw new Error('state override mode (or auto-detect) not available unless you use JsonRpcProvider');
         }
         const codeOfIface = new ethers_1.Interface(codeOfContractAbi);
@@ -66,7 +78,8 @@ class Deployless {
             { [arbitraryAddr]: { code: codeOfContractCode } }
         ]));
         // any response bigger than 0x is sufficient to know that state override worked
-        this.stateOverrideSupported = code.length > 2;
+        // the response would be just "0x" if state override doesn't work
+        this.stateOverrideSupported = code.startsWith('0x') && code.length > 2;
         this.contractRuntimeCode = mapResponse(code);
     }
     async call(methodName, args, opts = {}) {
@@ -74,26 +87,35 @@ class Deployless {
         const forceProxy = opts.mode === DeploylessMode.ProxyContract;
         // First, start by detecting which modes are available, unless we're forcing the proxy mode
         // if we use state override, we do need detection to run still so it can populate contractRuntimeCode
-        if (!this.detectionPromise && !forceProxy && this.contractRuntimeCode === undefined) {
+        if (this.stateOverrideSupported &&
+            !this.detectionPromise &&
+            !forceProxy &&
+            this.contractRuntimeCode === undefined) {
             this.detectionPromise = this.detectStateOverride();
         }
         await this.detectionPromise;
+        if (opts.stateToOverride !== null && opts.mode !== DeploylessMode.StateOverride) {
+            throw new Error('state override passed but not requested');
+        }
         if (opts.mode === DeploylessMode.StateOverride && !this.stateOverrideSupported) {
-            // @TODO test this case
             throw new Error('state override requested but not supported');
         }
         const callData = this.iface.encodeFunctionData(methodName, args);
+        const toAddr = opts.to ?? arbitraryAddr;
         const callPromise = !!this.stateOverrideSupported && !forceProxy
             ? this.provider.send('eth_call', [
                 {
-                    to: arbitraryAddr,
+                    to: toAddr,
                     data: callData,
                     from: opts.from,
                     gasPrice: opts?.gasPrice,
                     gas: opts?.gasLimit
                 },
                 opts.blockTag,
-                { [arbitraryAddr]: { code: this.contractRuntimeCode } }
+                {
+                    [toAddr]: { code: this.contractRuntimeCode },
+                    ...(opts.stateToOverride || {})
+                }
             ])
             : this.provider.call({
                 blockTag: opts.blockTag,
@@ -143,7 +165,7 @@ function parseErr(data) {
     const dataNoPrefix = data.slice(10);
     if (data.startsWith(panicSig)) {
         // https://docs.soliditylang.org/en/v0.8.11/control-structures.html#panic-via-assert-and-error-via-require
-        const num = parseInt('0x' + dataNoPrefix);
+        const num = parseInt(`0x${dataNoPrefix}`);
         if (num === 0x00)
             return 'generic compiler error';
         if (num === 0x01)
@@ -156,14 +178,24 @@ function parseErr(data) {
     }
     if (data.startsWith(errorSig)) {
         try {
-            return abiCoder.decode(['string'], '0x' + dataNoPrefix)[0];
+            return abiCoder.decode(['string'], `0x${dataNoPrefix}`)[0];
         }
         catch (e) {
             if (e.code === 'BUFFER_OVERRUN' || e.code === 'NUMERIC_FAULT')
                 return dataNoPrefix;
-            else
-                throw e;
+            return e;
         }
+    }
+    if (data.startsWith(invalidPath)) {
+        return 'Invalid swap path. Sometimes the router fails to pass correctly the swap parameters. Please try again to reinit the swap';
+    }
+    // uniswap expired error
+    if (data === expiredSwap) {
+        return 'Swap expired';
+    }
+    // uniswap signature expired error
+    if (data.startsWith(expiredSig)) {
+        return 'Signature expired';
     }
     return null;
 }
