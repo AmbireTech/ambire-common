7897af359f22d4a94939e77ec191e1afe3da14f9
sonytooo <simeonpetkov11999977@gmail.com>
2024-05-31 10:04:35 +0300

    add more actions tests and imrove the existing ones

--- src/controllers/actions/actions.test.ts
+++ src/controllers/actions/actions.test.ts
@@ -70,6 +70,37 @@ describe('SignMessageController', () => {
     expect(actionsCtrl.actionsQueue).toHaveLength(1)
     expect(actionsCtrl.currentAction).toEqual(action1)
   })
+  test('should update action', (done) => {
+    const updatedReq2: DappUserRequest = {
+      id: 2,
+      action: { kind: 'dappConnect', params: { someUpdatedParams: {} } },
+      meta: { isSignAction: false },
+      session: { name: '', icon: '', origin: '' },
+      dappPromise: { resolve: () => {}, reject: () => {} }
+    }
+    const updatedAction2: DappRequestAction = {
+      id: updatedReq2.id,
+      type: 'dappRequest',
+      userRequest: updatedReq2
+    }
+
+    let emitCounter = 0
+    const unsubscribe = actionsCtrl.onUpdate(async () => {
+      emitCounter++
+      if (emitCounter === 1) {
+        expect(actionsCtrl.actionsQueue).toHaveLength(2)
+        expect(actionsCtrl.visibleActionsQueue).toHaveLength(2)
+        expect(actionsCtrl.currentAction?.id).not.toEqual(null)
+        // update does not change the currently selectedAction
+        expect(actionsCtrl.currentAction?.id).not.toEqual(updatedAction2.id)
+        expect(actionsCtrl.actionWindowId).toEqual(1)
+        unsubscribe()
+        done()
+      }
+    })
+
+    actionsCtrl.addOrUpdateAction(updatedAction2, true)
+  })
   test('should add an action with priority', (done) => {
     const req3: SignUserRequest = {
       id: 3,
@@ -88,6 +119,7 @@ describe('SignMessageController', () => {
 
       if (emitCounter === 1) {
         expect(actionsCtrl.actionsQueue).toHaveLength(3)
+        expect(actionsCtrl.visibleActionsQueue).toHaveLength(3)
         expect(actionsCtrl.currentAction).toEqual(action3)
         expect(actionsCtrl.actionWindowId).toEqual(1)
         unsubscribe()
@@ -96,6 +128,26 @@ describe('SignMessageController', () => {
     })
 
     actionsCtrl.addOrUpdateAction(action3, true)
+  })
+  test('should have banners', () => {
+    // no banner for benzin and one banner for the 2 other actions
+    expect(actionsCtrl.banners).toHaveLength(1)
+  })
+  test('on selectedAccount change', (done) => {
+    let emitCounter = 0
+    const unsubscribe = actionsCtrl.onUpdate(async () => {
+      emitCounter++
+
+      if (emitCounter === 1) {
+        expect(actionsCtrl.actionsQueue).toHaveLength(3)
+        expect(actionsCtrl.visibleActionsQueue).toHaveLength(2)
+        expect(actionsCtrl.actionWindowId).toEqual(1)
+        actionsCtrl.update({ selectedAccount: '0x77777777789A8BBEE6C64381e5E89E501fb0e4c8' })
+        unsubscribe()
+        done()
+      }
+    })
+    actionsCtrl.update({ selectedAccount: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' })
   })
   test('on window close', (done) => {
     let emitCounter = 0
@@ -114,11 +166,30 @@ describe('SignMessageController', () => {
 
     event.emit('windowRemoved', windowId)
   })
-  test('should remove actions from actionsQueue', (done) => {
+  test('should select action by id', (done) => {
     let emitCounter = 0
     const unsubscribe = actionsCtrl.onUpdate(async () => {
       emitCounter++
 
+      if (emitCounter === 2) {
+        expect(actionsCtrl.actionWindowId).toBe(2) // action-window is reopened on setCurrentAction
+        unsubscribe()
+        done()
+      }
+
+      if (emitCounter === 1) {
+        expect(actionsCtrl.currentAction?.id).toEqual(2)
+      }
+    })
+
+    expect(actionsCtrl.actionsQueue).toHaveLength(2)
+    expect(actionsCtrl.visibleActionsQueue).toHaveLength(2)
+    actionsCtrl.setCurrentActionById(2)
+  })
+  test('should remove actions from actionsQueue', (done) => {
+    let emitCounter = 0
+    const unsubscribe = actionsCtrl.onUpdate(async () => {
+      emitCounter++
       if (emitCounter === 2) {
         expect(actionsCtrl.actionWindowId).toBe(null)
         expect(actionsCtrl.actionsQueue).toHaveLength(0)
@@ -127,7 +198,7 @@ describe('SignMessageController', () => {
         done()
       }
       if (emitCounter === 1) {
-        expect(actionsCtrl.actionWindowId).toEqual(null)
+        expect(actionsCtrl.actionWindowId).toEqual(2)
         expect(actionsCtrl.actionsQueue).toHaveLength(1)
         expect(actionsCtrl.currentAction?.id).toBe(2)
         actionsCtrl.removeAction(2)
