224679f4e599962050c88c822370f3014b9455aa
Yosif Hamed <josif.hameed@gmail.com>
2024-06-05 15:16:14 +0300

    Fully remove check for fetched tokens length 0 in portfolio to change

--- src/hooks/usePortfolio/usePortfolioFetch/useVelcroFetch.ts
+++ src/hooks/usePortfolio/usePortfolioFetch/useVelcroFetch.ts
@@ -311,9 +311,6 @@ export default function useVelcroFetch({
 
         // In case we have cached data from velcro - call balance oracle
         if ((!quickResponse && shouldSkipUpdate) || !tokensToUpdateBalance.length) {
-          // if (!formattedTokens?.length) {
-          //   formattedTokens = assets?.tokens
-          // }
           formattedTokens = removeDuplicatedAssets([
             ...(formattedTokens || []),
             ...(extraTokensAssets?.length ? extraTokensAssets : [])
