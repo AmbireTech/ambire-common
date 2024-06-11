a5a3e32fee1fab369d383192ffe24c977ce4d75e
Petromir Petrov <petromir@devlabs.bg>
2024-06-11 11:10:23 +0300

    delete: name field from CustomToken

--- src/libs/portfolio/customToken.ts
+++ src/libs/portfolio/customToken.ts
@@ -3,7 +3,6 @@ import { NetworkId } from '../../interfaces/networkDescriptor'
 export interface CustomToken {
   address: string
   isHidden?: boolean
-  name: string
   standard: string
   symbol: string
   decimals: number
