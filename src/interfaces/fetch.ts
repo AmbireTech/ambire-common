93cc5db41c61bf83639d8867e2e33ac171f8b573
Kaloyan Kosev <superkalo@devlabs.bg>
2024-06-13 18:11:29 +0300

    Add: x-api-key header + make all optional

--- src/interfaces/fetch.ts
+++ src/interfaces/fetch.ts
@@ -1,7 +1,8 @@
 // Re-use the global fetch type (type should be with the same footprint across all apps)
 interface RequestInitWithCustomHeaders extends RequestInit {
   headers: HeadersInit & {
-    'X-App-Source': string // the first 11 chars from the Keystore Uid
+    'x-app-source'?: string // the first 11 chars from the Keystore Uid
+    'x-api-key'?: string // JJiffy Scan API key
   }
 }
 
