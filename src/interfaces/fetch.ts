bd920cd85b301eeb42fd4c221d481c7b1b883c39
Kaloyan Kosev <superkalo@devlabs.bg>
2024-06-13 18:36:34 +0300

    Add: export fetch init type

--- src/interfaces/fetch.ts
+++ src/interfaces/fetch.ts
@@ -1,5 +1,5 @@
 // Re-use the global fetch type (type should be with the same footprint across all apps)
-interface RequestInitWithCustomHeaders extends RequestInit {
+export interface RequestInitWithCustomHeaders extends RequestInit {
   headers: HeadersInit & {
     'x-app-source'?: string // part (substring) of the Keystore Uid
     'x-api-key'?: string // JJiffy Scan API key
