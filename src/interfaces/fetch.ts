2a138cd1399b07d561dff2e26a347e4cfbce5e6d
Kaloyan Kosev <superkalo@devlabs.bg>
2024-06-13 17:44:48 +0300

    Add: custom header expected to the Fetch type

--- src/interfaces/fetch.ts
+++ src/interfaces/fetch.ts
@@ -1,2 +1,8 @@
 // Re-use the global fetch type (type should be with the same footprint across all apps)
-export type Fetch = typeof fetch
+interface RequestInitWithCustomHeaders extends RequestInit {
+  headers: HeadersInit & {
+    'X-App-Source': string // the first 11 chars from the Keystore Uid
+  }
+}
+
+export type Fetch = (input: RequestInfo, init?: RequestInitWithCustomHeaders) => Promise<Response>
