26cc8b4eed521befa8d825b8a76e23c834bc4116
Kaloyan Kosev <superkalo@devlabs.bg>
2024-06-13 18:23:41 +0300

    Change: update note

--- src/interfaces/fetch.ts
+++ src/interfaces/fetch.ts
@@ -1,7 +1,7 @@
 // Re-use the global fetch type (type should be with the same footprint across all apps)
 interface RequestInitWithCustomHeaders extends RequestInit {
   headers: HeadersInit & {
-    'x-app-source'?: string // the first 11 chars from the Keystore Uid
+    'x-app-source'?: string // part (substring) of the Keystore Uid
     'x-api-key'?: string // JJiffy Scan API key
   }
 }
