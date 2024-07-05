1625e865fe15ac67cfeabb699ad5c0f35266dc47
Kaloyan Kosev <superkalo@devlabs.bg>
2024-07-05 13:07:18 +0300

    Change: no longer needed readyToSign flag

--- src/controllers/signMessage/signMessage.ts
+++ src/controllers/signMessage/signMessage.ts
@@ -289,10 +289,6 @@ export class SignMessageController extends EventEmitter {
     await this.withStatus('sign', async () => this.#sign())
   }
 
-  get readyToSign() {
-    return this.isInitialized && this.signingKeyAddr && this.signingKeyType && this.messageToSign
-  }
-
   #throwNotInitialized() {
     this.emitError({
       level: 'major',
@@ -300,12 +296,5 @@ export class SignMessageController extends EventEmitter {
         'Looks like there is an error while processing your sign message. Please retry, or contact support if issue persists.',
       error: new Error('signMessage: controller not initialized')
     })
-  }
-
-  toJSON(): this & { emittedErrors: ErrorRef[] } {
-    return {
-      ...this,
-      readyToSign: this.readyToSign
-    }
   }
 }
