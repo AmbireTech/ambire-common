4757a7cb9e786f7b4a9ee6b0e59de64aed04ac5f
Yosif Hamed <josif.hameed@gmail.com>
2024-07-24 16:04:01 +0300

    Update args

--- src/services/humanizers/Paraswap.ts
+++ src/services/humanizers/Paraswap.ts
@@ -3,15 +3,23 @@
 import { Interface } from 'ethers/lib/utils'
 import { HumanizerInfoType } from 'hooks/useConstants'
 
+// eslint-disable-next-line import/no-cycle
 import { token } from '../humanReadableTransactions'
 
-const parseZeroAddressIfNeeded = (address) => {
+const parseZeroAddressIfNeeded = (address: string) => {
   return address.toLowerCase() === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
     ? '0x0000000000000000000000000000000000000000'
     : address
 }
 
-const getSwap = (humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended) =>
+const getSwap = (
+  humanizerInfo: HumanizerInfoType,
+  srcToken: string,
+  fromAmount: string,
+  destToken: string,
+  toAmount: bigint,
+  extended: bigint
+) =>
   extended
     ? [
         'Swap',
@@ -34,56 +42,56 @@ const getSwap = (humanizerInfo, srcToken, fromAmount, destToken, toAmount, exten
 const ParaswapMapping = (humanizerInfo: HumanizerInfoType) => {
   const iface = new Interface(humanizerInfo.abis.ParaswapRouter)
   return {
-    [iface.getSighash('swapExactAmountIn')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountIn')]: (txn, network, opts = {} as any) => {
       const {
         swapData: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountInOnCurveV1')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountInOnCurveV1')]: (txn, network, opts = {} as any) => {
       const {
         curveV1Data: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountInOnCurveV2')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountInOnCurveV2')]: (txn, network, opts = {} as any) => {
       const {
         curveV2Data: srcToken,
         destToken,
         fromAmount,
         toAmount
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountInOnUniswapV2')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountInOnUniswapV2')]: (txn, network, opts = {} as any) => {
       const {
         uniData: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountInOnUniswapV3')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountInOnUniswapV3')]: (txn, network, opts = {} as any) => {
       const {
         uniData: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountOut')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountOut')]: (txn, network, opts = {} as any) => {
       const {
         swapData: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountOutOnUniswapV2')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountOutOnUniswapV2')]: (txn, network, opts = {} as any) => {
       const {
         uniData: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     },
-    [iface.getSighash('swapExactAmountOutOnUniswapV3')]: (txn, network, { extended }) => {
+    [iface.getSighash('swapExactAmountOutOnUniswapV3')]: (txn, network, opts = {} as any) => {
       const {
         uniData: { srcToken, destToken, fromAmount, toAmount }
       } = iface.parseTransaction(txn).args
-      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, extended)]
+      return [getSwap(humanizerInfo, srcToken, fromAmount, destToken, toAmount, opts?.extended)]
     }
   }
 }
