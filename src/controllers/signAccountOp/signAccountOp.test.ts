b5f62fffebe6ef2ceb2eed56654b5d97d348c77e
Yosif Hamed <josif.hameed@gmail.com>
2024-08-15 14:24:09 +0300

    Remove blobal.structuredClone as it does not run locally

--- src/controllers/signAccountOp/signAccountOp.test.ts
+++ src/controllers/signAccountOp/signAccountOp.test.ts
@@ -37,7 +37,8 @@ const providers = Object.fromEntries(
   networks.map((network) => [network.id, getRpcProvider(network.rpcUrls, network.chainId)])
 )
 
-global.structuredClone = structuredClone as any
+// @TODO delete or uncomment this
+// global.structuredClone = structuredClone as any
 
 const createAccountOp = (
   account: Account,
