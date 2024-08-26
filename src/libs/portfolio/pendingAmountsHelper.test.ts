81b2a132d1eff97bd796e4db48b220b5420c28ae
Jordan Enev <jordantoen@gmail.com>
2024-08-26 18:12:28 +0300

    Add: `calculatePendingAmounts` tests and documentation.

---
+++ src/libs/portfolio/pendingAmountsHelper.test.ts
@@ -0,0 +1,170 @@
+import { expect } from '@jest/globals'
+import { calculatePendingAmounts } from './pendingAmountsHelper'
+
+describe('Portfolio -> Pending Amounts Helper', () => {
+  describe('calculatePendingAmounts', () => {
+    test('[PendingToBeSigned] - Having simulation and the AccOp is not broadcasted yet; should result in `pendingToBeSigned` badge', async () => {
+      const latestAmount = 2000000000000000000n
+      const pendingAmount = 2000000000000000000n
+      const priceUSD = 0.5
+      const decimals = 18
+      const amountPostSimulation = 1000000000000000000n
+      const activityNonce = 1n
+      const portfolioNonce = 2n
+
+      const simulationDelta = -1000000000000000000n
+
+      const result = calculatePendingAmounts(
+        latestAmount,
+        pendingAmount,
+        priceUSD,
+        decimals,
+        amountPostSimulation,
+        simulationDelta,
+        activityNonce,
+        portfolioNonce
+      )
+
+      expect(result).toEqual({
+        isPending: true,
+        pendingBalance: 1,
+        pendingBalanceUSD: 0.5,
+        pendingToBeSigned: -1000000000000000000n // Here we expect pendingToBeSigned property
+      })
+    })
+
+    test('[PendingToBeConfirmed] - Having simulation, but the AccOp is already broadcasted (but not confirmed); should result in pendingToBeConfirmed badge', async () => {
+      const latestAmount = 2000000000000000000n
+      const pendingAmount = 2000000000000000000n
+      const priceUSD = 0.5
+      const decimals = 18
+      const amountPostSimulation = 1000000000000000000n
+
+      // Once the AccOp is broadcasted and added to the Activity, there is a brief period
+      // where the Activity nonce matches the latest simulation nonce, indicating a `pendingToBeConfirmed` amount.
+      const activityNonce = 2n
+      const portfolioNonce = 2n
+
+      const simulationDelta = -1000000000000000000n
+
+      const result = calculatePendingAmounts(
+        latestAmount,
+        pendingAmount,
+        priceUSD,
+        decimals,
+        amountPostSimulation,
+        simulationDelta,
+        activityNonce,
+        portfolioNonce
+      )
+
+      expect(result).toEqual({
+        isPending: true,
+        pendingBalance: 1,
+        pendingBalanceUSD: 0.5,
+        pendingToBeConfirmed: -1000000000000000000n // Here we expect pendingToBeConfirmed property
+      })
+    })
+
+    test('[PendingToBeConfirmed] - Having difference between token latest and pending amounts; should result in `pendingToBeConfirmed` badge', async () => {
+      // Here we have a delta between latest and pending block amounts
+      const latestAmount = 1000000000000000000n
+      const pendingAmount = 2000000000000000000n
+
+      const priceUSD = 0.5
+      const decimals = 18
+      const amountPostSimulation = undefined
+      const activityNonce = 2n
+      const portfolioNonce = undefined
+      const simulationDelta = undefined
+
+      const result = calculatePendingAmounts(
+        latestAmount,
+        pendingAmount,
+        priceUSD,
+        decimals,
+        amountPostSimulation,
+        simulationDelta,
+        activityNonce,
+        portfolioNonce
+      )
+
+      expect(result).toEqual({
+        isPending: true,
+        pendingBalance: 2,
+        pendingBalanceUSD: 1,
+        pendingToBeConfirmed: 1000000000000000000n // Here we expect pendingToBeConfirmed property
+      })
+    })
+
+    test('[PendingToBeSigned], [PendingToBeConfirmed] - Having difference between token latest and pending amounts and having AccOp (not broadcasted); should result in both badges pendingToBeSigned and pendingToBeConfirmed', async () => {
+      // first latest/pending delta
+      const latestAmount = 2000000000000000000n
+      const pendingAmount = 3000000000000000000n
+
+      const priceUSD = 0.5
+      const decimals = 18
+      const amountPostSimulation = 4000000000000000000n
+      const activityNonce = 1n
+      const portfolioNonce = 2n
+
+      // AccOp simatulation delta
+      const simulationDelta = 1000000000000000000n
+
+      const result = calculatePendingAmounts(
+        latestAmount,
+        pendingAmount,
+        priceUSD,
+        decimals,
+        amountPostSimulation,
+        simulationDelta,
+        activityNonce,
+        portfolioNonce
+      )
+
+      expect(result).toEqual({
+        isPending: true,
+        pendingBalance: 4,
+        pendingBalanceUSD: 2,
+        pendingToBeSigned: 1000000000000000000n, // Here we expect pendingToBeSigned property
+        pendingToBeConfirmed: 1000000000000000000n // Here we expect pendingToBeConfirmed property
+      })
+    })
+
+    test('[PendingToBeConfirmed] - Having difference between token latest and pending amounts and having AccOp the same time (broadcasted, but not confirmed); should result in `pendingToBeConfirmed` badge', async () => {
+      // Here we have a delta between latest and pending block amounts
+      const latestAmount = 2000000000000000000n
+      const pendingAmount = 3000000000000000000n
+
+      const priceUSD = 0.5
+      const decimals = 18
+      const amountPostSimulation = 4000000000000000000n
+
+      // Here we know that having the same nonces, it means we have BroadcastedButNotConfirmed AccOp
+      const activityNonce = 2n
+      const portfolioNonce = 2n
+
+      // We know the sumulatioh delta
+      const simulationDelta = 1000000000000000000n
+
+      const result = calculatePendingAmounts(
+        latestAmount,
+        pendingAmount,
+        priceUSD,
+        decimals,
+        amountPostSimulation,
+        simulationDelta,
+        activityNonce,
+        portfolioNonce
+      )
+
+      expect(result).toEqual({
+        isPending: true,
+        pendingBalance: 4,
+        pendingBalanceUSD: 2,
+        // We expect the sum of both deltas here: `latestPendingDelta` + `simulationDelta`
+        pendingToBeConfirmed: 2000000000000000000n // Here we expect pendingToBeConfirmed property
+      })
+    })
+  })
+})
--- src/libs/portfolio/pendingAmountsHelper.ts
+++ src/libs/portfolio/pendingAmountsHelper.ts
@@ -1,8 +1,43 @@
 import { formatUnits } from 'ethers'
 
-// TODO: Determining the token's pending state (pending-to-be-signed or pending-to-be-confirmed) is quite complex,
-//  with over six possible variations. We should thoroughly document all potential scenarios.
-//  Additionally, creating unit tests for this function is highly recommended.
+type PendingAmounts = {
+  isPending: boolean
+  pendingBalance: number // TODO, better to return bigint
+  pendingBalanceUSD: number // TODO, move it to formatPendingAmounts helper
+  pendingToBeSigned?: bigint
+  pendingToBeConfirmed?: bigint
+}
+
+/**
+ * Function for calculating the pending balance and the delta amounts
+ * for pendingToBeSigned and pendingToBeConfirmed states (referred to as token badges) of a token.
+ *
+ * While calculating the balance is a routine operation,
+ * calculating the pending delta and determining its state (whether it needs to be signed or confirmed)
+ * can be quite challenging.
+ *
+ * We use this information to display PendingToBeSigned, PendingToBeConfirmed, or both badges on the Dashboard.
+ *
+ * Let's review the main scenarios where we encounter delta amounts, followed by a discussion of corner cases.
+ *
+ * Main scenarios:
+ * 1. If there is an AccOp that has not yet been signed, we return the `pendingToBeSigned` amount.
+ * 2. If there is an AccOp that has been signed and broadcasted (but not yet confirmed),
+ *    we display the `pendingToBeConfirmed` badge, reflecting the corresponding amount.
+ * 3. If we detect a delta between the pending and latest token amounts, it indicates that there is something awaiting confirmation.
+ *    In this case, we also return the `pendingToBeConfirmed` amount.
+ *    For example, if someone sends you tokens outside of the extension, the extension will show the `pendingToBeConfirmed` amount.
+ *
+ * Rare scenarios:
+ * 1. It is possible to have both `pendingToBeSigned` and `pendingToBeConfirmed` badges in the following scenario:
+ *    - If someone sends you tokens, the `latestPendingDelta` is triggered.
+ *      At the same time, if you have an AccOp waiting to be signed, both badges will be displayed,
+ *       and the amounts for `pendingToBeSigned` and `pendingToBeConfirmed` will be calculated.
+ * 2. If you sign and broadcast an AccOp while the `latestPendingDelta` also has a value,
+ *    the `pendingToBeConfirmed` badge will be displayed, representing the sum of both `simulationDelta` and `pendingLatestDelta`.
+ *    - This is similar to the previous case, but here the AccOp has been broadcasted.
+ *      When there is a delta between the latest and pending block amounts, both deltas are summed, resulting in the `pendingToBeConfirmed` amount.
+ */
 export const calculatePendingAmounts = (
   latestAmount: bigint,
   pendingAmount: bigint,
@@ -12,15 +47,15 @@ export const calculatePendingAmounts = (
   simulationDelta?: bigint, // pending delta (this is the amount of the simulation itself)
   activityNonce?: bigint,
   portfolioNonce?: bigint
-) => {
-  const onChainToBeConfirmedDelta = pendingAmount - latestAmount
+): PendingAmounts | {} => {
+  const latestPendingDelta = pendingAmount - latestAmount
 
   // There is no Pending state changes
-  if (onChainToBeConfirmedDelta === 0n && !simulationDelta) return {}
+  if (latestPendingDelta === 0n && !simulationDelta) return {}
 
   let pendingBalance
 
-  if (onChainToBeConfirmedDelta && !amountPostSimulation) {
+  if (latestPendingDelta && !amountPostSimulation) {
     pendingBalance = parseFloat(formatUnits(pendingAmount, decimals))
   } else {
     pendingBalance = parseFloat(formatUnits(amountPostSimulation!, decimals))
@@ -35,6 +70,12 @@ export const calculatePendingAmounts = (
   }
 
   if (simulationDelta) {
+    // When an AccOp has not yet been signed and broadcasted, it is not added to the ActivityController.
+    // As a result, the latest known ActivityController nonce will always be lower than the Portfolio's pending simulation nonce.
+    // In this scenario, we know there is a `pendingToBeSigned` amount.
+    // However, once the AccOp is broadcasted and added to the Activity, there is a brief period
+    // where the Activity nonce matches the latest simulation nonce, indicating a `pendingToBeConfirmed` amount.
+    // Once the AccOp is confirmed by the network, the portfolio is updated, the simulation is cleared, and no badges are displayed.
     const hasPendingToBeConfirmed = activityNonce && activityNonce === portfolioNonce
 
     if (hasPendingToBeConfirmed) {
@@ -44,10 +85,11 @@ export const calculatePendingAmounts = (
     }
   }
 
-  if (onChainToBeConfirmedDelta) {
+  // Please refer to the function documentation, rare case #2
+  if (latestPendingDelta) {
     const pendingToBeConfirmed = result.pendingToBeConfirmed
-      ? result.pendingToBeConfirmed + onChainToBeConfirmedDelta
-      : onChainToBeConfirmedDelta
+      ? result.pendingToBeConfirmed + latestPendingDelta
+      : latestPendingDelta
     result.pendingToBeConfirmed = pendingToBeConfirmed
   }
 
