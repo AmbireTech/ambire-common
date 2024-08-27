e23c8268f4f1589eb5abd721be43a95c61ab09a8
Yosif Hamed <josif.hameed@gmail.com>
2024-08-27 16:00:26 +0300

    minor fix for gas tank humanizermodule

--- src/libs/humanizer/modules/GasTankModule/gasTankModule.ts
+++ src/libs/humanizer/modules/GasTankModule/gasTankModule.ts
@@ -8,7 +8,7 @@ import { getAction, getToken } from '../../utils'
 export const gasTankModule: HumanizerCallModule = (_: AccountOp, irCalls: IrCall[]) => {
   const newCalls = irCalls.map((call) => {
     // @TODO fix those upper/lowercase
-    if (call.to.toLowerCase() === FEE_COLLECTOR.toLowerCase())
+    if (call.to.toLowerCase() === FEE_COLLECTOR.toLowerCase() && call.value > 0n)
       return {
         ...call,
         fullVisualization: [getAction('Fuel gas tank with'), getToken(ZeroAddress, call.value)]
