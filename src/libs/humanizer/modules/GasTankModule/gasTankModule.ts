1ef015037c58e67fff492dd4c3dc947accfef696
Yosif Hamed <josif.hameed@gmail.com>
2024-08-27 16:21:33 +0300

    Add test

--- src/libs/humanizer/modules/GasTankModule/gasTankModule.ts
+++ src/libs/humanizer/modules/GasTankModule/gasTankModule.ts
@@ -1,4 +1,4 @@
-import { ZeroAddress } from 'ethers'
+import { AbiCoder, ZeroAddress } from 'ethers'
 
 import { FEE_COLLECTOR } from '../../../../consts/addresses'
 import { AccountOp } from '../../../accountOp/accountOp'
@@ -8,12 +8,22 @@ import { getAction, getToken } from '../../utils'
 export const gasTankModule: HumanizerCallModule = (_: AccountOp, irCalls: IrCall[]) => {
   const newCalls = irCalls.map((call) => {
     // @TODO fix those upper/lowercase
-    if (call.to.toLowerCase() === FEE_COLLECTOR.toLowerCase() && call.value > 0n)
-      return {
-        ...call,
-        fullVisualization: [getAction('Fuel gas tank with'), getToken(ZeroAddress, call.value)]
+    if (call.to.toLowerCase() === FEE_COLLECTOR.toLowerCase()) {
+      if (call.value > 0n) {
+        return {
+          ...call,
+          fullVisualization: [getAction('Fuel gas tank with'), getToken(ZeroAddress, call.value)]
+        }
       }
-    if (
+      try {
+        const [text] = new AbiCoder().decode(['string', 'uint256', 'string'], call.data)
+        // mostly useful for filtering out call in benzin
+        if (text === 'gasTank')
+          return { ...call, fullVisualization: [getAction('Pay feee with gas tank')] }
+      } catch (e) {
+        // do nothing
+      }
+    } else if (
       call.fullVisualization?.[0]?.content === 'Send' &&
       call.fullVisualization?.[1]?.type === 'token' &&
       call.fullVisualization?.[2]?.content === 'to' &&
