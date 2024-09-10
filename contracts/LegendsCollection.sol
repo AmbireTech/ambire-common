2f5ba5605832eafffa4aa1a9855a526296ded3cb
Yosif Hamed <josif.hameed@gmail.com>
2024-09-10 17:11:43 +0300

    add setBaseURI

--- contracts/LegendsCollection.sol
+++ contracts/LegendsCollection.sol
@@ -2,15 +2,32 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol"; 
 
-contract Legends is ERC721Enumerable {
-    constructor() ERC721("AmbireLegends", "AML") {}
+using Strings for uint256;
+
+contract Legends is IERC721Metadata, ERC721Enumerable, Ownable {
+
+    constructor() ERC721("AmbireLegends", "AML") Ownable(msg.sender) {}
+    
+    string baseURI;
 
     function mint() public {
-        require(balanceOf(msg.sender) == 0, "NFT already minted");
-        // first minted nft is 1
-        _mint(msg.sender, totalSupply());
+        // single mint allowed
+        // using address for front-end simplification
+        _mint(msg.sender, uint256(uint160(msg.sender)));
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, IERC721Metadata) returns (string memory) {
+        _requireOwned(tokenId);
+
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString()) : "";
+    }
+
+    function setBaseUri(string calldata _baseURI) public onlyOwner {
+        baseURI = _baseURI;
     }
 
     // Soulbound
