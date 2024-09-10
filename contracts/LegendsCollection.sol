59b9f33c515a1d7dd42896558190d729ad4485c0
Yosif Hamed <josif.hameed@gmail.com>
2024-09-10 16:17:45 +0300

    Add enumerable

--- contracts/LegendsCollection.sol
+++ contracts/LegendsCollection.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
-contract Legends is  ERC721 {
+contract Legends is  ERC721, IERC721Enumerable {
 
     error ERC721OutOfBoundsIndex(address owner, uint256 index);
     
-    uint totalSupply = 0;    
+    uint _totalSupply = 0;    
     mapping(address => uint) _owns;
     
     constructor() ERC721("AmbireLegends", "AML") {}
@@ -15,11 +16,21 @@ contract Legends is  ERC721 {
     function mint() public {
         require(balanceOf(msg.sender) == 0, "NFT already minted");
         // first minted nft is 1
-        _mint(address(msg.sender), ++totalSupply);
-        _owns[msg.sender] = totalSupply;
+        _mint(msg.sender, ++_totalSupply);
+        _owns[msg.sender] = _totalSupply;
     }
 
+    // function supportsInterface(interfaceId) pure returns(bool){
+    //     return ERC721;
+    // }
+
     // Enumerable
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+
     function tokenOfOwnerByIndex(address owner, uint256 index) public view virtual returns (uint256) {
         // 1 nft per user at most
         require( index == 0 && balanceOf(owner) != 0, ERC721OutOfBoundsIndex(owner, index));
@@ -31,23 +42,23 @@ contract Legends is  ERC721 {
     }
 
     // Soulbound
-    function approve(address to, uint256 tokenId) public view override {
+    function approve(address to, uint256 tokenId) public view override(IERC721,ERC721) {
         revert("Soulbound: cannot approve token transfer");
     }
 
-    function setApprovalForAll(address operator, bool approved) public view override {
+    function setApprovalForAll(address operator, bool approved) public view override(IERC721,ERC721)  {
         revert("Soulbound: cannot set approval for all");
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public view override {
+    function transferFrom(address from, address to, uint256 tokenId) public view override(IERC721,ERC721)  {
         revert("Soulbound: cannot transfer nft");
     }
 
-    function safeTransferFrom(address from, address to, uint256 tokenId) public view override {
+    function safeTransferFrom(address from, address to, uint256 tokenId) public view override(IERC721,ERC721)  {
         revert("Soulbound: cannot transfer nft");
     }
 
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public view override {
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public view override(IERC721,ERC721)  {
         revert("Soulbound: cannot transfer nft");
     }
 
