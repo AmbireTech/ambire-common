275d55e5094aed89dd4e0b40c1adb5cba40dec5c
Kaloyan Kosev <superkalo@devlabs.bg>
2024-09-18 12:16:02 +0300

    Add: from token addr and network

--- src/controllers/swapAndBridge/swapAndBridge.ts
+++ src/controllers/swapAndBridge/swapAndBridge.ts
@@ -1,8 +1,14 @@
+import { TokenResult } from '../../libs/portfolio'
 import EventEmitter from '../eventEmitter/eventEmitter'
 
 export class SwapAndBridgeController extends EventEmitter {
+  fromTokenAddress: TokenResult['address'] | null = null
+
+  fromTokenNetwork: TokenResult['networkId'] | null = null
+
   constructor() {
     super()
+
     this.emitUpdate()
   }
 
