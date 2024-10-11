fb85e850415318fdd41a5f081bc44d90f58c6a52
Kaloyan Kosev <superkalo@devlabs.bg>
2024-10-11 18:32:06 +0300

    Add: public (dummy) socket api key

--- src/controllers/main/main.test.ts
+++ src/controllers/main/main.test.ts
@@ -17,6 +17,9 @@ import { getBytecode } from '../../libs/proxyDeploy/bytecode'
 import { getAmbireAccountAddress } from '../../libs/proxyDeploy/getAmbireAddressTwo'
 import { MainController } from './main'
 
+// Public API key, shared by Socket, for testing purposes only
+const socketApiKey = '72a5b4b0-e727-48be-8aa1-5da9d62fe635'
+
 const windowManager = {
   focus: () => Promise.resolve(),
   open: () => Promise.resolve(0),
@@ -72,6 +75,7 @@ describe('Main Controller ', () => {
       storage,
       fetch,
       relayerUrl,
+      socketApiKey,
       keystoreSigners: { internal: KeystoreSigner },
       externalSignerControllers: {},
       windowManager,
@@ -202,6 +206,7 @@ describe('Main Controller ', () => {
       storage,
       fetch,
       relayerUrl,
+      socketApiKey,
       windowManager,
       notificationManager,
       keystoreSigners: { internal: KeystoreSigner },
@@ -287,6 +292,7 @@ describe('Main Controller ', () => {
       storage,
       fetch,
       relayerUrl,
+      socketApiKey,
       windowManager,
       notificationManager,
       keystoreSigners: { internal: KeystoreSigner },
