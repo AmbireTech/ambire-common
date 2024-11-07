2e29c2ec4e612b68f0d1b59e7ede9d4187ce30ac
Petromir Petrov <petromirdev@gmail.com>
2024-11-07 12:04:26 +0200

    fix: defi positions unit tests

--- src/libs/defiPositions/defiPositions.test.ts
+++ src/libs/defiPositions/defiPositions.test.ts
@@ -3,9 +3,8 @@ import { JsonRpcProvider } from 'ethers'
 import { describe, expect, test } from '@jest/globals'
 
 import { networks } from '../../consts/networks'
-import { getAAVEPositions } from './aaveV3'
-import { Position } from './types'
-import { getUniV3Positions } from './uniV3'
+import { getAAVEPositions, getUniV3Positions } from './providers'
+import { PositionsByProvider } from './types'
 
 describe('Portfolio', () => {
   const userAddrUni = '0xC2E6dFcc2C6722866aD65F211D5757e1D2879337'
@@ -21,7 +20,7 @@ describe('Portfolio', () => {
   const providerPolygon = new JsonRpcProvider('https://invictus.ambire.com/polygon')
 
   test(`get aave positions for ${userAddrAave} on Optimism`, async () => {
-    const aavePositions: Position[] | null = await getAAVEPositions(
+    const aavePositions: PositionsByProvider | null = await getAAVEPositions(
       userAddrAave,
       providerOptimism,
       optimism
@@ -29,13 +28,13 @@ describe('Portfolio', () => {
 
     expect(aavePositions).not.toBeNull()
     if (aavePositions !== null) {
-      const pos1 = aavePositions[0]
+      const pos1 = aavePositions.positions[0]
       expect(pos1.additionalData.healthRate).toBeGreaterThan(1)
     }
   })
 
   test(`get uni v3 positions for ${userAddrUni} on Polygon`, async () => {
-    const uniV3Positions: Position[] | null = await getUniV3Positions(
+    const uniV3Positions: PositionsByProvider | null = await getUniV3Positions(
       userAddrUni,
       providerPolygon,
       polygon
@@ -43,7 +42,7 @@ describe('Portfolio', () => {
     expect(uniV3Positions).not.toBeNull()
 
     if (uniV3Positions !== null) {
-      expect(uniV3Positions[0].additionalData.inRange).toBe(false)
+      expect(uniV3Positions.positions[0].additionalData.inRange).toBe(false)
     }
   })
 })
