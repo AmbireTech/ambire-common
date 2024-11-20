5d3ee2cacd9cedfd8a47986611a00e326f6db666
Petromir Petrov <petromirdev@gmail.com>
2024-11-20 17:32:10 +0200

    update: generic message for panic/revert errors to be contract error

--- src/libs/errorHumanizer/humanizeCommonCases.ts
+++ src/libs/errorHumanizer/humanizeCommonCases.ts
@@ -23,16 +23,16 @@ function getGenericMessageFromType(
       return `${messagePrefix} of a Paymaster error.${messageSuffix}`
     case ErrorType.RpcError:
       return `${messagePrefix} of an RPC error.${messageSuffix}`
-    case ErrorType.PanicError:
-      return `${messagePrefix} of a panic error.${messageSuffix}`
     case ErrorType.BundlerError:
       return `${messagePrefix} of a Bundler error.${messageSuffix}`
     case ErrorType.UnknownError:
       return `${messagePrefix} of an unknown error.${messageSuffix}`
     case ErrorType.InnerCallFailureError:
       return `${messagePrefix} of a failure while validating the transaction.${messageSuffix}`
+    // Panic error may scare the user so let's call it a contract error
+    case ErrorType.PanicError:
     case ErrorType.RevertError:
-      return `${messagePrefix} of a revert error.${messageSuffix}`
+      return `${messagePrefix} of a contract error.${messageSuffix}`
     default:
       return lastResortMessage
   }
