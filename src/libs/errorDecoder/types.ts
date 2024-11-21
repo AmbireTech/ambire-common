d622e8b0059fad5b90f88cd61ce2983c6e436fe2
Petromir Petrov <petromirdev@gmail.com>
2024-11-21 09:52:40 +0200

    docs: errorDecoder ErrorType docs

--- src/libs/errorDecoder/types.ts
+++ src/libs/errorDecoder/types.ts
@@ -1,12 +1,21 @@
 export enum ErrorType {
+  /** Error due to contract reverting, identified by prefix 0x08c379a0 */
   RevertError = 'RevertError',
+  /** Error due to contract panic, identified by prefix 0x4e487b71 */
   PanicError = 'PanicError',
+  /** Error originating from a relayer call */
   RelayerError = 'RelayerError',
+  /** Error originating from the Paymaster (our Relayer) */
   PaymasterError = 'PaymasterError',
+  /** Error during bundler estimation or broadcast */
   BundlerError = 'BundlerError',
+  /** Error from an RPC call */
   RpcError = 'RpcError',
+  /** Error that cannot be decoded */
   UnknownError = 'UnknownError',
+  /** Error due to the user rejecting a transaction */
   UserRejectionHandler = 'UserRejectionHandler',
+  /** Error due to an inner call failure during estimation */
   InnerCallFailureError = 'InnerCallFailureError'
 }
 
