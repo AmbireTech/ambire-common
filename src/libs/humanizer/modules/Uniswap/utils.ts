cc61ebecfbd85f6d5e135d72db79374ef5898cde
Yosif Hamed <josif.hameed@gmail.com>
2024-11-22 20:53:56 +0200

    simplify uniswap reducer

--- src/libs/humanizer/modules/Uniswap/utils.ts
+++ src/libs/humanizer/modules/Uniswap/utils.ts
@@ -54,32 +54,38 @@ const isSend = (call: HumanizerVisualization[] | undefined) =>
   call[2]?.content?.includes('to') &&
   call[3].type === 'address'
 export const uniReduce = (_calls: HumanizerVisualization[][]): HumanizerVisualization[] => {
-  const calls: (HumanizerVisualization[] | undefined)[] = _calls
+  const calls = _calls
+  const originalCallsLength = calls.length
   for (let i = 0; i < calls.length; i++) {
-    let doneFlag = false
     // looks for wraps before the swap
-    for (let j = 0; j < i; j++) {
-      if (isSwap(calls[i]) && isWrap(calls[j]) && calls[j]![1].value === calls[i]![1].value) {
-        calls[i]![1].address = ZeroAddress
-        delete calls[j]
-        doneFlag = true
-        break
-      }
-    }
-    if (doneFlag) continue
-
-    for (let j = i + 1; j < calls.length; j++) {
-      if (isSwap(calls[i]) && isUnwrap(calls[j]) && calls[j]![1].value === calls[i]![3].value) {
-        calls[i]![3].address = ZeroAddress
-        delete calls[j]
-        doneFlag = true
-        break
-      }
-    }
-    if (doneFlag) continue
     for (let j = 0; j < calls.length; j++) {
       if (
+        j < i &&
+        calls[i] &&
+        calls[j] &&
+        isSwap(calls[i]) &&
+        isWrap(calls[j]) &&
+        calls[j]![1].value === calls[i]![1].value
+      ) {
+        calls[i]![1].address = ZeroAddress
+        calls.splice(j, 1)
+      }
+
+      if (
+        calls[i] &&
+        calls[j] &&
+        isSwap(calls[i]) &&
+        isUnwrap(calls[j]) &&
+        calls[j]![1].value === calls[i]![3].value
+      ) {
+        calls[i]![3].address = ZeroAddress
+        calls.splice(j, 1)
+      }
+
+      if (
         i !== j &&
+        calls[i] &&
+        calls[j] &&
         isSwap(calls[i]!) &&
         isSwap(calls[j]!) &&
         calls[i]![1].address === calls[j]![1].address &&
@@ -87,32 +93,33 @@ export const uniReduce = (_calls: HumanizerVisualization[][]): HumanizerVisualiz
       ) {
         calls[i]![1].value = calls[i]![1].value! + calls[j]![1].value!
         calls[i]![3].value = calls[i]![3].value! + calls[j]![3].value!
-        delete calls[j]
+        calls.splice(j, 1)
       }
 
       if (
+        i !== j &&
+        calls[i] &&
+        calls[j] &&
         isSend(calls[j]) &&
         isSwap(calls[i]!) &&
         calls[i]![3].value! / 400n >= calls[j]![1].value!
       ) {
         calls[i]![3].value = calls[i]![3].value! - calls[j]![1].value!
-        delete calls[j]
+        calls.splice(j, 1)
       }
-    }
 
-    if (doneFlag) continue
-    for (let j = 0; j < calls.length; j++) {
       if (
         i !== j &&
+        calls[i] &&
+        calls[j] &&
         isSwap(calls[i]!) &&
         isTake(calls[j]!) &&
         calls[i]![3].address === calls[j]![2].address
       ) {
         calls[i]![3].value = calls[j]![2].value!
-        delete calls[j]
+        calls.splice(j, 1)
       }
     }
   }
-  const res = calls.filter((x) => x) as HumanizerVisualization[][]
-  return res.length === calls.length ? joinWithAndLabel(res) : uniReduce(res)
+  return originalCallsLength === calls.length ? joinWithAndLabel(calls) : uniReduce(calls)
 }
