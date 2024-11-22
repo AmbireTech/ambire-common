1aed0544e05f141b908fe34f08419a84c148a142
Petromir Petrov <petromirdev@gmail.com>
2024-11-22 14:45:12 +0200

    update: getBenzinUrlParams in common and open benzin from activity banner

---
+++ src/utils/benzin.ts
@@ -0,0 +1,29 @@
+import {
+  AccountOpIdentifiedBy,
+  isIdentifiedByRelayer,
+  isIdentifiedByUserOpHash
+} from '../libs/accountOp/submittedAccountOp'
+
+const getBenzinUrlParams = ({
+  chainId,
+  txnId,
+  identifiedBy,
+  isInternal
+}: {
+  chainId?: string | number | bigint
+  txnId?: string | null
+  identifiedBy?: AccountOpIdentifiedBy
+  isInternal?: boolean
+}): string => {
+  const userOpHash =
+    identifiedBy && isIdentifiedByUserOpHash(identifiedBy) ? identifiedBy.identifier : undefined
+
+  const relayerId =
+    identifiedBy && isIdentifiedByRelayer(identifiedBy) ? identifiedBy.identifier : undefined
+
+  return `?chainId=${String(chainId)}${txnId ? `&txnId=${txnId}` : ''}${
+    userOpHash ? `&userOpHash=${userOpHash}` : ''
+  }${relayerId ? `&relayerId=${relayerId}` : ''}${isInternal ? '&isInternal' : ''}`
+}
+
+export { getBenzinUrlParams }
