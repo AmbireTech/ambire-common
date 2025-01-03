b73cf64b7eeb7ad6a75b946586d554ec738c477f
Yosif Hamed <josif.hameed@gmail.com>
2025-01-03 17:52:08 +0200

    docs: fix test names for humanizer for ambir eoperations

--- src/libs/humanizer/modules/embeddedAmbireOperationHumanizer/ambireOperation.test.ts
+++ src/libs/humanizer/modules/embeddedAmbireOperationHumanizer/ambireOperation.test.ts
@@ -15,8 +15,8 @@ const iface = new Interface([
   ...AmbireAccount,
   'function transfer(address recipient, uint256 amount)'
 ])
-describe('Legends', () => {
-  it('Linking, both invitation and no invitation', () => {
+describe('Hidden ambire operations', () => {
+  it('Ambire transactions', () => {
     const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
     const transactionsToWrap = [
       {
