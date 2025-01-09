e6fc27358246b660068f4335aec5b9bd149077f1
Kaloyan Kosev <superkalo@devlabs.bg>
2025-01-09 10:26:15 +0200

    Change: sort swap and bridge Receive tokens highest value first

--- src/libs/swapAndBridge/swapAndBridge.ts
+++ src/libs/swapAndBridge/swapAndBridge.ts
@@ -16,25 +16,31 @@ import { SignUserRequest } from '../../interfaces/userRequest'
 import { isSmartAccount } from '../account/account'
 import { Call } from '../accountOp/types'
 import { TokenResult } from '../portfolio'
+import { getTokenDetails } from '../portfolio/token'
 
 export const sortTokenListResponse = (
   tokenListResponse: SocketAPIToken[],
   accountPortfolioTokenList: TokenResult[]
 ) => {
-  return (
-    tokenListResponse
-      // Alphabetically, by project name (not token symbol)
-      .sort((a: SocketAPIToken, b: SocketAPIToken) => a.name?.localeCompare(b?.name))
-      // Sort fist the tokens that exist in the account portfolio
-      .sort((a: SocketAPIToken, b: SocketAPIToken) => {
-        const aInPortfolio = accountPortfolioTokenList.some((t) => t.address === a.address)
-        const bInPortfolio = accountPortfolioTokenList.some((t) => t.address === b.address)
+  return tokenListResponse.sort((a: SocketAPIToken, b: SocketAPIToken) => {
+    const aInPortfolio = accountPortfolioTokenList.find((t) => t.address === a.address)
+    const bInPortfolio = accountPortfolioTokenList.find((t) => t.address === b.address)
 
-        if (aInPortfolio && !bInPortfolio) return -1
-        if (!aInPortfolio && bInPortfolio) return 1
-        return 0 // retain the alphabetical order
-      })
-  )
+    // Tokens in portfolio should come first
+    if (aInPortfolio && !bInPortfolio) return -1
+    if (!aInPortfolio && bInPortfolio) return 1
+
+    // If both are in the portfolio, sort by highest balance
+    if (aInPortfolio && bInPortfolio) {
+      const aBalanceUSD = getTokenDetails(aInPortfolio).balanceUSD || 0
+      const bBalanceUSD = getTokenDetails(bInPortfolio).balanceUSD || 0
+
+      if (aBalanceUSD !== bBalanceUSD) return bBalanceUSD - aBalanceUSD
+    }
+
+    // Otherwise, just alphabetical
+    return (a.name || '').localeCompare(b.name || '')
+  })
 }
 
 export const convertPortfolioTokenToSocketAPIToken = (
