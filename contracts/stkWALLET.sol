c8332f70b5b76f596226f82a5ac9fe4951390fba
Yosif Hamed <josif.hameed@gmail.com>
2025-01-21 17:53:39 +0200

    style: linting stkWallet

--- contracts/stkWALLET.sol
+++ contracts/stkWALLET.sol
@@ -2,97 +2,100 @@
 pragma solidity ^0.8.19;
 
 interface IXWallet {
-	function balanceOf(address owner) external view returns (uint256);
-	function shareValue() external view returns (uint256);
-	function transfer(address to, uint256 amount) external returns (bool);
-	function transferFrom(address from, address to, uint256 amount) external returns (bool);
+  function balanceOf(address owner) external view returns (uint256);
+
+  function shareValue() external view returns (uint256);
+
+  function transfer(address to, uint256 amount) external returns (bool);
+
+  function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
 contract stkWALLET {
-	// ERC20 stuff
-	// Constants
-	string public constant name = "Staked $WALLET";
-	uint8 public constant decimals = 18;
-	string public constant symbol = "stkWALLET";
+  // ERC20 stuff
+  // Constants
+  string public constant name = 'Staked $WALLET';
+  uint8 public constant decimals = 18;
+  string public constant symbol = 'stkWALLET';
 
-	// Immutables
-	IXWallet xWallet;
+  // Immutables
+  IXWallet xWallet;
 
-	// Mutable variables
-	mapping(address => uint256) public shares;
-	mapping(address => mapping(address => uint256)) private allowed;
+  // Mutable variables
+  mapping(address => uint256) public shares;
+  mapping(address => mapping(address => uint256)) private allowed;
 
-	// ERC20 events
-	event Approval(address indexed owner, address indexed spender, uint256 amount);
-	event Transfer(address indexed from, address indexed to, uint256 amount);
-	// Custom events
-	event ShareValueUpdate(uint256 shareValue);
+  // ERC20 events
+  event Approval(address indexed owner, address indexed spender, uint256 amount);
+  event Transfer(address indexed from, address indexed to, uint256 amount);
+  // Custom events
+  event ShareValueUpdate(uint256 shareValue);
 
-	// ERC20 methods
-	// Note: any xWALLET sent to this contract will be burned as there's nothing that can be done with it. Expected behavior.
-	function totalSupply() external view returns (uint256) {
-		return (xWallet.balanceOf(address(this)) * xWallet.shareValue()) / 1e18;
-	}
+  // ERC20 methods
+  // Note: any xWALLET sent to this contract will be burned as there's nothing that can be done with it. Expected behavior.
+  function totalSupply() external view returns (uint256) {
+    return (xWallet.balanceOf(address(this)) * xWallet.shareValue()) / 1e18;
+  }
 
-	function balanceOf(address owner) external view returns (uint256 balance) {
-		return (shares[owner] * xWallet.shareValue()) / 1e18;
-	}
+  function balanceOf(address owner) external view returns (uint256 balance) {
+    return (shares[owner] * xWallet.shareValue()) / 1e18;
+  }
 
-	function transfer(address to, uint256 amount) external returns (bool success) {
-		require(to != address(this) && to != address(0), "BAD_ADDRESS");
-		uint256 shareValue = xWallet.shareValue();
-		uint256 sharesAmount = (amount * 1e18) / shareValue;
-		shares[msg.sender] = shares[msg.sender] - sharesAmount;
-		shares[to] = shares[to] + sharesAmount;
-		emit Transfer(msg.sender, to, amount);
-		emit ShareValueUpdate(shareValue);
-		return true;
-	}
+  function transfer(address to, uint256 amount) external returns (bool success) {
+    require(to != address(this) && to != address(0), 'BAD_ADDRESS');
+    uint256 shareValue = xWallet.shareValue();
+    uint256 sharesAmount = (amount * 1e18) / shareValue;
+    shares[msg.sender] = shares[msg.sender] - sharesAmount;
+    shares[to] = shares[to] + sharesAmount;
+    emit Transfer(msg.sender, to, amount);
+    emit ShareValueUpdate(shareValue);
+    return true;
+  }
 
-	function transferFrom(address from, address to, uint256 amount) external returns (bool success) {
-		require(to != address(this) && to != address(0), "BAD_ADDRESS");
-		uint256 shareValue = xWallet.shareValue();
-		uint256 sharesAmount = (amount * 1e18) / shareValue;
-		shares[from] = shares[from] - sharesAmount;
-		uint256 prevAllowance = allowed[from][msg.sender];
-		if (prevAllowance < type(uint256).max) allowed[from][msg.sender] = prevAllowance - amount;
-		shares[to] = shares[to] + sharesAmount;
-		emit Transfer(from, to, amount);
-		emit ShareValueUpdate(shareValue);
-		return true;
-	}
+  function transferFrom(address from, address to, uint256 amount) external returns (bool success) {
+    require(to != address(this) && to != address(0), 'BAD_ADDRESS');
+    uint256 shareValue = xWallet.shareValue();
+    uint256 sharesAmount = (amount * 1e18) / shareValue;
+    shares[from] = shares[from] - sharesAmount;
+    uint256 prevAllowance = allowed[from][msg.sender];
+    if (prevAllowance < type(uint256).max) allowed[from][msg.sender] = prevAllowance - amount;
+    shares[to] = shares[to] + sharesAmount;
+    emit Transfer(from, to, amount);
+    emit ShareValueUpdate(shareValue);
+    return true;
+  }
 
-	function approve(address spender, uint256 amount) external returns (bool success) {
-		allowed[msg.sender][spender] = amount;
-		emit Approval(msg.sender, spender, amount);
-		return true;
-	}
+  function approve(address spender, uint256 amount) external returns (bool success) {
+    allowed[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
+    return true;
+  }
 
-	function allowance(address owner, address spender) external view returns (uint256 remaining) {
-		return allowed[owner][spender];
-	}
+  function allowance(address owner, address spender) external view returns (uint256 remaining) {
+    return allowed[owner][spender];
+  }
 
-	constructor(IXWallet token) {
-		xWallet = token;
-	}
+  constructor(IXWallet token) {
+    xWallet = token;
+  }
 
-	// NTE: no need to implement entering/exiting with $WALLET, we can just use wrap/unwrap
+  // NTE: no need to implement entering/exiting with $WALLET, we can just use wrap/unwrap
 
-	// convert xWALLET to stkWALLET
-	function wrapAll() external {
-		wrap(xWallet.balanceOf(msg.sender));
-	}
+  // convert xWALLET to stkWALLET
+  function wrapAll() external {
+    wrap(xWallet.balanceOf(msg.sender));
+  }
 
-	function wrap(uint256 shareAmount) public {
-		shares[msg.sender] += shareAmount;
-		require(xWallet.transferFrom(msg.sender, address(this), shareAmount));
-		emit Transfer(address(0), msg.sender, (shareAmount * xWallet.shareValue()) / 1e18);
-	}
+  function wrap(uint256 shareAmount) public {
+    shares[msg.sender] += shareAmount;
+    require(xWallet.transferFrom(msg.sender, address(this), shareAmount));
+    emit Transfer(address(0), msg.sender, (shareAmount * xWallet.shareValue()) / 1e18);
+  }
 
-	// this is used to trigger unstaking
-	function unwrap(uint256 shareAmount) external {
-		shares[msg.sender] -= shareAmount;
-		require(xWallet.transfer(msg.sender, shareAmount));
-		emit Transfer(msg.sender, address(0), (shareAmount * xWallet.shareValue()) / 1e18);
-	}
+  // this is used to trigger unstaking
+  function unwrap(uint256 shareAmount) external {
+    shares[msg.sender] -= shareAmount;
+    require(xWallet.transfer(msg.sender, shareAmount));
+    emit Transfer(msg.sender, address(0), (shareAmount * xWallet.shareValue()) / 1e18);
+  }
 }
