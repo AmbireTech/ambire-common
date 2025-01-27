d7b69108cebd4e828487fb3955911fc40d47bbc7
Borislav Itskov <borislav@devlabs.bg>
2025-01-27 10:54:35 +0200

    add: odyssey test network correct 7702 addr

--- src/consts/7702.ts
+++ src/consts/7702.ts
@@ -2,7 +2,7 @@ import { Hex } from '../interfaces/hex'
 
 // for networks that don't have a singleton
 export const PECTRA_7702 = '0x7b829de68DA4B1C7f75b88061CaF530A2b56fF7e'
-export const ODYSSEY_7702 = '0x7b829de68DA4B1C7f75b88061CaF530A2b56fF7e'
+export const ODYSSEY_7702 = '0x8226995E02C70293595E0634C5F89547EDb08126'
 
 export interface Custom7702Settings {
   [chainId: string]: {
