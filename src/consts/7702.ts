81b1a2aed63369519b47fba03637fd3cef91e177
Borislav Itskov <borislav.ickov@gmail.com>
2025-02-03 14:22:36 +0200

    latest ambire 7702

--- src/consts/7702.ts
+++ src/consts/7702.ts
@@ -2,7 +2,7 @@ import { Hex } from '../interfaces/hex'
 
 // for networks that don't have a singleton
 export const PECTRA_7702 = '0x7b829de68DA4B1C7f75b88061CaF530A2b56fF7e'
-export const ODYSSEY_7702 = '0xd74e1EE696258E2bA32467e1D1c424c9f2fBeB62'
+export const ODYSSEY_7702 = '0xd5E2Da9f0Fe3F7A074D5ADD6FedCF383209d91FE'
 
 export interface Custom7702Settings {
   [chainId: string]: {
