52d063fb56f969abe47f31184a96ba0c6f282e47
sonytooo <simeonpetkov11999977@gmail.com>
2025-02-03 18:25:18 +0200

    readability improvements

--- src/libs/swapAndBridge/swapAndBridge.ts
+++ src/libs/swapAndBridge/swapAndBridge.ts
@@ -74,25 +74,20 @@ export const sortPortfolioTokenList = (accountPortfolioTokenList: TokenResult[])
  * Not all tokens in the portfolio are eligible.
  */
 export const getIsTokenEligibleForSwapAndBridge = (token: TokenResult) => {
-  const getAmount = (t: TokenResult): bigint => {
-    if (typeof t.amountPostSimulation === 'bigint') {
-      // Prevents the token from disappearing from the list if the
-      // amountPostSimulation equals the full token amount
-      if (t.amountPostSimulation === 0n && t.amount > 0n) return t.amount
-
-      return t.amountPostSimulation
-    }
-
-    return t.amount
-  }
-
+  // Prevent filtering out tokens with amountPostSimulation = 0 if the actual amount is positive.
+  // This ensures the token remains in the list when sending the full amount of it
+  const amount =
+    token.amountPostSimulation === 0n && token.amount > 0n
+      ? token.amount
+      : token.amountPostSimulation ?? token.amount
+  const hasPositiveBalance = Number(amount) > 0
   return (
     // The same token can be in the Gas Tank (or as a Reward) and in the portfolio.
     // Exclude the one in the Gas Tank (swapping Gas Tank tokens is not supported).
     !token.flags.onGasTank &&
     // And exclude the rewards ones (swapping rewards is not supported).
     !token.flags.rewardsType &&
-    Number(getAmount(token)) > 0
+    hasPositiveBalance
   )
 }
 
