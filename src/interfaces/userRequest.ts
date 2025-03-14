fce4f6bcc49d3e82c3f24caaaadcc1d6b75b3414
Petromir Petrov <petromirdev@gmail.com>
2025-03-14 16:43:02 +0200

    fix: SignUserRequest activeRouteId type

--- src/interfaces/userRequest.ts
+++ src/interfaces/userRequest.ts
@@ -59,7 +59,7 @@ export interface SignUserRequest {
     paymasterService?: PaymasterService
     isWalletSendCalls?: boolean
     submittedAccountOp?: any
-    activeRouteId?: string
+    activeRouteId?: number
     [key: string]: any
   }
   // defined only when SignUserRequest is built from a DappRequest
