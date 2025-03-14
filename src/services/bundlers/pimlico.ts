d2c2a35d3076029fa053103181abe03288afb743
Borislav Itskov <borislav.ickov@gmail.com>
2025-03-14 16:45:17 +0200

    fix: bundler and estimateBundler tests

--- src/services/bundlers/pimlico.ts
+++ src/services/bundlers/pimlico.ts
@@ -1,13 +1,7 @@
 /* eslint-disable class-methods-use-this */
-import { toBeHex } from 'ethers'
 
 import { BUNDLER, PIMLICO } from '../../consts/bundlers'
-import { ERC_4337_ENTRYPOINT } from '../../consts/deploy'
-import { EOA_SIMULATION_NONCE } from '../../consts/deployless'
 import { Network } from '../../interfaces/network'
-import { BundlerEstimateResult } from '../../libs/estimate/interfaces'
-import { UserOperation } from '../../libs/userOperation/types'
-import { getCleanUserOp } from '../../libs/userOperation/userOperation'
 import { Bundler } from './bundler'
 import { GasSpeeds, UserOpStatus } from './types'
 
@@ -32,36 +26,5 @@ export class Pimlico extends Bundler {
 
   public getName(): BUNDLER {
     return PIMLICO
-  }
-
-  private async send7702EstimateReq(
-    userOperation: UserOperation,
-    network: Network,
-    shouldStateOverride = false
-  ): Promise<BundlerEstimateResult> {
-    const provider = this.getProvider(network)
-
-    if (shouldStateOverride) {
-      return provider.send('pimlico_experimental_estimateUserOperationGas7702', [
-        {
-          ...getCleanUserOp(userOperation)[0]
-        },
-        ERC_4337_ENTRYPOINT,
-        {
-          [userOperation.sender]: {
-            stateDiff: {
-              [toBeHex(1, 32)]: EOA_SIMULATION_NONCE
-            }
-          }
-        }
-      ])
-    }
-
-    return provider.send('pimlico_experimental_estimateUserOperationGas7702', [
-      {
-        ...getCleanUserOp(userOperation)[0]
-      },
-      ERC_4337_ENTRYPOINT
-    ])
   }
 }
