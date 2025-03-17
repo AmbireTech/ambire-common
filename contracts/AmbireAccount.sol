dc48c9df33dad089d35c9e96872a8374b8309fb7
Ivo Georgiev <ivo@ambire.com>
2025-03-17 17:08:39 +0200

    AmbireAccount: change supportedInterface to pure

--- contracts/AmbireAccount.sol
+++ contracts/AmbireAccount.sol
@@ -267,7 +267,7 @@ contract AmbireAccount is IAmbireAccount {
 	 * @param   interfaceID  the interface we're signaling support for
 	 * @return  bool  do we support the interface or not
 	 */
-	function supportsInterface(bytes4 interfaceID) external view returns (bool) {
+	function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
 		bool supported = interfaceID == 0x01ffc9a7 || // ERC-165 support (i.e. `bytes4(keccak256('supportsInterface(bytes4)'))`).
 			interfaceID == 0x150b7a02 || // ERC721TokenReceiver
 			interfaceID == 0x4e2312e0 || // ERC-1155 `ERC1155TokenReceiver` support (i.e. `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")) ^ bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`).
