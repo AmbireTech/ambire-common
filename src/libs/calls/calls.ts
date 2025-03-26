2afe82c269049313b51a6fe79af006856058ebe9
Petromir Petrov <petromirdev@gmail.com>
2025-03-26 16:16:33 +0200

    update: decodeFeeCall to be backwards compatible with the old gas tank

--- src/libs/calls/calls.ts
+++ src/libs/calls/calls.ts
@@ -3,6 +3,7 @@ import { AbiCoder, Interface, ZeroAddress } from 'ethers'
 import ERC20 from '../../../contracts/compiled/IERC20.json'
 import { FEE_COLLECTOR } from '../../consts/addresses'
 import { DEPLOYLESS_SIMULATION_FROM } from '../../consts/deploy'
+import gasTankFeeTokens from '../../consts/gasTankFeeTokens'
 import { networks } from '../../consts/networks'
 import { Network } from '../../interfaces/network'
 import { Call } from '../accountOp/types'
@@ -66,17 +67,44 @@ export function decodeFeeCall(
       }
     }
 
-    const [, amount] = abiCoder.decode(['string', 'uint256', 'string'], data)
+    const [, amount, symbol] = abiCoder.decode(['string', 'uint256', 'string'], data)
 
-    // USDC is the only token in the gas tank.
-    // It's hard-coded this way as the gas tank can be used
-    // on multiple networks and some of them have a different
-    // amount of decimals. (e.g. Binance Smart Chain)
+    // Prioritize Ethereum tokens
+    const ethereumToken = gasTankFeeTokens.find(
+      ({ symbol: tSymbol, networkId: tNetworkId }) =>
+        tSymbol.toLowerCase() === symbol.toLowerCase() && tNetworkId === 'ethereum'
+    )
+    // Fallback to network tokens
+    const networkToken =
+      network.id !== 'ethereum'
+        ? gasTankFeeTokens.find(
+            ({ symbol: tSymbol, networkId: tNetworkId }) =>
+              tSymbol.toLowerCase() === symbol.toLowerCase() && tNetworkId === network.id
+          )
+        : null
+    // Fallback to any network token. Example: user paid the fee on Base
+    // with Wrapped Matic (neither Ethereum nor Base token)
+    const anyNetworkToken = gasTankFeeTokens.find(
+      ({ symbol: tSymbol }) => tSymbol.toLowerCase() === symbol.toLowerCase()
+    )
+
+    // This is done for backwards compatibility with the old gas tank. A known flaw
+    // is that it may prioritize the wrong token. Example: a user had paid the fee with
+    // USDT on BSC, but we prioritize the USDT on Ethereum. 18 vs 6 decimals.
+    // There is no way to fix this as the call data doesn't contain the decimals nor
+    // the network of the token.
+    const { address, networkId } = ethereumToken || networkToken || anyNetworkToken || {}
+
+    if (!address)
+      throw new Error(
+        `Unable to find gas tank fee token for symbol ${symbol} and network ${network.id}`
+      )
+
     return {
       amount,
-      address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      address,
       isGasTank: true,
-      network: networks.find(({ chainId }) => chainId === 1n)!
+      network: networks.find(({ id }) => id === networkId)!
     }
   }
 
