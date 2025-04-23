7ae8375ee8b965ec1a07467c710b69ef68788ac9
Gergana Mihaylova <gergana@devlabs.bg>
2025-04-23 18:05:44 +0300

    Update: legends in whitelisted dapps

--- src/consts/dappCommunication.ts
+++ src/consts/dappCommunication.ts
@@ -1,6 +1,8 @@
 const ORIGINS_WHITELISTED_TO_ALL_ACCOUNTS = [
   'https://rewards.ambire.com',
   'https://rewards-staging.ambire.com',
+  'https://legends.ambire.com',
+  'https://legends-staging.ambire.com',
   'http://localhost:19006',
   'http://localhost:19007'
 ]
