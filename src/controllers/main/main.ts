c67d10310130550e9d48b5c42a27d757a7b274e5
Borislav Itskov <borislav.ickov@gmail.com>
2025-05-05 15:45:44 +0300

    if after simulateSwapOrBridge there was a signing error, override the pending results

--- src/controllers/main/main.ts
+++ src/controllers/main/main.ts
@@ -606,7 +606,18 @@ export class MainController extends EventEmitter {
     }
 
     if (type === SIGN_ACCOUNT_OP_SWAP) {
-      this.swapAndBridge.signAccountOpController?.simulateSwapOrBridge()
+      this.swapAndBridge.signAccountOpController?.simulateSwapOrBridge().then(() => {
+        // if an error has ocurred while signing and we're back to SigningStatus.ReadyToSign,
+        // override the pending results as they will be incorrect
+        if (
+          this.swapAndBridge.signAccountOpController &&
+          this.swapAndBridge.signAccountOpController.status?.type === SigningStatus.ReadyToSign
+        ) {
+          this.portfolio.overridePendingResults(
+            this.swapAndBridge.signAccountOpController.accountOp
+          )
+        }
+      })
     }
 
     await this.withStatus(
