6e198fdee9b1294ececb86343bdc94f2d63a9450
Borislav Itskov <borislav.ickov@gmail.com>
2025-05-16 16:11:54 +0300

    persist the sponsor data limits only if they are present

--- src/controllers/signAccountOp/signAccountOp.ts
+++ src/controllers/signAccountOp/signAccountOp.ts
@@ -1452,11 +1452,16 @@ export class SignAccountOpController extends EventEmitter {
     if (!paymaster.isUsable()) return { required: false }
 
     const localOp = { ...originalUserOp }
-    // persist the paymaster properties from the pm_stubData request
+
+    // persist the paymaster properties from the pm_stubData request if any
     if (paymaster.isSponsored() && paymaster.sponsorDataEstimation) {
-      localOp.paymasterVerificationGasLimit =
-        paymaster.sponsorDataEstimation.paymasterVerificationGasLimit
-      localOp.paymasterPostOpGasLimit = paymaster.sponsorDataEstimation.paymasterPostOpGasLimit
+      if (paymaster.sponsorDataEstimation.paymasterVerificationGasLimit) {
+        localOp.paymasterVerificationGasLimit =
+          paymaster.sponsorDataEstimation.paymasterVerificationGasLimit
+      }
+      if (paymaster.sponsorDataEstimation.paymasterPostOpGasLimit) {
+        localOp.paymasterPostOpGasLimit = paymaster.sponsorDataEstimation.paymasterPostOpGasLimit
+      }
     }
     const response = await paymaster.call(this.account, this.accountOp, localOp, this.#network)
 
