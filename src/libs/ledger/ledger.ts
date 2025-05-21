e5570fd20d7df7aefb083de18efdebf1b262bac0
Kaloyan Kosev <superkalo@devlabs.bg>
2025-05-21 15:18:57 +0300

    Add: blind sinign disabled error code

--- src/libs/ledger/ledger.ts
+++ src/libs/ledger/ledger.ts
@@ -34,7 +34,7 @@ export const normalizeLedgerMessage = (error?: string): string => {
   if (error.includes('6985')) {
     return 'Rejected by your Ledger device.'
   }
-  if (error.toLowerCase().includes('please enable blind signing')) {
+  if (error.toLowerCase().includes('please enable blind signing') || error.includes('6a80')) {
     return 'Blind Signing is disabled on your Ledger device. To sign this transaction, please enable Blind Signing (formerly called Contract Data) in the Ethereum app settings on your Ledger device, then try again.'
   }
 
