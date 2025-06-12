57472cdf026b99dbb0f234f232fdb5c2792541c7
Petromir Petrov <petromirdev@gmail.com>
2025-06-12 12:30:39 +0300

    tests: update 0x7b36c479 humanization

--- src/libs/errorHumanizer/estimationErrorHumanizer.test.ts
+++ src/libs/errorHumanizer/estimationErrorHumanizer.test.ts
@@ -39,7 +39,7 @@ describe('Estimation errors are humanized', () => {
   })
   it('0x7b36c479 (PartialSwapsNotAllowed)', () => {
     const { restore } = suppressConsole()
-    const EXPECTED_MESSAGE = `${MESSAGE_PREFIX} of a Swap failure. Please try performing the same swap again.`
+    const EXPECTED_MESSAGE = `${MESSAGE_PREFIX} of low liquidity, slippage limits, or insufficient token approval.`
     const error = new Error('0x7b36c479')
 
     const humanizedError = getHumanReadableEstimationError(error)
