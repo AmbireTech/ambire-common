b87849ae1022282820d2f361ceff9bbab182586f
Onizuka-wl <onizuka@wonderland.xyz>
2025-07-03 12:31:34 -0300

    fix: 🐛 clean the form when back (#38)

--- src/controllers/transaction/transactionFormState.ts
+++ src/controllers/transaction/transactionFormState.ts
@@ -283,7 +283,8 @@ export class TransactionFormState extends EventEmitter {
   }
 
   unloadScreen(sessionId: string, forceUnload?: boolean) {
-    const isFormDirty = !!this.fromAmount || !!this.toSelectedToken
+    const isFormDirty =
+      !!this.fromAmount || !!this.toSelectedToken || !!this.addressState.fieldValue
     // const signAccountOpCtrlStatus = this.dependencies.signAccountOpController?.status?.type
     // const isSigningOrBroadcasting =
     //   signAccountOpCtrlStatus && noStateUpdateStatuses.includes(signAccountOpCtrlStatus)
