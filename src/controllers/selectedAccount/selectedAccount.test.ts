440308e7ed69bd4193480d74c981143f09b6f6fa
sonytooo <simeonpetkov11999977@gmail.com>
2025-07-28 17:36:39 +0300

    tests fixes

--- src/controllers/selectedAccount/selectedAccount.test.ts
+++ src/controllers/selectedAccount/selectedAccount.test.ts
@@ -287,7 +287,13 @@ describe('SelectedAccount Controller', () => {
     it('Defi error banner is displayed when there is a critical network error and the user has positions on that network/provider', async () => {
       // Bypass the `updatePositions` cache by setting `maxDataAgeMs` to 0.
       // Otherwise, no update is emitted and the test cannot proceed.
+      jest.spyOn(defiPositionsCtrl, 'getDefiPositionsState').mockImplementation(() => ({
+        '1': { positionsByProvider: [], isLoading: false, updatedAt: 0 }
+      }))
+      await defiPositionsCtrl.updatePositions({ maxDataAgeMs: 0, forceUpdate: true })
+      await waitNextControllerUpdate(selectedAccountCtrl)
 
+      expect(selectedAccountCtrl.balanceAffectingErrors.length).toBe(0)
       // Mock an error
       jest.spyOn(defiPositionsCtrl, 'getDefiPositionsState').mockImplementation(() => ({
         '1': {
