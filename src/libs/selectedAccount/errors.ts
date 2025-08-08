a437bed660ea614b47edd42d1a616b2ca90f702f
Petromir Petrov <petromirdev@gmail.com>
2025-08-08 12:12:20 +0300

    fix: selected account isReadyToBeVisualized and error reporting bugs

--- src/libs/selectedAccount/errors.ts
+++ src/libs/selectedAccount/errors.ts
@@ -190,9 +190,17 @@ export const getNetworksWithPortfolioErrorErrors = ({
       return
     }
 
-    if (portfolioForNetwork?.isLoading) {
+    // If the network is loading a loading-too-long error is added
+    // BUT only if there is no critical error+no result, as that implies that the first
+    // loading has failed and a subsequent loading is in the process.
+    // Scenario: on the first load all networks fail with a critical error. Something triggers
+    // a second portfolio load. The state is: isLoading = true, criticalError = true, result = undefined.
+    // In this case we MUST display an error in the UI, so this branch is skipped.
+    if (portfolioForNetwork?.isLoading && !(criticalError && !portfolioForNetwork.result)) {
       // Add an error if the network is preventing the portfolio from going ready
-      // Otherwise skip the network
+      // The error is added, regardless of whether the loading is taking too long (> 5s)
+      // or not. This is determined in the UI. The error is added so the UI knows which networks
+      // are preventing the portfolio from going ready.
       if (!isAllReady) errors = addPortfolioError(errors, networkName, 'loading-too-long')
       return
     }
@@ -204,10 +212,12 @@ export const getNetworksWithPortfolioErrorErrors = ({
     if (!portfolioForNetwork || !chainId) return
     // Don't display an error banner if the RPC isn't working because an RPC error banner is already displayed.
     // In case of additional networks don't check the RPC as there isn't one
-    if (
-      criticalError &&
-      (['gasTank', 'rewards'].includes(chainId) || providers[chainId]?.isWorking)
-    ) {
+    const rpcProvider = providers[chainId]
+    // Don't display an error banner if the RPC isn't working because an RPC error banner is already displayed.
+    const isProviderWorking =
+      rpcProvider && typeof rpcProvider.isWorking === 'boolean' && rpcProvider.isWorking
+
+    if (criticalError && (['gasTank', 'rewards'].includes(chainId) || isProviderWorking)) {
       errors = addPortfolioError(errors, networkName, 'portfolio-critical')
       return
     }
