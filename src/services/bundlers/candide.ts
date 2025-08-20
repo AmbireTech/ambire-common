cf1a5edd39452fe8ff6ab90371517d60093836b1
Borislav Itskov <borislav.ickov@gmail.com>
2025-08-20 14:03:07 +0900

    make candide's bundler work smoothly by firing a re-estimate request before broadcast

--- src/services/bundlers/candide.ts
+++ src/services/bundlers/candide.ts
@@ -1,8 +1,12 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { toBeHex } from 'ethers'
 import { BUNDLER, CANDIDE } from '../../consts/bundlers'
+import { Hex } from '../../interfaces/hex'
 import { Network } from '../../interfaces/network'
+import { BundlerEstimateResult, BundlerStateOverride } from '../../libs/estimate/interfaces'
+import { UserOperation } from '../../libs/userOperation/types'
 import { Bundler } from './bundler'
 import { GasSpeeds, UserOpStatus } from './types'
 
@@ -60,11 +64,32 @@ export class Candide extends Bundler {
     }
   }
 
+  async estimate(
+    userOperation: UserOperation,
+    network: Network,
+    stateOverride?: BundlerStateOverride
+  ): Promise<BundlerEstimateResult> {
+    const estimatiton = await this.sendEstimateReq(userOperation, network, stateOverride)
+
+    return {
+      // add 20000n overhead as discussed with candide
+      preVerificationGas: toBeHex(BigInt(estimatiton.preVerificationGas) + 20000n) as Hex,
+      verificationGasLimit: toBeHex(BigInt(estimatiton.verificationGasLimit) + 20000n) as Hex,
+      callGasLimit: toBeHex(estimatiton.callGasLimit) as Hex,
+      paymasterVerificationGasLimit: estimatiton.paymasterVerificationGasLimit
+        ? (toBeHex(estimatiton.paymasterVerificationGasLimit) as Hex)
+        : '0x00',
+      paymasterPostOpGasLimit: estimatiton.paymasterPostOpGasLimit
+        ? (toBeHex(estimatiton.paymasterPostOpGasLimit) as Hex)
+        : '0x00'
+    }
+  }
+
   public getName(): BUNDLER {
     return CANDIDE
   }
 
   public shouldReestimateBeforeBroadcast(network: Network): boolean {
-    return false
+    return true
   }
 }
