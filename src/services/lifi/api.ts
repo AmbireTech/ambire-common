eecdcfdbc7e854bdf3da67aab2df442b43836a59
Kaloyan Kosev <superkalo@devlabs.bg>
2025-08-29 19:34:21 +0300

    Add: a note

--- src/services/lifi/api.ts
+++ src/services/lifi/api.ts
@@ -376,7 +376,7 @@ export class LiFiAPI {
         // Upstream error coming from LiFi, that must be the most accurate
         upstreamMessage && upstreamCode
           ? `${upstreamMessage} Reference: ${upstreamCode}`
-          : upstreamMessage || JSON.stringify(responseBody).slice(0, 200)
+          : upstreamMessage || JSON.stringify(responseBody).slice(0, 200) // results a toast up to 5 lines
 
       const error = `${errorPrefix} Our service provider LiFi responded: <${fallbackMessage}>`
       throw new SwapAndBridgeProviderApiError(error)
