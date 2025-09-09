76c029e0e1f753190cca1cb84cbbcaabd2bd6669
Yosif Hamed <josif.hameed@gmail.com>
2025-09-09 18:27:34 +0300

    chore(contract-names): remove commented-out code

--- src/interfaces/contractNames.ts
+++ src/interfaces/contractNames.ts
@@ -1,21 +1,5 @@
 import { ControllerInterface } from './controller'
 
-// @TODO add the controller
 export type IContractNamesController = ControllerInterface<
   InstanceType<typeof import('../controllers/contractNames/contractNames').ContractNamesController>
 >
-
-// TODO add types
-// type AddressState = {
-//   fieldValue: string
-//   ensAddress: string
-//   isDomainResolving: boolean
-// }
-
-// type AddressStateOptional = {
-//   fieldValue?: AddressState['fieldValue']
-//   ensAddress?: AddressState['ensAddress']
-//   isDomainResolving?: AddressState['isDomainResolving']
-// }
-
-// export type { AddressState, AddressStateOptional }
