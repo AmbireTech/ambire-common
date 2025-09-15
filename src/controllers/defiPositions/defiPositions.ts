78d6073d149fe62c326660c380535ee3d6fcbbc0
sonytooo <simeonpetkov11999977@gmail.com>
2025-09-15 16:11:13 +0300

    write it more optimally

--- src/controllers/defiPositions/defiPositions.ts
+++ src/controllers/defiPositions/defiPositions.ts
@@ -379,7 +379,6 @@ export class DefiPositionsController extends EventEmitter implements IDefiPositi
 
     let debankPositions: PositionsByProvider[] = []
 
-    let failedToFetchDebankPositions = false
     // Skip Debank call in testing mode — only fetch custom DeFi positions
     if (process.env.IS_TESTING !== 'true') {
       try {
@@ -401,13 +400,10 @@ export class DefiPositionsController extends EventEmitter implements IDefiPositi
           chainId: BigInt(p.chainId)
         }))
       } catch (err) {
-        failedToFetchDebankPositions = true
         console.error('Debank fetch failed:', err)
-        // Proceed with empty debank positions
+        return // return if it fails to prevent hiding/overriding already stored DeFi pos
       }
     }
-
-    if (failedToFetchDebankPositions) return
 
     await Promise.all(networksToUpdate.map((n) => updateSingleNetwork(n, debankPositions)))
     await this.#updateNetworksWithPositions(selectedAccountAddr, this.#state[selectedAccountAddr])
