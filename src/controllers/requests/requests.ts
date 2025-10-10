a3477057b876a50d1f65e3c36669495a1648f9c1
Petromir Petrov <petromirdev@gmail.com>
2025-10-10 13:52:22 +0300

    revert: account state change

--- src/controllers/requests/requests.ts
+++ src/controllers/requests/requests.ts
@@ -333,20 +333,10 @@ export class RequestsController extends EventEmitter implements IRequestsControl
       const { id, action, meta } = req
       if (action.kind === 'calls') {
         const account = this.#accounts.accounts.find((x) => x.addr === meta.accountAddr)!
-        const accountStateBefore = this.#accounts.accountStates?.[meta.accountAddr]?.[meta.chainId]
-
-        // Try to update the account state for 3 seconds. If that fails, use the previous account state if it exists,
-        // otherwise wait for the fetch to complete (no matter how long it takes).
-        // This is done in an attempt to always have the latest nonce, but without blocking the UI for too long if the RPC is slow to respond.
-        const accountState = (await Promise.race([
-          this.#accounts.forceFetchPendingState(meta.accountAddr, meta.chainId),
-          // Fallback to the old account state if fetch takes too long and it exists
-          accountStateBefore
-            ? new Promise((res) => {
-                setTimeout(() => res(accountStateBefore), 3000)
-              })
-            : new Promise(() => {}) // Explicitly never-resolving promise
-        ])) as any
+        const accountState = await this.#accounts.getOrFetchAccountOnChainState(
+          meta.accountAddr,
+          meta.chainId
+        )
         const network = this.#networks.networks.find((n) => n.chainId === meta.chainId)!
 
         const accountOpAction = makeAccountOpAction({
