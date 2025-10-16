8b83b7336d1e668755b33b716f70263cacc6d890
Kaloyan Kosev <superkalo@devlabs.bg>
2025-10-16 12:52:20 +0300

    Fix: missing req prop

--- src/libs/tracer/debugTraceCall.test.ts
+++ src/libs/tracer/debugTraceCall.test.ts
@@ -56,7 +56,8 @@ describe('Debug tracecall detection for transactions', () => {
         inToken: '0x0000000000000000000000000000000000000000',
         amount: 5205038755874012n,
         simulatedGasLimit: 139864n,
-        gasPrice: 33831818155n
+        gasPrice: 33831818155n,
+        paidByKeyType: 'internal'
       },
       nonce: 1n,
       signature: '0x000000000000000000000000db26aeea3b986887feaba661df6d211e725797a003',
