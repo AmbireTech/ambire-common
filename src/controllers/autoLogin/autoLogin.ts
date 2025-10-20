ffe67d9f4150d81a953333d7eaab4830eb903d2a
Petromir Petrov <petromirdev@gmail.com>
2025-10-20 11:59:07 +0300

    update: auto login duration options and set default to 30 days

--- src/controllers/autoLogin/autoLogin.ts
+++ src/controllers/autoLogin/autoLogin.ts
@@ -36,22 +36,24 @@ const prefixRegex =
 const DEFAULT_POLICIES: DefaultAutoLoginPolicy[] = []
 
 const DEFAULT_AUTO_LOGIN_DURATION_OPTION = {
-  label: '7 days',
-  value: 7 * 24 * 60 * 60 * 1000
+  label: '30 days',
+  value: 30 * 24 * 60 * 60 * 1000
 }
 
 // Implemented here to ensure consistency between the controller and the UI
 // Also, in the future when the duration setting becomes exposed to the UI we
 // will need to validate the input from the UI, so these will be useful
 export const AUTO_LOGIN_DURATION_OPTIONS = [
-  { label: '4 hours', value: 4 * 60 * 60 * 1000 },
-  { label: '8 hours', value: 8 * 60 * 60 * 1000 },
   { label: '24 hours', value: 24 * 60 * 60 * 1000 },
-  DEFAULT_AUTO_LOGIN_DURATION_OPTION,
   {
-    label: '30 days',
-    value: 30 * 24 * 60 * 60 * 1000
-  }
+    label: '7 days',
+    value: 7 * 24 * 60 * 60 * 1000
+  },
+  {
+    label: '14 days',
+    value: 14 * 24 * 60 * 60 * 1000
+  },
+  DEFAULT_AUTO_LOGIN_DURATION_OPTION
 ]
 
 export class AutoLoginController extends EventEmitter implements IAutoLoginController {
