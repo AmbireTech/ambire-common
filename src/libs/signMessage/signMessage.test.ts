3973036930e761119d40a0de7834bfcd06271596
Kaloyan Kosev <superkalo@devlabs.bg>
2025-10-24 11:08:01 +0300

    Fix: sync unit tests with latest interface changes

--- src/libs/signMessage/signMessage.test.ts
+++ src/libs/signMessage/signMessage.test.ts
@@ -880,10 +880,13 @@ describe('Sign Message, Keystore with key dedicatedToOneSA: true ', () => {
     const accountStates = await getAccountsInfo([eoaAccount])
     const signer = await keystore.getSigner(eoaSigner.keyPublicAddress, 'internal')
 
-    const authorizationHash = getAuthorizationHash(1n, EIP_7702_AMBIRE_ACCOUNT, 0n)
-    // TODO: Adjust test
-    const signature = signer.sign7702(authorizationHash)
+    const signature = await signer.sign7702({
+      chainId: 1n,
+      contract: EIP_7702_AMBIRE_ACCOUNT,
+      nonce: 0n
+    })
     const provider = getRpcProvider(ethereumNetwork.rpcUrls, ethereumNetwork.chainId)
+    const authorizationHash = getAuthorizationHash(1n, EIP_7702_AMBIRE_ACCOUNT, 0n)
     const authorizationRes = await verifyMessage({
       provider,
       signer: eoaSigner.keyPublicAddress,
@@ -896,10 +899,13 @@ describe('Sign Message, Keystore with key dedicatedToOneSA: true ', () => {
     })
     expect(authorizationRes).toBe(true)
 
-    // increment the nonce to be sure 'v' transform is working
+    const signature2 = await signer.sign7702({
+      chainId: 1n,
+      contract: EIP_7702_AMBIRE_ACCOUNT,
+      // increment the nonce to be sure 'v' transform is working
+      nonce: 1n
+    })
     const authorizationHash2 = getAuthorizationHash(1n, EIP_7702_AMBIRE_ACCOUNT, 1n)
-    // TODO: Adjust test
-    const signature2 = signer.sign7702(authorizationHash2)
     const authorizationRes2 = await verifyMessage({
       provider,
       signer: eoaSigner.keyPublicAddress,
