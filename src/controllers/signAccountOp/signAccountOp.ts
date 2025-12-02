5d96574dd23487828d2e022046876d2f4d70c77c
Borislav Itskov <borislav.ickov@gmail.com>
2025-12-02 12:48:56 +0200

    throw the emitted error if passed

--- src/controllers/signAccountOp/signAccountOp.ts
+++ src/controllers/signAccountOp/signAccountOp.ts
@@ -2668,7 +2668,7 @@ export class SignAccountOpController extends EventEmitter implements ISignAccoun
       error: _err || new Error(message),
       sendCrashReport: _err && 'sendCrashReport' in _err ? _err.sendCrashReport : undefined
     })
-    throw new Error(message) // so that broadcast resolves with an error status
+    throw _err || new Error(message) // so that broadcast resolves with an error status
   }
 
   canUpdate(): boolean {
