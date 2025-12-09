35c449231869b730055219c929c66e7b719644bd
Petromir Petrov <petromirdev@gmail.com>
2025-12-09 11:09:37 +0200

    remove: debug log

--- src/controllers/ui/ui.ts
+++ src/controllers/ui/ui.ts
@@ -69,7 +69,7 @@ export class UiController extends EventEmitter implements IUiController {
 
   removeView(viewId: string) {
     const view = this.views.find((v) => v.id === viewId)
-    console.log('Debug: removing view with id', viewId, 'found view:', view)
+
     if (!view) return
 
     this.views = this.views.filter((v) => v.id !== viewId)
