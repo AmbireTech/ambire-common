379845a3afbe92558d93c4dc5a38a70ea6c1b750
Borislav Itskov <borislav.ickov@gmail.com>
2026-02-16 07:06:50 +0200

    fix: better error msg for safe account not deployed

--- src/controllers/signAccountOp/signAccountOp.ts
+++ src/controllers/signAccountOp/signAccountOp.ts
@@ -978,7 +978,7 @@ export class SignAccountOpController extends EventEmitter implements ISignAccoun
     // if the safe txn is not deployed, display an error
     if (!!this.account.safeCreation && accountState && !accountState.isDeployed) {
       errors.push({
-        title: 'Safe account not deployed. Please activate it from Safe global'
+        title: 'Safe account not deployed on this network. Please activate it from Safe global'
       })
     }
 
