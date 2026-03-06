68e9af5c6bf0052ddb06e77c1ac2e7f2d79fe1a5
Petromir Petrov <petromirdev@gmail.com>
2026-03-06 16:32:22 +0200

    strip exchange info

--- src/libs/portfolio/interfaces.ts
+++ src/libs/portfolio/interfaces.ts
@@ -24,15 +24,8 @@ export type SuspectedType = 'suspected' | null
 export type ExchangeInfo = {
   id: string
   name: string
-  year_established: number
-  country: string
-  description: string
   url: string
   image: string
-  has_trading_incentive: boolean
-  trust_score: number
-  trust_score_rank: number
-  trade_volume_24h_btc: number
 }
 
 export type ExchangeInfoMap = {
