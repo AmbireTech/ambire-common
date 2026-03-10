686aeee6b0de20a880c2a84e70b9e49f001cfad8
Borislav Itskov <borislav@devlabs.bg>
2026-03-10 17:57:20 +0200

    add: owner & threshold safe humanization

--- src/libs/humanizer/messageModules/safeModule.ts
+++ src/libs/humanizer/messageModules/safeModule.ts
@@ -1,9 +1,109 @@
-import { isAddress } from 'ethers'
+import { Interface, isAddress } from 'ethers'
 
 import { Message } from '../../../interfaces/userRequest'
-import { HumanizerTypedMessageModule, HumanizerVisualization } from '../interfaces'
+import { SafeV2 } from '../const/abis/Safe'
+import {
+  HumanizerTypedMessageModule,
+  HumanizerVisualization,
+  HumanizerWarning
+} from '../interfaces'
 import { genericErc20Humanizer } from '../modules/Tokens'
-import { getAction, getAddressVisualization, getLabel, getWarning } from '../utils'
+import { getAction, getAddressVisualization, getBreak, getLabel, getWarning } from '../utils'
+
+const getOwnerChangeHumanization = (
+  data?: string
+): { visuals?: HumanizerVisualization[]; warnings?: HumanizerWarning[] } | undefined => {
+  if (!data) return
+
+  const selector = data.substring(0, 10)
+  const safeInterface = new Interface(SafeV2)
+  const fullVisualization: HumanizerVisualization[] = []
+  const warnings: HumanizerWarning[] = []
+
+  const addOwnerWithThreshold = safeInterface.getFunction('addOwnerWithThreshold')?.selector
+  if (selector === addOwnerWithThreshold) {
+    const decoded = safeInterface.decodeFunctionData('addOwnerWithThreshold', data)
+    const newOwner = decoded[0]
+    const newThreshold = decoded[1]
+    fullVisualization.push(
+      ...[
+        getBreak(),
+        getAction('Add owner'),
+        getAddressVisualization(newOwner),
+        getAction('and set threshold to'),
+        getLabel(newThreshold)
+      ]
+    )
+    warnings.push(
+      getWarning(`Owner & threshold configuration changes detected`, 'SAFE{WALLET}_CONFIG_CHANGE')
+    )
+    return {
+      visuals: fullVisualization,
+      warnings
+    }
+  }
+
+  const changeThreshold = safeInterface.getFunction('changeThreshold')?.selector
+  if (selector === changeThreshold) {
+    const decoded = safeInterface.decodeFunctionData('changeThreshold', data)
+    const newThreshold = decoded[0]
+    fullVisualization.push(...[getBreak(), getAction('Set threshold to'), getLabel(newThreshold)])
+    warnings.push(
+      getWarning(`Threshold configuration changes detected`, 'SAFE{WALLET}_CONFIG_CHANGE')
+    )
+    return {
+      visuals: fullVisualization,
+      warnings
+    }
+  }
+
+  const removeOwner = safeInterface.getFunction('removeOwner')?.selector
+  if (selector === removeOwner) {
+    const decoded = safeInterface.decodeFunctionData('removeOwner', data)
+    const removedOwner = decoded[1]
+    const newThreshold = decoded[2]
+    fullVisualization.push(
+      ...[
+        getBreak(),
+        getAction('Remove owner'),
+        getAddressVisualization(removedOwner),
+        getAction('and set threshold to'),
+        getLabel(newThreshold)
+      ]
+    )
+    warnings.push(
+      getWarning(`Owner & threshold configuration changes detected`, 'SAFE{WALLET}_CONFIG_CHANGE')
+    )
+    return {
+      visuals: fullVisualization,
+      warnings
+    }
+  }
+
+  const swapOwner = safeInterface.getFunction('swapOwner')?.selector
+  if (selector === swapOwner) {
+    const decoded = safeInterface.decodeFunctionData('swapOwner', data)
+    const removedOwner = decoded[1]
+    const newOwner = decoded[2]
+    fullVisualization.push(
+      ...[
+        getBreak(),
+        getAction('Remove owner'),
+        getAddressVisualization(removedOwner),
+        getBreak(),
+        getAction('Set new owner'),
+        getLabel(newOwner)
+      ]
+    )
+    warnings.push(getWarning(`Owner configuration changes detected`, 'SAFE{WALLET}_CONFIG_CHANGE'))
+    return {
+      visuals: fullVisualization,
+      warnings
+    }
+  }
+
+  return undefined
+}
 
 export const safeMessageModule: HumanizerTypedMessageModule = (message: Message) => {
   if (message.content.kind === 'message' || typeof message.content.message === 'string')
@@ -13,6 +113,7 @@ export const safeMessageModule: HumanizerTypedMessageModule = (message: Message)
   const { accountAddr } = message
   const { verifyingContract } = message.content.domain
   const humanizedCalls = genericErc20Humanizer({ accountAddr }, [{ to, value, data }])
+  const ownerHumanization = getOwnerChangeHumanization(data)
   const fullVisualization: HumanizerVisualization[] = []
   if (!isAddress(verifyingContract)) return {}
   fullVisualization.push(
@@ -20,7 +121,8 @@ export const safeMessageModule: HumanizerTypedMessageModule = (message: Message)
       getAction('Safe{WALLET} transaction'),
       getLabel('from'),
       getAddressVisualization(verifyingContract)
-    ]
+    ],
+    ...(ownerHumanization && ownerHumanization.visuals ? ownerHumanization.visuals : [])
   )
   if (humanizedCalls[0]?.fullVisualization) {
     fullVisualization.push(...humanizedCalls[0].fullVisualization)
@@ -34,5 +136,8 @@ export const safeMessageModule: HumanizerTypedMessageModule = (message: Message)
     }
   }
 
-  return { fullVisualization }
+  return {
+    fullVisualization,
+    warnings: ownerHumanization && ownerHumanization.warnings ? ownerHumanization.warnings : []
+  }
 }
