b32db31f1ec3f7aa2c61b1580b1cff11d798c7d9
Kaloyan Kosev <superkalo@goodmorning.dev>
2026-05-11 14:20:27 +0300

    Refactor: consolidate ESLint ignore rules into config and update dependencies
    - Removed .eslintignore file and integrated ignore rules into eslint.config.js.
    - Updated ESLint and related plugins to latest versions in package.json and package-lock.json.

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,16 @@ const ambirePlugin = require('./eslint-rules')
 
 module.exports = [
   {
+    ignores: [
+      '.eslintrc.js',
+      'node_modules/**',
+      'coverage/**',
+      'babel_cache/**',
+      'artifacts/**',
+      'dist/**'
+    ]
+  },
+  {
     files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: typescriptParser,
