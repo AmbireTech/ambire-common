a31ff3a3d6f6915b1292877da8374d7ffd0ff6cb
Petromir Petrov <petromir@goodmorning.dev>
2026-09-09 12:41:52 +0300

    fix: forgotten hardcoded warning

--- src/libs/swapAndBridge/swapAndBridge.ts
+++ src/libs/swapAndBridge/swapAndBridge.ts
@@ -605,13 +605,6 @@ export const calculateAmountWarnings = (
   fromAmount: string,
   fromSelectedTokenDecimals: number
 ): SwapAmountWarning | null => {
-  //test
-  return {
-    type: 'highPriceImpact',
-    percentageDiff: 2,
-    estimatedLossUsd: 20,
-    severity: 'extreme'
-  }
   if (!selectedRoute) return null
 
   let inputValueInUsd = 0
